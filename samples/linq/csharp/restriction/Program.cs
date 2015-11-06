674ff399b14b6c4b62bed38bb68dbb1ada4c3cb8
Matthew Reily <matthew.reily@interworks.com>
2015-11-06 16:50:11 -0600

    added 2 additional restriction samples. Added a fake data repository and refactored example methods to be static.
    fixed tabbing issue in where-sample-4.cs

--- samples/linq/csharp/restriction/Program.cs
+++ samples/linq/csharp/restriction/Program.cs
@@ -2,14 +2,17 @@ namespace Restriction
 {
     public class Program
     {
+        //Entry point of application
         public void Main(string[] args)
         {
-            var sample = new WhereClause1();
-            sample.QuerySyntaxExample();
-            sample.MethodSyntaxExample();
-            var sample2 = new WhereClause2();
-            sample2.MethodSyntaxExample();
-            sample2.QuerySyntaxExample();
+            //Invoke the linq restriction examples within the project
+            WhereClause1.QuerySyntaxExample();
+            WhereClause1.MethodSyntaxExample();
+            WhereClause2.QuerySyntaxExample();
+            WhereClause2.MethodSyntaxExample();
+            WhereClause3.QuerySyntaxExample();
+            WhereClause3.MethodSyntaxExample();
+            WhereClause4.Example();
         }
     }
 }

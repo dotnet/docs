39116260931c9d42fcc28ed3193648fab2c53ed4
matthew.reily@interworks.com <matthew.reily@interworks.com>
2015-11-11 10:55:34 -0600

    fixed some spacing issues in the program.cs

--- samples/linq/csharp/aggregate/program.cs
+++ samples/linq/csharp/aggregate/program.cs
@@ -1,14 +1,13 @@
 public class Program
+{
+    public void Main(string[] args)
     {
-        public void Main(string[] args)
-        {
-            Aggregate.AggregateSample1.MethodSyntaxExample();
+        Aggregate.AggregateSample1.MethodSyntaxExample();
             
-            Aggregate.AggregateSample2.MethodSyntaxExample();
-            
-            Aggregate.CountSample1.MethodSyntaxExample();
-            
-            Aggregate.CountSample1.QuerySyntaxExample();
-            
-        }
+        Aggregate.AggregateSample2.MethodSyntaxExample();
+
+        Aggregate.CountSample1.MethodSyntaxExample();
+
+        Aggregate.CountSample1.QuerySyntaxExample();   
     }
+}

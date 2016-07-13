6128fc1cfc7de25440bd07a379e46b9086b939a1
dendeli@microsoft.com <dendeli@microsoft.com>
2016-07-13 15:20:09 -0700

    :fire: Adding CI scripts for samples.

---
+++ ci-scripts/itbuild.ps1
@@ -0,0 +1,18 @@
+$HomePath = (Get-Item -Path ".\" -Verbose).FullName
+
+$Content = Get-Content "$HomePath\test.txt" | Foreach-Object {
+    if ($_) {
+        $Folder = (Get-Item $_.ToString().Trim()).Directory.ToString()
+        Write-Host "Working on $Folder..."
+        $CustomCommand = "dotnet --version; `$core = Get-ChildItem Env:path;Write-Host `$path.Value;`$pathValue = `$core.Value -Replace 'C:\\Program Files\\dotnet','C:\\dotnet';Write-Host `$pathValue;`$env:Path = `$pathValue;dotnet --version;cd $Folder `| dotnet restore `| dotnet build"
+        
+        powershell.exe -Command $CustomCommand
+        if ($LastExitCode) {
+            Write-Warning "Build for project failed."
+        }
+        else
+        {
+            Write-Host "Build for project OK."
+        }
+    }
+}

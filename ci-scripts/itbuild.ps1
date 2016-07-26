dd75b061c53c87432e7b9af830dfdd1510c7698b
dendeli@microsoft.com <dendeli@microsoft.com>
2016-07-26 15:53:14 -0700

    :fire_engine: [ci] - testing re-deploy to 1.

--- ci-scripts/itbuild.ps1
+++ ci-scripts/itbuild.ps1
@@ -4,7 +4,7 @@ $Content = Get-Content "$HomePath\test.txt" | Foreach-Object {
     if ($_) {
         $Folder = (Get-Item $_.ToString().Trim()).Directory.ToString()
         Write-Host "Working on $Folder..."
-        $CustomCommand = "dotnet --version; `$core = Get-ChildItem Env:path;Write-Host `$path.Value;`$pathValue = `$core.Value -Replace 'C:\\Program Files\\dotnet','C:\\dotnet';Write-Host `$pathValue;`$env:Path = `$pathValue;dotnet --version;cd $Folder `| dotnet restore `| dotnet build 2>&1"
+        $CustomCommand = "dotnet --version; `$core = Get-ChildItem Env:path;Write-Host `$path.Value;`$pathValue = `$core.Value -Replace 'C:\\Program Files\\dotnet','C:\\dotnet';Write-Host `$pathValue;`$env:Path = `$pathValue;dotnet --version;cd $Folder `| dotnet restore 2>&1 `| dotnet build 2>&1"
         
         powershell.exe -Command $CustomCommand
         if ($LastExitCode) {

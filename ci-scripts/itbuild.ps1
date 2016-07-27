fcdd28122929c125cc99dcdcb06883767d1aeb9c
dendeli@microsoft.com <dendeli@microsoft.com>
2016-07-27 12:04:57 -0700

    :fire: [ci] Tracking brutal failures.

--- ci-scripts/itbuild.ps1
+++ ci-scripts/itbuild.ps1
@@ -1,15 +1,22 @@
 $HomePath = (Get-Item -Path ".\" -Verbose).FullName
 
+$buildResults = New-Object 'System.Collections.Generic.Dictionary[String,String]'
+
 $Content = Get-Content "$HomePath\test.txt" | Foreach-Object {
     if ($_) {
+
         $Folder = (Get-Item $_.ToString().Trim()).Directory.ToString()
         Write-Host "Working on $Folder..."
+
         $CustomCommand = "dotnet --version; `$core = Get-ChildItem Env:path;Write-Host `$path.Value;`$pathValue = `$core.Value -Replace 'C:\\Program Files\\dotnet','C:\\dotnet';Write-Host `$pathValue;`$env:Path = `$pathValue;dotnet --version;cd $Folder `| dotnet restore `| dotnet build"
         
         powershell.exe -Command $CustomCommand
 
-        Write-Host "Exited with EXCODE: " + $LastExitCode
-        
+        Write-Host "Exited with EXCODE: " $LastExitCode
+
+        # Add the current build result to the dictionary that tracks the overall success.
+        $buildResults.Add($Folder, $LastExitCode)
+
         if ($LastExitCode) {
             Write-Warning "Build for project failed."
         }
@@ -20,4 +27,15 @@ $Content = Get-Content "$HomePath\test.txt" | Foreach-Object {
     }
 }
 
-exit 0
+$brutalFailures = $buildResults | where {$_.Value -eq "1"}
+$numberOfBrutalFailures = $brutalFailures.Count
+
+Write-Host "Number of brutal failures in this build: " $numberOfBrutalFailures
+
+if ($numberOfBrutalFailures > 0)
+{
+    exit 1
+}
+else {
+    exit 0
+}

66a016025c96e30523b79383950f0b1cf962353b
dendeli@microsoft.com <dendeli@microsoft.com>
2016-07-27 13:43:58 -0700

    :fire_engine: [ci] getting a list of global projects.

--- ci-scripts/getlist.ps1
+++ ci-scripts/getlist.ps1
@@ -1,3 +1,14 @@
+## Script that gets the list of projects that need to be built.
+## Spec reference:https://microsoft.sharepoint.com/teams/CE_CSI/_layouts/OneNote.aspx?id=%2Fteams%2FCE_CSI%2FSiteAssets%2FCE_CSI%20Notebook&wd=target%28Samples%20CI%2FGeneral%20Architecture.one%7CAF430CFB-930B-4949-BD23-198A8485E1C9%2FSpec%7CB6587481-E481-450D-BDF2-2C2E2C2E70B3%2F%29
+## This script it used by the VSTS build agents.
+## Author: Den Delimarsky (dendeli)
+## Last Modified: 7/27/2016
+
 $HomePath = (Get-Item -Path ".\" -Verbose).FullName
+
+$globalProjects = Get-ChildItem $HomePath -Recurse | where {$_.Name -eq "global.json"}
+
+$globalProjects | Format-Table FullName | Write-Host
+
 $FullOutput = Get-ChildItem $HomePath -Recurse | where {$_.Name -eq "project.json"}
 $FullOutput | Format-Table FullName -HideTableHeaders | Out-File single.projects
--- ci-scripts/itbuild.ps1
+++ ci-scripts/itbuild.ps1
@@ -1,4 +1,4 @@
-## Script that iteratively builds the samples in the repository that are single projects (aka one project.json file)
+## Script that iteratively builds the samples in the repository
 ## Spec reference:https://microsoft.sharepoint.com/teams/CE_CSI/_layouts/OneNote.aspx?id=%2Fteams%2FCE_CSI%2FSiteAssets%2FCE_CSI%20Notebook&wd=target%28Samples%20CI%2FGeneral%20Architecture.one%7CAF430CFB-930B-4949-BD23-198A8485E1C9%2FSpec%7CB6587481-E481-450D-BDF2-2C2E2C2E70B3%2F%29
 ## This script it used by the VSTS build agents.
 ## Author: Den Delimarsky (dendeli)

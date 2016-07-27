0b80d400632abb5fd25546ccc5ef4d4e87c79aa9
dendeli@microsoft.com <dendeli@microsoft.com>
2016-07-27 16:25:09 -0700

    :fire_engine: [ci] global project build.

--- ci-scripts/itbuild.ps1
+++ ci-scripts/itbuild.ps1
@@ -8,6 +8,10 @@ $HomePath = (Get-Item -Path ".\" -Verbose).FullName
 
 $buildResults = @{}
 
+## =============================================
+## Single Projects
+## =============================================
+Write-Host "Bootstraping build for single projects..."
 $Content = Get-Content "$HomePath\single.projects" | Foreach-Object {
     if ($_) {
 
@@ -33,10 +37,43 @@ $Content = Get-Content "$HomePath\single.projects" | Foreach-Object {
     }
 }
 
-Write-Host "Total samples built: " $buildResults.Count
+Write-Host "Total samples built by now: " $buildResults.Count
+Write-Host "Building of single projects is complete."
 
 ## Obviously the color does nothing when this shows up in the VSTS console.
 Write-Host ($buildResults | Out-String) -ForegroundColor Yellow
+
+## =============================================
+## Global Projects
+## =============================================
+Write-Host "Bootstraping build for global projects..."
+$Content = Get-Content "$HomePath\global.projects" | Foreach-Object {
+    if ($_) {
+
+        $Folder = (Get-Item $_.ToString().Trim()).Directory.ToString()
+        Write-Host "Working on $Folder..."
+
+        $CustomCommand = "dotnet --version; `$core = Get-ChildItem Env:path;Write-Host `$path.Value;`$pathValue = `$core.Value -Replace 'C:\\Program Files\\dotnet','C:\\dotnet';Write-Host `$pathValue;`$env:Path = `$pathValue;dotnet --version;cd $Folder `| dotnet restore 2>&1 `| Write-Host `| dotnet build 2>&1 `| Write-Host "
+        
+        powershell.exe -Command $CustomCommand
+
+        Write-Host "Exited with EXCODE: " $LastExitCode
+
+        ## Add the current build result to the dictionary that tracks the overall success.
+        $buildResults.Add($Folder, $LastExitCode)
+
+        if ($LastExitCode) {
+            Write-Warning "Build for project failed."
+        }
+        else
+        {
+            Write-Host "Build for project OK."
+        }
+    }
+}
+
+Write-Host "Total samples built by now: " $buildResults.Count
+Write-Host "Building of global projects is complete."
 
 $brutalFailures = @($buildResults.GetEnumerator())| where {$_.Value -eq 1}
 $numberOfBrutalFailures = $brutalFailures.Count

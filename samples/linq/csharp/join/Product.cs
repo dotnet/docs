eb6144543f6f37c4ce13a371b7ed09b99601eb1c
dendeli@microsoft.com <dendeli@microsoft.com>
2016-07-28 15:07:40 -0700

    :sailboat: Reverting sample changes back.

--- samples/linq/csharp/join/Product.cs
+++ samples/linq/csharp/join/Product.cs
@@ -3,9 +3,9 @@ namespace Join
     public class Product
     {
         public int ProductId { get; set; }
-        public string ProductName { get; set
+        public string ProductName { get; set; }
         public string Category { get; set; }
         public decimal UnitPrice { get; set; }
-        public int UnitsInStock { get; set
+        public int UnitsInStock { get; set; }
     }
 }

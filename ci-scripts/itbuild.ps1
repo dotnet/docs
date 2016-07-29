9768efef01eaabaf1df4983544582b53a6394d11
dendeli@microsoft.com <dendeli@microsoft.com>
2016-07-29 11:26:34 -0700

    :fire_engine: [ci] logging to file.

--- ci-scripts/itbuild.ps1
+++ ci-scripts/itbuild.ps1
@@ -2,16 +2,24 @@
 ## Spec reference:https://microsoft.sharepoint.com/teams/CE_CSI/_layouts/OneNote.aspx?id=%2Fteams%2FCE_CSI%2FSiteAssets%2FCE_CSI%20Notebook&wd=target%28Samples%20CI%2FGeneral%20Architecture.one%7CAF430CFB-930B-4949-BD23-198A8485E1C9%2FSpec%7CB6587481-E481-450D-BDF2-2C2E2C2E70B3%2F%29
 ## This script it used by the VSTS build agents.
 ## Author: Den Delimarsky (dendeli)
-## Last Modified: 7/28/2016
+## Last Modified: 7/29/2016
 
 ## This is needed for JSON parsing
 [System.Reflection.Assembly]::LoadWithPartialName("System.Web.Extensions")
 
-$HomePath = (Get-Item -Path ".\" -Verbose).FullName
+$homePath = (Get-Item -Path ".\" -Verbose).FullName
 
-get-host
+$logIdentifier = [Guid]::NewGuid().ToString()
+$logFile = "$homePath\$logIdentifier.txt"
 
 $buildResults = @{}
+
+Function LogWrite
+{
+   Param ([string]$logString)
+   Write-Host $logString
+   Add-content $logFile -value $logString
+}
 
 Function ProcessBuildCommand ($command, $activePath)
 {
@@ -27,17 +35,18 @@ Function ProcessBuildCommand ($command, $activePath)
     $p.WaitForExit()
     $stdout = $p.StandardOutput.ReadToEnd()
     $stderr = $p.StandardError.ReadToEnd()
-    Write-Host "OUT: $stdout"
-    Write-Host "ERROR: $stderr"
-    Write-Host "EXCODE: "$p.ExitCode
+    
+    LogWrite "OUT: $stdout"
+    LogWrite "ERROR: $stderr"
+    LogWrite "EXCODE: "$p.ExitCode
 
     if ($p.ExitCode) 
     {
-        Write-Host "[][$activePath] Failure with current operation."
+        LogWrite "[][$activePath] Failure with current operation."
     }
     else
     {
-        Write-Host "[][$activePath] Operation succeeded."
+        LogWrite "[][$activePath] Operation succeeded."
     }
 
     ## Add the current build result to the dictionary that tracks the overall success.
@@ -47,12 +56,14 @@ Function ProcessBuildCommand ($command, $activePath)
 ## =============================================
 ## Global Projects
 ## =============================================
-Write-Host "===== Bootstraping build for global projects... ====="
-$Content = Get-Content "$HomePath\global.projects" | Foreach-Object {
+
+LogWrite "===== Bootstraping build for global projects... ====="
+
+$Content = Get-Content "$homePath\global.projects" | Foreach-Object {
     if ($_) {
 
         $restorePath = (Get-Item $_.ToString().Trim()).Directory.ToString()
-        Write-Host "Working on $restorePath..."
+        LogWrite "Working on $restorePath..."
 
         $rawJson = Get-Content $_
 
@@ -92,18 +103,18 @@ $Content = Get-Content "$HomePath\global.projects" | Foreach-Object {
     }
 }
 
-Write-Host "Total samples built by now: " $buildResults.Count
-Write-Host "===== Building of global projects is complete. ====="
+LogWrite "Total samples built by now: " $buildResults.Count
+LogWrite "===== Building of global projects is complete. ====="
 
 ## =============================================
 ## Single Projects
 ## =============================================
-Write-Host "===== Bootstraping build for single projects... ====="
-$Content = Get-Content "$HomePath\single.projects" | Foreach-Object {
+LogWrite "===== Bootstraping build for single projects... ====="
+$Content = Get-Content "$homePath\single.projects" | Foreach-Object {
     if ($_) {
 
         $projectPath = (Get-Item $_.ToString().Trim()).Directory.ToString()
-        Write-Host "Working on $projectPath..."
+        LogWrite "Working on $projectPath..."
 
         $CustomCommand = "dotnet --version; `$core = Get-ChildItem Env:path;Write-Host `$path.Value;`$pathValue = `$core.Value -Replace 'C:\\Program Files\\dotnet','C:\\dotnet';Write-Host `$pathValue;`$env:Path = `$pathValue;dotnet --version;cd $projectPath `| dotnet restore `| dotnet build "
         
@@ -111,22 +122,22 @@ $Content = Get-Content "$HomePath\single.projects" | Foreach-Object {
     }
 }
 
-Write-Host "Total samples built by now: " $buildResults.Count
-Write-Host "===== Building of single projects is complete. ====="
+LogWrite "Total samples built by now: " $buildResults.Count
+LogWrite "===== Building of single projects is complete. ====="
 
 ## Obviously the color does nothing when this shows up in the VSTS console.
-Write-Host ($buildResults | Out-String) -ForegroundColor Yellow
+LogWrite ($buildResults | Out-String) -ForegroundColor Yellow
 
 $brutalFailures = @($buildResults.GetEnumerator())| where {$_.Value -eq 1}
 $numberOfBrutalFailures = $brutalFailures.Count
 
-Write-Host "Number of brutal failures in this build: " $numberOfBrutalFailures
+LogWrite "Number of brutal failures in this build: " $numberOfBrutalFailures
 
 ## Check if we have any breaking errors - currently warnings are ignored as those do
 ## not impede the overall sample performance. Those are still logged.
 if ($numberOfBrutalFailures -gt 0)
 {
-    Write-Error "Build failed. See log for details."
+    LogWrite "Build failed. See log for details."
     exit 1
 }
 else 

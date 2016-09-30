6b9ae37f4053d292c21b02f9e7f93ccd32a78976
Bill Wagner <wiwagn@microsoft.com>
2016-09-30 18:58:03 -0400

    Running aspnet mvc in docker (#1090)
    * Update TOC for Docker
    
    This includes placeholders for this week’s deliverables.
    
    * respond to feedback
    
    Update metadata
    fix TOC
    Update the framework index
    
    * update per feedback
    
    * Updated language for .NET Framework
    
    * add F#
    
    * update TOC
    
    * file:new project.
    
    * interim checkin
    
    Thinking through the way I want this to work.
    
    * build the sample.
    
    * ignore the sample publish artifacts.
    
    * massive rename
    
    * try a global rename
    
    * reset on the sample
    
    * file new project
    
    * First draft of topic done.
    
    * proofread and update.
    
    * Finish the sample
    
    * Address feedback
    
    Bonus:
    Add note about VPN software.
    Add instructions to stop the container.
    
    * Respond to PR feedback.
    
    All existing comments have been addressed.
    
    * update publish location
    
    * respond to feedback.
    
    * add note for Windows Server installation
    
    * address 2nd round of feedback.
    
    * update source image in the docker file.
    
    * add the table marker for the metadata.

---
+++ samples/framework/docker/MVCRandomAnswerGenerator/MVCRandomAnswerGenerator/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using MVCRandomAnswerGenerator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCRandomAnswerGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<QuestionAndAnswer> allAnswers = new List<QuestionAndAnswer>();
+
+    public ActionResult Index()
+        {
+            // Ask a question.
+            // Return with the answer.
+            return View(allAnswers);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(string NextQuestion)
+        {
+            var latest = new QuestionAndAnswer
+            {
+                Question = NextQuestion,
+                Answer = AnswerGenerator.GenerateAnswer(NextQuestion)
+            };
+
+            allAnswers.Insert(0,latest);
+
+            return View(allAnswers);
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "The ASP.NET MVC Random Answer Generator";
+
+            return View();
+        }
+    }
+}

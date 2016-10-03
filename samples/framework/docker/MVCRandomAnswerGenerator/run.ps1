ae1713d8abaf9cc9efef1a5f9aa67f7659935c72
Michael Friis <friism@gmail.com>
2016-10-03 13:06:33 -0700

    Don't rely on `docker exec` for getting ip address

--- samples/framework/docker/MVCRandomAnswerGenerator/run.ps1
+++ samples/framework/docker/MVCRandomAnswerGenerator/run.ps1
@@ -2,5 +2,5 @@ docker build -t mvcrandomanswers .
 
 docker images
 docker run -d -p 8000:8000 --name randomanswers mvcrandomanswers
-docker exec randomanswers ipconfig
+docker inspect -f "{{ .NetworkSettings.Networks.nat.IPAddress }}" randomanswers
 

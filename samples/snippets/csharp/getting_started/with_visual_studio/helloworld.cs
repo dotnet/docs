193e1839634d2f9b5dc7a4c5e18e3005e266f735
rpetrusha <ronpet@microsoft.com>
2016-10-26 12:28:29 -0700

    Hello World with Visual Studio 2015

---
+++ samples/snippets/csharp/getting_started/with_visual_studio/helloworld.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class HelloWorld
+    {
+        public static void Main(string[] args)
+        {
+            // <Snippet1>
+            Console.WriteLine("\nWhat is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine("\nHello, {0}, on {1:d} at {1:t}", name, date);
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
+            // </Snippet1>
+        }
+    }
+}

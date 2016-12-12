9988a3e632c8909852417c67b111236b0789aa98
Banani-Rath <v-barath@microsoft.com>
2016-12-12 15:39:00 -0800

    Update .openpublishing.build.ps1

--- .openpublishing.build.ps1
+++ .openpublishing.build.ps1
@@ -1,5 +1,5 @@
 param(
-    [string]$buildCorePowershellUrl = "https://opbuildstoragesandbox2.blob.core.windows.net/opps1container/.openpublishing.buildcore.ps1",
+    [string]$buildCorePowershellUrl = "https://opbuildstorageprod.blob.core.windows.net/opps1container/.openpublishing.buildcore.ps1",
     [string]$parameters
 )
 # Main

c954064337e3e77cbd09e61c64aa67a3c450743a
Petr Onderka <gsvick@gmail.com>
2017-01-06 17:27:09 +0100

    Fixed C# operator titles (#1388)
    * Fixed C# operator titles
    
    * Fixed links after renaming articles

--- docs/csharp/language-reference/operators/codesnippet/CSharp/subtraction-assignment-operator_1.cs
+++ docs/csharp/language-reference/operators/codesnippet/CSharp/subtraction-assignment-operator_1.cs
@@ -1,17 +1,13 @@
-    class MainClass2
+    class MainClass3
     {
         static void Main()
         {
             int a = 5;
-            a /= 6;
+            a -= 6;
             Console.WriteLine(a);
-            double b = 5;
-            b /= 6;
-            Console.WriteLine(b);
         }
     }
     /*
     Output:
-    0
-    0.833333333333333
+    -1
     */

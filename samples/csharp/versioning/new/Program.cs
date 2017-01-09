6308e17539e5388e887a728870902b456ed9ebe7
Toni Solarin-Sodara <toni.edward@outlook.com>
2017-01-09 18:18:06 +0100

    C# Versioning (#1197)
    * add article outline
    
    * flesh out content for virtual modifier
    
    * flesh out content for override modifier
    
    * flesh out content for new modifier
    
    * add documentation metadata
    
    * complete second draft based on PR feedback
    
    * add versioning to toc
    
    * include code samples
    
    * fix broken SemVer link
    
    * address PR feedback
    
    * address PR feedback
    
    * address PR feedback
    
    * address PR feedback

---
+++ samples/csharp/versioning/new/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+#region sample
+    public class BaseClass
+    {
+        public void MyMethod()
+        {
+            Console.WriteLine("A base method");
+        }
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        public new void MyMethod()
+        {
+            Console.WriteLine("A derived method");
+        }
+    }
+
+    public static void Main()
+    {
+        BaseClass b = new BaseClass();
+        DerivedClass d = new DerivedClass();
+
+        b.MyMethod();
+        d.MyMethod();
+    }
+#endregion
+}

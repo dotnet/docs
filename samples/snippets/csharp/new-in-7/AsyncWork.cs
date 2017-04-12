26fc52be2ab8c363510e57221720293e95aebe92
Philippe Vlérick <pvlerick@gmail.com>
2017-04-12 08:44:35 +0200

    Fixed thread safety for the ValueTask example
    The ```cache = true``` line should be after the ```cacheResult = 100``` line, otherwise it is not thread safe.
    Also fixed method's name casing.

--- samples/snippets/csharp/new-in-7/AsyncWork.cs
+++ samples/snippets/csharp/new-in-7/AsyncWork.cs
@@ -77,12 +77,12 @@ namespace new_in_7
         }
         private bool cache = false;
         private int cacheResult;
-        private async Task<int> loadCache()
+        private async Task<int> LoadCache()
         {
             // simulate async work:
             await Task.Delay(100);
-            cache = true;
             cacheResult = 100;
+            cache = true;
             return cacheResult;
         }
         #endregion

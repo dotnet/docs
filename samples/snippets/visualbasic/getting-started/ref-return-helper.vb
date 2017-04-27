922a28318c8fba5c6202e5d91d926817017dcd0a
Ron Petrusha <ronpet@microsoft.com>
2017-04-27 13:58:02 -0700

    More what's new for VB

---
+++ samples/snippets/visualbasic/getting-started/ref-return-helper.vb
@@ -0,0 +1,31 @@
+﻿Imports Sentence
+
+' <Snippet1>
+Module Example
+    Dim sentence As Sentence
+
+    Public Sub Main()
+        sentence = New Sentence("A time to see the world is now.")
+        Dim original = sentence.GetSentence()
+        Dim found = False
+        Dim returns = RefHelper(sentence.FindNext("A", found), "A good", found)
+        If returns.Item2 Then
+            Console.WriteLine($"Changed '{original}' to '{sentence.GetSentence()}'")
+        Else
+            Console.WriteLine($"'{returns.Item1}' not found in '{sentence.GetSentence()}'")
+        End If
+        Console.ReadLine()
+    End Sub
+
+    Private Function RefHelper(ByRef stringFound As String, replacement As String, success As Boolean) _
+                    As (String, Boolean)
+        Dim originalString = stringFound
+        If success Then stringFound = replacement
+        Return (originalString, success)
+    End Function
+End Module
+' The example displays the following output:
+'         Changed 'A time to see the world is now.' to 'A good time to see the world is now.'
+' </Snippet1>
+
+

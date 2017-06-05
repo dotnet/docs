12273c29148ca3e5b6947678820ccac6c6a5826a
Den Delimarsky <ddelimarsky@live.com>
2017-06-04 18:42:27 -0700

    :package: Script tweak

--- ci-scripts/uploadtoazure.sh
+++ ci-scripts/uploadtoazure.sh
@@ -1,14 +1,11 @@
 export AZURE_STORAGE_CONNECTION_STRING=$AZURESCS
 
-CONTAINER_NAME="constructors-images"
+CONTAINER_NAME=constructors-images
 SOURCE_FOLDER=/buildimages/*
 
 az storage container create -n $CONTAINER_NAME
 
 for file in $SOURCE_FOLDER
 do
-  az storage blob upload \
-      --file $file \
-      --container-name $CONTAINER_NAME \
-      --name $BUILD_BUILDNUMBER
+  az storage blob upload --file $file --container-name $CONTAINER_NAME --name $BUILD_BUILDNUMBER
 done

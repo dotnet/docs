c174fd75fcd939a534e89ca85023094df12d6563
Den Delimarsky <dend@outlook.com>
2017-06-05 16:09:41 -0700

    Update preserveimage.sh

--- ci-scripts/preserveimage.sh
+++ ci-scripts/preserveimage.sh
@@ -1,4 +1,4 @@
 IMAGE="$1"
 
 mkdir "$BUILD_REPOSITORY_LOCALPATH/buildimage"
-docker save -o "$BUILD_REPOSITORY_LOCALPATH/buildimage/$BUILD_BUILDNUMBER-dotnet.tar" "$IMAGE"
+docker save -o $BUILD_REPOSITORY_LOCALPATH/buildimage/$BUILD_BUILDNUMBER-dotnet.tar "$IMAGE"

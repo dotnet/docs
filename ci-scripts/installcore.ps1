1d7067b032c59e608915e2e13c4d1e4a13aaf60e
Den Delimarsky <ddelimarsky@live.com>
2017-06-05 11:40:45 -0700

    :package: Removing old scripts.

--- ci-scripts/installcore.ps1
+++
@@ -1,3 +0,0 @@
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0-preview2/scripts/obtain/dotnet-install.ps1" -OutFile "./dotnet-install.ps1"
-
-./dotnet-install.ps1 -Version 1.0.0-preview2-003121 -InstallDir "C:\dotnet"

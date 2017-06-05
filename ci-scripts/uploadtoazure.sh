219d00b8a7a0e09ce68040cb783a136a8faf81a9
Den Delimarsky <ddelimarsky@live.com>
2017-06-05 10:37:26 -0700

    File fixes.

--- ci-scripts/uploadtoazure.sh
+++ ci-scripts/uploadtoazure.sh
@@ -1,27 +1,16 @@
 export AZURE_STORAGE_CONNECTION_STRING="$1"
 
-#config.ssh.shell = "bash -c 'BASH_ENV=/etc/profile exec bash'"
-
 CONTAINER_NAME=constructors-images
 SOURCE_FOLDER="$BUILD_REPOSITORY_LOCALPATH/buildimage/*"
-
-# Install the CLI
-# sudo npm install -g azure-cli
-
-# Reload bash
-#source ~/.profile
-
-# azure
 
 CONTAINER_LIST=$(azure storage container list)
 
 if [[ $CONTAINER_LIST == *$CONTAINER_NAME* ]]; then
   echo "It's there!"
 else
-  azure storage container create $CONTAINER_NAME
+  azure storage container create "$CONTAINER_NAME"
 fi
 
-for file in $SOURCE_FOLDER
-do
-  azure storage blob upload $file $CONTAINER_NAME $BUILD_BUILDNUMBER
+for file in $SOURCE_FOLDER; do
+  azure storage blob upload "$file" "$CONTAINER_NAME" "$BUILD_BUILDNUMBER"
 done

8599f14f7fb459727326fe08bef8fdb0d85456f1
Den Delimarsky <dend@outlook.com>
2017-06-05 16:08:31 -0700

    Update preserveimage.sh

--- ci-scripts/preserveimage.sh
+++ ci-scripts/preserveimage.sh
@@ -1,2 +1,4 @@
+IMAGE="$1"
+
 mkdir "$BUILD_REPOSITORY_LOCALPATH/buildimage"
-docker save -o "$BUILD_REPOSITORY_LOCALPATH/buildimage/$BUILD_BUILDNUMBER-dotnet.tar" "constructors.azurecr.io/platforms/netcoresdk"
+docker save -o "$BUILD_REPOSITORY_LOCALPATH/buildimage/$BUILD_BUILDNUMBER-dotnet.tar" "$IMAGE"

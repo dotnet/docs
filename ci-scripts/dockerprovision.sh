5459db98289595073b40c4441b2edc0aa971d7e6
Den Delimarsky <ddelimarsky@live.com>
2017-06-05 12:55:22 -0700

    :package: Update the container naming.

--- ci-scripts/dockerprovision.sh
+++ ci-scripts/dockerprovision.sh
@@ -16,4 +16,4 @@ docker commit builder constructors.azurecr.io/platforms/netcoresdk
 
 #docker run --name builder bash -c "for sample in $(find . -name *.csproj); do dotnet restore $sample; dotnet build $sample; done"
 
-docker run --name builder --rm constructors.azurecr.io/platforms/netcoresdk bash -c "for sample in $(find . -name *.csproj); do dotnet restore $sample; dotnet build $sample; done"
+docker run --name newbuilder --rm constructors.azurecr.io/platforms/netcoresdk bash -c "for sample in $(find . -name *.csproj); do dotnet restore $sample; dotnet build $sample; done"

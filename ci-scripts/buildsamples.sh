44dc75281647b08151d3f82adf7fa4b9e55e090c
Den Delimarsky <dend@outlook.com>
2017-06-05 16:33:39 -0700

    Update buildsamples.sh

--- ci-scripts/buildsamples.sh
+++ ci-scripts/buildsamples.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-TARGET_FOLDER=`cat buildtarget.txt`
+TARGET_FOLDER=`cat ../../buildtarget.txt`
 
 for sample in $(find $TARGET_FOLDER -name *.csproj); do dotnet restore $sample; dotnet build $sample; done

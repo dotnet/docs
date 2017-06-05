bce9e0f16a7de3ccf2ef757c0a2f93df3f02cf1a
Den Delimarsky <ddelimarsky@live.com>
2017-06-05 09:18:00 -0700

    :package: Parameters.

--- ci-scripts/uploadtoazure.sh
+++ ci-scripts/uploadtoazure.sh
@@ -1,6 +1,6 @@
-export AZURE_STORAGE_CONNECTION_STRING=$AZURESCS
+export AZURE_STORAGE_CONNECTION_STRING=$1
 
-config.ssh.shell = "bash -c 'BASH_ENV=/etc/profile exec bash'"
+#config.ssh.shell = "bash -c 'BASH_ENV=/etc/profile exec bash'"
 
 CONTAINER_NAME=constructors-images
 SOURCE_FOLDER=/buildimages/*
@@ -18,7 +18,7 @@ CONTAINER_LIST=$(azure storage container list)
 if [[ $CONTAINER_LIST == *$CONTAINER_NAME* ]]; then
   echo "It's there!"
 else
-  azure storage container create -n $CONTAINER_NAME
+  azure storage container create $CONTAINER_NAME
 fi
 
 for file in $SOURCE_FOLDER

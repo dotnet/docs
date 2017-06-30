216194a3839e765cee38374550ef157ffc5a8346
Shawn Fox <kempofighter@gmail.com>
2017-06-29 19:49:05 -0700

    Update index_5.vb
    The previous example contained no use of the ReDim statement as the preceding paragraph implied.  It was an example of a List collection type.

--- docs/visual-basic/programming-guide/language-features/arrays/codesnippet/VisualBasic/index_5.vb
+++ docs/visual-basic/programming-guide/language-features/arrays/codesnippet/VisualBasic/index_5.vb
@@ -1,4 +1,9 @@
-        Dim customers = New List(Of Customer) 
-        customers.Add(New Customer("City Power & Light", "http://www.cpandl.com/"))
-        customers.Add(New Customer("Wide World Importers", "http://www.wideworldimporters.com/"))
-        customers.Add(New Customer("Lucerne Publishing", "http://www.lucernepublishing.com/"))
+        Dim numbers(4) As Integer
+        For i = 0 To 4
+            numbers(i) = i ^ 2
+        Next
+        ' numbers contains 0, 1, 4, 9, 16
+        ReDim Preserve numbers(5)
+        ' numbers contains 0, 1, 4, 9, 16, 0
+        ReDim numbers(4)
+        ' numbers contains 0, 0, 0, 0, 0

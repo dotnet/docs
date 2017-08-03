ac11a031caf9eca78f3c81c547cd7357545932e4
Ron Petrusha <ronpet@microsoft.com>
2017-08-03 14:02:25 -0700

    Added VB debugging, publishing topics

--- samples/snippets/core/tutorials/vb-with-visual-studio/helloworld.vb
+++ samples/snippets/core/tutorials/vb-with-visual-studio/helloworld.vb
@@ -6,8 +6,8 @@ Module Program
         Console.WriteLine(vbCrLf + "What is your name? ")
         Dim name = Console.ReadLine()
         Dim currentDate = DateTime.Now
-        Console.WriteLine(vbCrLf + $"Hello, {name}, on {currentDate:d} at {currentDate:t}")
-        Console.Write(vbCrLf + "Press any key to exit...")
+        Console.WriteLine($"{vbCrLf}Hello, {name}, on {currentDate:d} at {currentDate:t}")
+        Console.Write(vbCrLf + "Press any key to exit... ")
         Console.ReadKey(True)
         ' </Snippet1>
     End Sub

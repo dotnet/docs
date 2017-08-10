6cc182311a04a9ead31a8e1f85774ea87af81e17
IvenBach <ivenbach@gmail.com>
2017-08-09 18:28:11 -0700

    Update shape.cs

--- samples/snippets/csharp/tutorials/inheritance/shape.cs
+++ samples/snippets/csharp/tutorials/inheritance/shape.cs
@@ -64,7 +64,7 @@ public class Circle : Shape
       Radius = radius;
    } 
 
-   public override double Area => Math.Round(Math.PI Math.Pow(Radius, 2), 2); 
+   public override double Area => Math.Round(Math.PI * Math.Pow(Radius, 2), 2); 
 
    public override double Perimeter => Math.Round(Math.PI * 2 * Radius, 2); 
 

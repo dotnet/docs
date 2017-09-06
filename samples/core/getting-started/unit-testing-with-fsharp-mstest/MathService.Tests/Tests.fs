02718b957973f23141bf40ea57ffe8129c48c8fe
Bill Wagner <wiwagn@microsoft.com>
2017-09-06 13:26:17 -0400

    Use AreEqual or CollectionAssert.

--- samples/core/getting-started/unit-testing-with-fsharp-mstest/MathService.Tests/Tests.fs
+++ samples/core/getting-started/unit-testing-with-fsharp-mstest/MathService.Tests/Tests.fs
@@ -14,20 +14,23 @@ type TestClass () =
     [<TestMethod>]
     member this.FailEveryTime() = Assert.IsTrue(false)
 
+    // Won't compile because AreEquivalent needs ICollection.
     [<TestMethod>]
     member this.TestEvenSequence() = 
         let expected = Seq.empty<int>
         let actual = MyMath.sumOfSquares [2; 4; 6; 8; 10]
-        Assert.Equals(expected, actual)
+        CollectionAssert.AreEquivalent(expected, actual)
 
+
+    // Fails because Assert.Equal is using reference equality.
     [<TestMethod>]
     member public this.SumOnesAndEvens() =
         let expected = [1; 1; 1; 1]
         let actual = MyMath.sumOfSquares [2; 1; 4; 1; 6; 1; 8; 1; 10]
-        Assert.Equals(expected, actual)
+        Assert.AreEqual(expected, actual)
 
     [<TestMethod>]
     member public this.TestSquaresOfOdds() =
         let expected = [1; 9; 25; 49; 81]
         let actual = MyMath.sumOfSquares [1; 2; 3; 4; 5; 6; 7; 8; 9; 10]
-        Assert.Equals(expected, actual)
+        Assert.AreEqual(expected, actual)

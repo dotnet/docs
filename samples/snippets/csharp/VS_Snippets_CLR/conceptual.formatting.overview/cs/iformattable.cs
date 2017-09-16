c6d3b496f1dc31467f0822fb8e9359533aec4a2a
Paul Rohorzka <paul@paulroho.com>
2017-09-16 06:00:59 +0200

    Code cleanup
    - Removed useless comments
    - Fixed indentation for better readability
    - Fixed annoying `temp1` variable name

--- samples/snippets/csharp/VS_Snippets_CLR/conceptual.formatting.overview/cs/iformattable.cs
+++ samples/snippets/csharp/VS_Snippets_CLR/conceptual.formatting.overview/cs/iformattable.cs
@@ -38,22 +38,19 @@ public class Temperature : IFormattable
    
    public string ToString(string format, IFormatProvider provider)  
    {
-      // Handle null or empty arguments.
-      if (String.IsNullOrEmpty(format)) format = "G";
-      // Remove any white space and convert to uppercase.
+      if (String.IsNullOrEmpty(format))
+         format = "G";
       format = format.Trim().ToUpperInvariant();
 
-      if (provider == null) provider = NumberFormatInfo.CurrentInfo;
+      if (provider == null)
+         provider = NumberFormatInfo.CurrentInfo;
             
       switch (format)
       {
-         // Convert temperature to Fahrenheit and return string.
          case "F":
             return this.Fahrenheit.ToString("N2", provider) + "°F";
-         // Convert temperature to Kelvin and return string.
          case "K":
             return this.Kelvin.ToString("N2", provider) + "K";
-         // Return temperature in Celsius.
          case "C":
          case "G":
             return this.Celsius.ToString("N2", provider) + "°C";
@@ -69,11 +66,11 @@ public class Example
 {
    public static void Main()
    {
-      Temperature temp1 = new Temperature(22m);
-      Console.WriteLine(Convert.ToString(temp1, new CultureInfo("ja-JP")));
-      Console.WriteLine("Temperature: {0:K}", temp1);
-      Console.WriteLine("Temperature: {0:F}", temp1);
-      Console.WriteLine(String.Format(new CultureInfo("fr-FR"), "Temperature: {0:F}", temp1));
+      Temperature temp = new Temperature(22m);
+      Console.WriteLine(Convert.ToString(temp, new CultureInfo("ja-JP")));
+      Console.WriteLine("Temperature: {0:K}", temp);
+      Console.WriteLine("Temperature: {0:F}", temp);
+      Console.WriteLine(String.Format(new CultureInfo("fr-FR"), "Temperature: {0:F}", temp));
    }
 }
 // The example displays the following output:

594bc8b767ba83f7038cbbcc26ae6392adfbe177
Maira Wenzel <mairaw@microsoft.com>
2017-09-22 23:10:13 -0700

    Update live with current master (#3211)
    * Typo in document - IOne instead of One (#3202)
    
    Typo in document - IOne instead of One
    
    * Fix framework in dotnet store command topic (#3188)
    
    Reported by @PauliusNorkus. Thanks to @dasmulli for reminding me about the framework problem here. Using `netstandard2.0` worked during pre-release but no more.
    
    * Modified BindingOperations.EnableCollectionSynchronization overloads (#3153)
    
    * Modified BindingOperations.EnableCollectionSynchronization overloads
    
    * Some corrections to last overload
    
    * Incorporated review comments
    
    * Addressed review comments
    
    * Fixed bad xrefs
    
    * 90785 WebClient.Credentials property is cleared on redirect (#3087)
    
    * 90785 WebClient.Credentials property is cleared on redirect
    
    * Removing "or UseDefaultCredentials"; it's also cleared during redirects.
    
    * apply feedback
    
    * Added comment about virtual methods (#3105)
    
    * Added comment about virtual methods
    
    Added a line about not overriding virtual methods because of issues involved with it.
    
    * applying feedback
    
    * Fixed: Invalid example code (#3204)
    
    * Fixed wrong example code
    
    A large chunk of the the example code in 'Listening by Registering a Callback' section had nothing to do with registering a callback. I added a small but complete example of using the CancellationToken.Register method.
    
    * Fixed wrong example code
    
    A large chunk of the the example code in 'Listening by Registering a Callback' section had nothing to do with registering a callback. I added a small but complete example of using the CancellationToken.Register method. (I submitted an equivalent change proposal for the C# example).
    
    * Problem of round-tripping Double values (#3101)
    
    * Problem of round-tripping Double values
    
    * Addressed review comments, revised Single documentation
    
    * Incorporated reviewer comments

--- samples/snippets/csharp/VS_Snippets_Misc/cancellation/cs/cancellationex4.cs
+++ samples/snippets/csharp/VS_Snippets_Misc/cancellation/cs/cancellationex4.cs
@@ -2,81 +2,35 @@
 using System;
 using System.Net;
 using System.Threading;
-using System.Threading.Tasks;
 
 class Example
 {
-   EventHandler externalEvent;
+    static void Main()
+    {
+        CancellationTokenSource cts = new CancellationTokenSource();
 
-   void Example1()
-   {
-      CancellationTokenSource cts = new CancellationTokenSource();
-      externalEvent +=
-         (sender, obj) => { cts.Cancel(); }; //wire up an external requester
-      try {
-          int val = LongRunningFunc(cts.Token);
-      }
-      catch (OperationCanceledException) {
-          //cleanup after cancellation if required...
-          Console.WriteLine("Operation was canceled as expected.");
-      }
-      finally {
-         cts.Dispose();
-      }
+        StartWebRequest(cts.Token);
 
-  }
+        // cancellation will cause the web 
+        // request to be cancelled
+        cts.Cancel();
+    }
 
-  private static int LongRunningFunc(CancellationToken token)
-  {
-      Console.WriteLine("Long running method");
-      int total = 0;
-      for (int i = 0; i < 100000; i++)
-      {
-          for (int j = 0; j < 100000; j++)
-          {
-              total++;
-          }
-          if (token.IsCancellationRequested)
-          { // observe cancellation
-              Console.WriteLine("Cancellation observed.");
-              throw new OperationCanceledException(token); // acknowledge cancellation
-          }
-      }
-      Console.WriteLine("Done looping");
-      return total;
-   }
+    static void StartWebRequest(CancellationToken token)
+    {
+        WebClient wc = new WebClient();
+        wc.DownloadStringCompleted += (s, e) => Console.WriteLine("Request completed.");
 
-   static void Main()
-   {
-      Example ex = new Example();
+        // Cancellation on the token will 
+        // call CancelAsync on the WebClient.
+        token.Register(() =>
+        {
+            wc.CancelAsync();
+            Console.WriteLine("Request cancelled!");
+        });
 
-      Thread t = new Thread(new ThreadStart(ex.Example1));
-      t.Start();
-
-      Console.WriteLine("Press 'c' to cancel.");
-      if (Console.ReadKey(true).KeyChar == 'c')
-          ex.externalEvent.Invoke(ex, new EventArgs());
-
-      Console.WriteLine("Press enter to exit.");
-      Console.ReadLine();
-  }
-}
-
-class CancelWaitHandleMiniSnippetsForOverviewTopic
-{
-
-  static void CancelByCallback()
-  {
-      CancellationTokenSource cts = new CancellationTokenSource();
-      CancellationToken token = cts.Token;
-      WebClient wc = new WebClient();
-
-      // To request cancellation on the token
-      // will call CancelAsync on the WebClient.
-      token.Register(() => wc.CancelAsync());
-
-      Console.WriteLine("Starting request");
-      wc.DownloadStringAsync(new Uri("http://www.contoso.com"));
-   }
+        Console.WriteLine("Starting request.");
+        wc.DownloadStringAsync(new Uri("http://www.contoso.com"));
+    }
 }
 //</snippet4>

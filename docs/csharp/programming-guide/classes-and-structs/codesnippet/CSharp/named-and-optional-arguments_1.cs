2e666ad08a83d94bb0d3ae4fdb4e894b628b1a83
Dhruv Garg <dhruvg@microsoft.com>
2017-10-03 01:18:42 -0700

    Change parameter name isShipped to hasShipped

--- docs/csharp/programming-guide/classes-and-structs/codesnippet/CSharp/named-and-optional-arguments_1.cs
+++ docs/csharp/programming-guide/classes-and-structs/codesnippet/CSharp/named-and-optional-arguments_1.cs
@@ -6,12 +6,12 @@
             PrintOrderDetails("Foo", 31, true);
 
             // Named arguments can be supplied for the parameters in any order.
-            PrintOrderDetails(orderNum: 31, isShipped: true, sellerName: "Foo");
-            PrintOrderDetails(isShipped: true, sellerName: "Foo", orderNum: 31);
+            PrintOrderDetails(orderNum: 31, hasShipped: true, sellerName: "Foo");
+            PrintOrderDetails(hasShipped: true, sellerName: "Foo", orderNum: 31);
 
             // Named arguments mixed with positional arguments are valid
             // as long as they are used in their correct position.
-            PrintOrderDetails("Foo", 31, isShipped: true);
+            PrintOrderDetails("Foo", 31, hasShipped: true);
             PrintOrderDetails("Foo", orderNum: 31, true);
             PrintOrderDetails(sellerName: "Foo", orderNum: 31, true);
 
@@ -22,8 +22,8 @@
             // PrintOrderDetails(31, true, sellerName: "Foo");
         }
 
-        static void PrintOrderDetails(string sellerName, int orderNum, bool isShipped)
+        static void PrintOrderDetails(string sellerName, int orderNum, bool hasShipped)
         {
-            Console.WriteLine($"Seller: {sellerName}, Order #: {orderNum}, Shipped: {isShipped}");
+            Console.WriteLine($"Seller: {sellerName}, Order #: {orderNum}, Shipped: {hasShipped}");
         }
     }

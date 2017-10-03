bbb2497c8c8f50c96b7b946ed63a2a08f49460b8
Dhruv Garg <dhruvg@microsoft.com>
2017-10-03 00:16:36 -0700

    Change example code explaining named arguments
    Since we're expanding named arguments with C# 7.2, we need to have a
    more sophisticated example to cover all the variants that are now
    allowed along with ones that are still illegal. So instead of having a
    2-parameter method, we'll have a 3-parameter method.
    
    [Issue #: 3240]

--- docs/csharp/programming-guide/classes-and-structs/codesnippet/CSharp/named-and-optional-arguments_1.cs
+++ docs/csharp/programming-guide/classes-and-structs/codesnippet/CSharp/named-and-optional-arguments_1.cs
@@ -3,22 +3,27 @@
         static void Main(string[] args)
         {
             // The method can be called in the normal way, by using positional arguments.
-            Console.WriteLine(CalculateBMI(123, 64));
+            PrintOrderDetails("Foo", 31, true);
 
-            // Named arguments can be supplied for the parameters in either order.
-            Console.WriteLine(CalculateBMI(weight: 123, height: 64));
-            Console.WriteLine(CalculateBMI(height: 64, weight: 123));
+            // Named arguments can be supplied for the parameters in any order.
+            PrintOrderDetails(orderNum: 31, isShipped: true, sellerName: "Foo");
+            PrintOrderDetails(isShipped: true, sellerName: "Foo", orderNum: 31);
 
-            // Positional arguments cannot follow named arguments.
-            // The following statement causes a compiler error.
-            //Console.WriteLine(CalculateBMI(weight: 123, 64));
+            // Named arguments mixed with positional arguments are valid
+            // as long as they are used in their correct position.
+            PrintOrderDetails("Foo", 31, isShipped: true);
+            PrintOrderDetails("Foo", orderNum: 31, true);
+            PrintOrderDetails(sellerName: "Foo", orderNum: 31, true);
 
-            // Named arguments can follow positional arguments.
-            Console.WriteLine(CalculateBMI(123, height: 64));
+            // However, mixed arguments are invalid if used out-of-order.
+            // The following statements will cause a compiler error.
+            // PrintOrderDetails(sellerName: "Foo", true, 31);
+            // PrintOrderDetails(31, sellerName: "Foo", true);
+            // PrintOrderDetails(31, true, sellerName: "Foo");
         }
 
-        static int CalculateBMI(int weight, int height)
+        static void PrintOrderDetails(string sellerName, int orderNum, bool isShipped)
         {
-            return (weight * 703) / (height * height);
+            Console.WriteLine($"Seller: {sellerName}, Order #: {orderNum}, Shipped: {isShipped}");
         }
     }

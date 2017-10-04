0918d585975fa4f15ddabcba2d7ad7c5e9109797
Dhruv Garg <dhruvg@microsoft.com>
2017-10-03 22:47:53 -0700

    Change code sample for named arguments
    Based on PR feedback, the method in the code sample has been changed
    to take in parameters that are the same type (string) to further amplify
    the need of named arguments in the documentation.

--- docs/csharp/programming-guide/classes-and-structs/codesnippet/CSharp/named-and-optional-arguments_1.cs
+++ docs/csharp/programming-guide/classes-and-structs/codesnippet/CSharp/named-and-optional-arguments_1.cs
@@ -3,27 +3,27 @@
         static void Main(string[] args)
         {
             // The method can be called in the normal way, by using positional arguments.
-            PrintOrderDetails("Foo", 31, true);
+            PrintOrderDetails("Gift Shop", 31, "Red Mug");
 
             // Named arguments can be supplied for the parameters in any order.
-            PrintOrderDetails(orderNum: 31, hasShipped: true, sellerName: "Foo");
-            PrintOrderDetails(hasShipped: true, sellerName: "Foo", orderNum: 31);
+            PrintOrderDetails(orderNum: 31, productName: "Red Mug", sellerName: "Gift Shop");
+            PrintOrderDetails(productName: "Red Mug", sellerName: "Gift Shop", orderNum: 31);
 
             // Named arguments mixed with positional arguments are valid
             // as long as they are used in their correct position.
-            PrintOrderDetails("Foo", 31, hasShipped: true);
-            PrintOrderDetails("Foo", orderNum: 31, true);
-            PrintOrderDetails(sellerName: "Foo", orderNum: 31, true);
+            PrintOrderDetails("Gift Shop", 31, productName: "Red Mug");
+            PrintOrderDetails(sellerName: "Gift Shop", 31, productName: "Red Mug");    // C# 7.2 onwards
+            PrintOrderDetails("Gift Shop", orderNum: 31, "Red Mug");                   // C# 7.2 onwards
 
             // However, mixed arguments are invalid if used out-of-order.
             // The following statements will cause a compiler error.
-            // PrintOrderDetails(sellerName: "Foo", true, 31);
-            // PrintOrderDetails(31, sellerName: "Foo", true);
-            // PrintOrderDetails(31, true, sellerName: "Foo");
+            // PrintOrderDetails(productName: "Red Mug", 31, "Gift Shop");
+            // PrintOrderDetails(31, sellerName: "Gift Shop", "Red Mug");
+            // PrintOrderDetails(31, "Red Mug", sellerName: "Gift Shop");
         }
 
-        static void PrintOrderDetails(string sellerName, int orderNum, bool hasShipped)
+        static void PrintOrderDetails(string sellerName, int orderNum, string productName)
         {
-            Console.WriteLine($"Seller: {sellerName}, Order #: {orderNum}, Shipped: {hasShipped}");
+            Console.WriteLine($"Seller: {sellerName}, Order #: {orderNum}, Product: {productName}");
         }
     }

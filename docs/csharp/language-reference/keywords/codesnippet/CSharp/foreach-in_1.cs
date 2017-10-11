c67f02d3a7853523422464019ab94ce7cfe3be56
Bill Wagner <wiwagn@microsoft.com>
2017-10-11 19:25:30 -0400

    use C# interactive in foreach
    See #3343

--- docs/csharp/language-reference/keywords/codesnippet/CSharp/foreach-in_1.cs
+++ docs/csharp/language-reference/keywords/codesnippet/CSharp/foreach-in_1.cs
@@ -8,7 +8,15 @@ class ForEachTest
             System.Console.WriteLine(element);
         }
         System.Console.WriteLine();
-
+        // Output:
+        // 0
+        // 1
+        // 1
+        // 2
+        // 3
+        // 5
+        // 8
+        // 13
 
         // Compare the previous loop to a similar for loop.
         for (int i = 0; i < fibarray.Length; i++)
@@ -16,7 +24,15 @@ class ForEachTest
             System.Console.WriteLine(fibarray[i]);
         }
         System.Console.WriteLine();
-
+        // Output:
+        // 0
+        // 1
+        // 1
+        // 2
+        // 3
+        // 5
+        // 8
+        // 13
 
         // You can maintain a count of the elements in the collection.
         int count = 0;
@@ -26,33 +42,17 @@ class ForEachTest
             System.Console.WriteLine("Element #{0}: {1}", count, element);
         }
         System.Console.WriteLine("Number of elements in the array: {0}", count);
+        // Output:
+        // Element #1: 0
+        // Element #2: 1
+        // Element #3: 1
+        // Element #4: 2
+        // Element #5: 3
+        // Element #6: 5
+        // Element #7: 8
+        // Element #8: 13
+        // Number of elements in the array: 8
     }
-    // Output:
-    // 0
-    // 1
-    // 1
-    // 2
-    // 3
-    // 5
-    // 8
-    // 13
 
-    // 0
-    // 1
-    // 1
-    // 2
-    // 3
-    // 5
-    // 8
-    // 13
 
-    // Element #1: 0
-    // Element #2: 1
-    // Element #3: 1
-    // Element #4: 2
-    // Element #5: 3
-    // Element #6: 5
-    // Element #7: 8
-    // Element #8: 13
-    // Number of elements in the array: 8
 }

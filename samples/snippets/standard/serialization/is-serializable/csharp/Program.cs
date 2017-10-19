ca16500a246959d4a9959b3b332184feed95fc10
Ron Petrusha <ronpet@microsoft.com>
2017-10-19 14:14:48 -0700

    Addressed review comments

--- samples/snippets/standard/serialization/is-serializable/csharp/Program.cs
+++ samples/snippets/standard/serialization/is-serializable/csharp/Program.cs
@@ -6,30 +6,33 @@ using Libraries;
 
 namespace test_serialization
 {
-    class Program
-    {
-        static void Main()
-        {
-            var value = ValueTuple.Create("03244562", DateTime.Now, 13452.50m);
-		    if (UtilityLibrary.IsSerializable(value))
-			{
-				// Serialize the value tuple.
+   class Program
+   {
+      static void Main()
+      {
+         var value = ValueTuple.Create("03244562", DateTime.Now, 13452.50m);
+         if (value.IsSerializable())
+         {
+            // Serialize the value tuple.
             var formatter = new BinaryFormatter();
-				var stream = new FileStream("data.bin", FileMode.Create, 
-				                            FileAccess.Write, FileShare.None);
-				formatter.Serialize(stream, value);
-				stream.Close();
+            using (var stream = new FileStream("data.bin", FileMode.Create, 
+                                        FileAccess.Write, FileShare.None))
+            {                            
+               formatter.Serialize(stream, value);
+            }
             // Deserialize the value tuple.
-            var readStream = new FileStream("data.bin", FileMode.Open);
-            var restoredValue = formatter.Deserialize(readStream);
-            Console.WriteLine($"{restoredValue.GetType().Name}: {restoredValue}");
-			}
-			else
-			{
-				Console.WriteLine($"{nameof(value)} is not serializable");
-			}
-        }
-    }
+            using (var readStream = new FileStream("data.bin", FileMode.Open))
+            {
+               object restoredValue = formatter.Deserialize(readStream);
+               Console.WriteLine($"{restoredValue.GetType().Name}: {restoredValue}");
+            }   
+         }
+         else
+         {
+            Console.WriteLine($"{nameof(value)} is not serializable");
+         }
+      }
+   }
 }
 // The example displays output like the following:
 //    ValueTuple`3: (03244562, 10/18/2017 5:25:22 PM, 13452.50)
@@ -40,9 +43,9 @@ namespace Libraries
 {
     using System;
     
-    public class UtilityLibrary
+    public static class UtilityLibrary
     {
-        public static bool IsSerializable(object obj)
+        public static bool IsSerializable(this object obj)
         {
             if (obj == null) 
                 return false;
--- samples/snippets/standard/serialization/is-serializable/vb/program.vb
+++ samples/snippets/standard/serialization/is-serializable/vb/program.vb
@@ -1,23 +1,24 @@
 ' <Snippet1>
 Imports System.IO
 Imports System.Runtime.Serialization.Formatters.Binary
-'Imports Libraries
+Imports Libraries
 
 Module Program
    Sub Main()
       Dim value = ValueTuple.Create("03244562", DateTime.Now, 13452.50d)
-      If Libraries.UtilityLibrary.IsSerializable(value) Then
-   		Dim formatter As New BinaryFormatter()
+   	If value.IsSerializable() Then
+         Dim formatter As New BinaryFormatter()
 	   	' Serialize the value tuple.
-         Dim stream As New FileStream("data.bin", FileMode.Create, 
-			                             FileAccess.Write, FileShare.None)
-			formatter.Serialize(stream, value)
-			stream.Close()
+         Using stream As New FileStream("data.bin", FileMode.Create, 
+			                               FileAccess.Write, FileShare.None)
+            formatter.Serialize(stream, value)
+			End Using
          ' Deserialize the value tuple.
-         Dim readStream As New FileStream("data.bin", FileMode.Open)
-         Dim restoredValue = formatter.Deserialize(readStream)
-         Console.WriteLine($"{restoredValue.GetType().Name}: {restoredValue}")
-		Else
+         Using readStream As New FileStream("data.bin", FileMode.Open)
+            Dim restoredValue = formatter.Deserialize(readStream)
+            Console.WriteLine($"{restoredValue.GetType().Name}: {restoredValue}")
+		   End Using
+      Else
 			Console.WriteLine($"{nameof(value)} is not serializable")
 		End If
     End Sub
@@ -26,15 +27,3 @@ End Module
 '    ValueTuple`3: (03244562, 10/18/2017 5:25:22 PM, 13452.50)
 ' </Snippet1>
 
-' <Snippet2>
-Namespace Libraries
-	Public Class UtilityLibrary
-    	Public Shared Function IsSerializable(obj As Object) As Boolean
-    		If obj Is Nothing Then Return False 
-
-			Dim t As Type = obj.GetType()
-			Return t.IsSerializable
-		End Function
-	End Class
-End Namespace
-' </Snippet2>

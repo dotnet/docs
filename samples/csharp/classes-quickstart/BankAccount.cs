60b09fb81374c394b2685416ad1858b7db7df15c
Bill Wagner <wiwagn@microsoft.com>
2017-10-25 14:18:57 -0400

    updates based on user feedback (#3509)
    * updates based on user feedback
    
    Users had two issues with the transition from dot.net to docs.
    
    1. The first step of the list quick start is repeated. I added a note to explain that.
    1. Several people already had an editor, and the instructions to install visual studio set them back. I added a not to make it more clear that you only need this if you don't already have an editor.
    
    * fix YAML syntax error
    
    whitespace     is        significant.
    
    * update per feedback.
    
    * fix typo in code sample.
    
    * respond to feedback.

--- samples/csharp/classes-quickstart/BankAccount.cs
+++ samples/csharp/classes-quickstart/BankAccount.cs
@@ -40,7 +40,7 @@ namespace classes
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
             if (amount <= 0)
-                throw new ArgumentOutOfRangeException(nameof(amount), "Amoung of deposit must be positive");
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
             var deposit = new Transaction(amount, date, note);
             allTransactions.Add(deposit);
         }
@@ -48,7 +48,7 @@ namespace classes
         public void MakeWithdrawal(decimal amount, DateTime date, string note)
         {
             if (amount <= 0)
-                throw new ArgumentOutOfRangeException(nameof(amount), "Amoung of withdrawal must be positive");
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
             if (Balance - amount < 0)
                 throw new InvalidOperationException("Not sufficient funds for this withdrawal");
             var withdrawal = new Transaction(-amount, date, note);

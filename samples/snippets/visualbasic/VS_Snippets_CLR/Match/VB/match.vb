701a11c2d5346deb704c935ee72193c0d110cbd2
Ron Petrusha <ronpet@microsoft.com>
2017-10-30 19:11:44 -0700

    Revised Equals and Match methods

--- samples/snippets/visualbasic/VS_Snippets_CLR/Match/VB/match.vb
+++ samples/snippets/visualbasic/VS_Snippets_CLR/Match/VB/match.vb
@@ -1,155 +1,120 @@
-' <Snippet1>
+Imports System.Collections.Generic
 Imports System.Reflection
-
-' A custom attribute to allow 2 authors per method.
+                                                               
+' A custom attribute to allow multiple authors per method.
 <AttributeUsage(AttributeTargets.Method)> _
-Public Class AuthorsAttribute : Inherits Attribute
-   Protected _authorName1 As String
-   Protected _authorName2 As String
-   
-   Public Sub New(name1 As String, name2 As String)
-      _authorName1 = name1
-      _authorName2 = name2
-   End Sub
-   
-   Public Property AuthorName1() As String
-      Get
-         Return _authorName1
-      End Get
-      Set
-         _authorName1 = value
-      End Set
-   End Property
-   
-   Public Property AuthorName2() As String
-      Get
-         Return _authorName2
-      End Get
-      Set
-         _authorName2 = value
-      End Set
-   End Property
-   
-   Public Overrides Function Equals(obj As Object) As Boolean
-      Dim auth As AuthorsAttribute = TryCast(obj, AuthorsAttribute)
-      If auth Is Nothing Then Return False
+Public Class AuthorsAttribute : Inherits Attribute 
+   Protected _authors As List(Of String)
 
-      Return (_authorName1 = obj.AuthorName1 And 
-              _authorName2 = obj.AuthorName2) Or
-              (_authorName1 = obj.AuthorName2 And
-              _authorName2 = obj.AuthorName1)
+	Public Sub New(paramarray names As String()) 
+      _authors = New List(Of string)(names)
+	End Sub
+
+   Public ReadOnly Property Authors As List(Of String) 
+	   Get 
+         Return _authors
+      End Get   
+   End Property
+
+	' Determine if the object is a match to this one.
+	Public Overrides Function Match(obj As Object) As Boolean 
+      ' Return false if obj is null or not an AuthorsAttribute.
+      Dim authors2 As AuthorsAttribute = DirectCast(obj, AuthorsAttribute)
+      If authors2 Is Nothing Then Return False
+      
+		' Return true if obj and this instance are the same object reference.
+      If Object.ReferenceEquals(Me, authors2) Then Return True
+
+      ' Return false if obj and this instance have different numbers of authors
+      If _authors.Count <> authors2._authors.Count Then Return False
+         
+      Dim matches As Boolean = False
+      For Each author in _authors 
+         For ctr As Integer = 0 To authors2._authors.Count - 1
+            If author = authors2._authors(ctr) Then
+               matches = True
+               Exit For
+            End If
+            If ctr = authors2._authors.Count Then matches = False
+         Next
+      Next
+      Return matches
    End Function
-   
-   ' Use the hash code of the string objects and xor them together.
-   Public Overrides Function GetHashCode() As Integer
-      Return _authorName1.GetHashCode() XOr _authorName2.GetHashCode()
-   End Function
-   
-   ' Determine if the object is a match to this one.
-   Public Overrides Function Match(ByVal obj As Object) As Boolean
-      ' Obviously a match.
-      If obj Is Me Then
-         Return True
-      End If
-   
-      ' Obviously we're not nothing, so no.
-      If obj Is Nothing Then
-         Return False
-      End If
-   
-      Dim authObj As AuthorsAttribute = TryCast(obj, AuthorsAttribute)
-      If authObj IsNot Nothing Then
-         ' Check for identical order.
-         If _authorName1 = authObj._authorName1 And
-            _authorName2 = authObj._authorName2 Then
-            Return True
-         ' Check for reversed order.
-         Else If _authorName1 = authObj._authorName2 And
-            _authorName2 = authObj._authorName1 Then
-            Return True
-         Else
-            Return False
-         End If
-      Else
-         Return False
-      End If
+
+   Public Overrides Function ToString() As String
+      Dim retval As String = ""
+      For ctr As Integer = 0 To _authors.Count -1
+         retval += $"{_authors(ctr)}{If(ctr < _authors.Count - 1, ", ", String.Empty)}"
+      Next
+      If retval.Trim().Length = 0 Then Return "<unknown>"
+
+      Return retval
    End Function
 End Class
 
 ' Add some authors to methods of a class.
-Public Class TestClass1
-   <Authors("William Shakespeare", "Herman Melville")> _
-   Public Sub Method1()
-   End Sub
+Public Class TestClass 
+	<Authors("Leo Tolstoy", "John Milton")>
+	Public sub Method1()
+	End sub
 
-   <Authors("Leo Tolstoy", "John Milton")> _
-   Public Sub Method2()
-   End Sub
+	<Authors("Anonymous")>
+	Public Sub Method2()
+	End Sub
+
+	<Authors("Leo Tolstoy", "John Milton", "Nathaniel Hawthorne")>
+	Public Sub Method3()
+	End Sub
+
+	<Authors("John Milton", "Leo Tolstoy")>
+	Public Sub Method4()
+	End Sub
 End Class
 
-' Add authors to a second class's methods.
-Public Class TestClass2
-   <Authors("William Shakespeare", "Herman Melville")> _
-   Public Sub Method1()
-   End Sub
+Public Module Example 
+	Public Sub Main() 
+		' Get the type for TestClass to access its metadata.
+		Dim clsType As Type = GetType(TestClass)
 
-   <Authors("Leo Tolstoy", "John Milton")> _
-   Public Sub Method2()
-   End Sub
-
-   <Authors("Francis Bacon", "Miguel Cervantes")> _
-   Public Sub Method3()
-   End Sub
-   
-   <Authors("John Milton", "Leo Tolstoy")> _
-   Public Sub Method4()
-   End Sub
-End Class
-
-Public Module Example
-   Sub Main()
-      ' Get the Type object for both classes.
-      Dim clsType1 As Type = GetType(TestClass1)
-      Dim clsType2 As Type = GetType(TestClass2)
-
-      ' Iterate through each method of the first class.
-      For Each method In clsType1.GetMethods()
-         ' Check each method for the Authors attribute.
-         Dim attr1 As AuthorsAttribute = CType(Attribute.GetCustomAttribute(method, 
-                                  GetType(AuthorsAttribute)), AuthorsAttribute)
-         If attr1 IsNot Nothing Then
-            Dim authAttr1 As AuthorsAttribute = 
-                             CType(attr1, AuthorsAttribute)
+		' Iterate through each method of the class.
+      Dim authors As AuthorsAttribute = Nothing
+		For Each method In clsType.GetMethods() 
+			' Check each method for the Authors attribute.
+			Dim authAttr As AuthorsAttribute = DirectCast(Attribute.GetCustomAttribute(method, 
+				                         GetType(AuthorsAttribute)), AuthorsAttribute)
+			If authAttr IsNot Nothing Then 
             ' Display the authors.
-            Console.WriteLine("{0}.{1} was authored by {2} and {3}.", 
-                              clsType1.Name, method.Name, authAttr1.AuthorName1, 
-                              authAttr1.AuthorName2)
-            ' Iterate through each method of the second class.
-            For Each method2 In clsType2.GetMethods()
-               ' Check each method for the Authors attribute.
-               Dim attr2 As AuthorsAttribute = CType(Attribute.GetCustomAttribute( 
-                                        method2, GetType(AuthorsAttribute)), 
-                                        AuthorsAttribute)
-               If attr2 IsNot Nothing Then
-                  Dim authAttr2 As AuthorsAttribute = _
-                                CType(attr2, AuthorsAttribute)
-                  ' Compare with the authors in the first class.
-                  If authAttr2.Match(authAttr1) = True Then
-                     Console.WriteLine("{0}.{1} was also authored by the same team.", 
-                                       clsType2.Name, method2.Name)
-                  End If
-               End If
-            Next
+				Console.WriteLine($"{clsType.Name}.{method.Name} was authored by {authAttr}.")
+
+            ' Select Method1's authors as the basis for comparison.
+            If method.Name = "Method1" Then
+				   authors = authAttr
+               Console.WriteLine()
+               Continue For
+            End If
+         
+   			' Compare first authors with the authors of this method.
+            If authors.Match(authAttr) Then
+					Console.WriteLine("TestClass.Method1 was also authored by the same team.")
+            End If
+
+            ' Perform an equality comparison of the two attributes.
+            Console.WriteLine($"{authors} {If(authors.Equals(authAttr), "=", "<>")} {authAttr}")
             Console.WriteLine()
-         End If
+			End If
       Next
    End Sub
 End Module
 ' The example displays the following output:
-'    TestClass1.Method1 was authored by William Shakespeare and Herman Melville.
-'    TestClass2.Method1 was also authored by the same team.
-'    
-'    TestClass1.Method2 was authored by Leo Tolstoy and John Milton.
-'    TestClass2.Method2 was also authored by the same team.
-'    TestClass2.Method4 was also authored by the same team.
-' </Snippet1>
+//       TestClass.Method1 was authored by Leo Tolstoy, John Milton.
+//       
+//       TestClass.Method2 was authored by Anonymous.
+//       Leo Tolstoy, John Milton <> Anonymous
+//       
+//       TestClass.Method3 was authored by Leo Tolstoy, John Milton, Nathaniel Hawthorne.
+//       Leo Tolstoy, John Milton <> Leo Tolstoy, John Milton, Nathaniel Hawthorne
+//       
+//       TestClass.Method4 was authored by John Milton, Leo Tolstoy.
+//       TestClass.Method1 was also authored by the same team.
+//       Leo Tolstoy, John Milton <> John Milton, Leo Tolstoy
+

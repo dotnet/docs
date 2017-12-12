bbc6d240bc97ccca7c69025170c154bde565c8f5
Bill Wagner <wiwagn@microsoft.com>
2017-12-11 20:15:46 -0500

    Update date time ref (#3799)
    * move examples section up
    
    It appears before remarks anyway, as part of the build process. Put it that way in the file to make editing easier.
    
    * tab all sample code
    
    Time for test build
    
    * fix buid errors
    
    * test build after updating samples
    
    * Create a single downloadable sample.
    
    * fix build errors
    
    * Add the last code sample
    
    * review and edit
    
    * respond to feedback
    
    * respond to feedback
    
    Fix links to sample code.
    Remove description of internal storage.
    
    * respond to feedback
    
    This commit responds to the editing feedback.
    The next commit will do the re-arranging for remarks / examples and how to present the information
    
    * test: reorder
    
    Remove the "Remarks" and "Examples" headers.
    Promote other headers 1 level.
    
    * add example section with links to anchors
    
    This provides easy navigation to all code samples.
    
    * format links to examples.
    
    Add the space before each line to get each link on a different line.
    
    * fix broken link.
    
    * fix example formatting.
    
    * trying that formatting update again
    
    And, headers named "Remarks" and "examples" are removed at build time. Put different headers in place.
    
    * fix anchor tags

---
+++ samples/snippets/csharp/System.DateTime/Calendar.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace SystemDateTimeReference
+{
+    public static class CalendarSamples
+    {
+        public static void Snippets()
+        {
+            ThaiBuddistEra();
+            ThaiBuddhistEraParse();
+            InstantiateCalendar();
+            CalendarFields();
+            CalculateWeeks();
+        }
+
+        private static void ThaiBuddistEra()
+        {
+            // <Snippet1>
+            var thTH = new System.Globalization.CultureInfo("th-TH");
+            var value = new DateTime(2016, 5, 28);
+
+            Console.WriteLine(value.ToString(thTH));
+
+            thTH.DateTimeFormat.Calendar = new System.Globalization.GregorianCalendar();
+            Console.WriteLine(value.ToString(thTH));
+            // Press "Run" to see the output
+            // </Snippet1>
+        }
+
+        private static void ThaiBuddhistEraParse()
+        {
+            // <Snippet2>
+            var thTH = new System.Globalization.CultureInfo("th-TH");
+            var value = DateTime.Parse("28/05/2559", thTH);
+            Console.WriteLine(value.ToString(thTH));
+
+            thTH.DateTimeFormat.Calendar = new System.Globalization.GregorianCalendar();
+            Console.WriteLine(value.ToString(thTH));
+            // Press "Run" to see the output
+            // </Snippet2>
+        }
+
+        private static void InstantiateCalendar()
+        {
+            // <Snippet3>
+            var thTH = new System.Globalization.CultureInfo("th-TH");
+            var dat = new DateTime(2559, 5, 28, thTH.DateTimeFormat.Calendar);
+            Console.WriteLine($"Thai Buddhist era date: {dat.ToString("d", thTH)}");
+            Console.WriteLine($"Gregorian date:   {dat:d}");
+            // Press "Run" to see the output
+            // </Snippet3>
+        }
+
+        private static void CalendarFields()
+        {
+            // <Snippet4>
+            var thTH = new System.Globalization.CultureInfo("th-TH");
+            var cal = thTH.DateTimeFormat.Calendar;
+            var dat = new DateTime(2559, 5, 28, cal);
+            Console.WriteLine("Using the Thai Buddhist Era calendar:");
+            Console.WriteLine($"Date: {dat.ToString("d", thTH)}");
+            Console.WriteLine($"Year: {cal.GetYear(dat)}");
+            Console.WriteLine($"Leap year: {cal.IsLeapYear(cal.GetYear(dat))}\n");
+
+            Console.WriteLine("Using the Gregorian calendar:");
+            Console.WriteLine($"Date: {dat:d}");
+            Console.WriteLine($"Year: {dat.Year}");
+            Console.WriteLine($"Leap year: {DateTime.IsLeapYear(dat.Year)}");
+            // Press "Run" to see the output
+            // </Snippet4>
+        }
+
+        private static void CalculateWeeks()
+        {
+            // <Snippet5>
+            var thTH = new System.Globalization.CultureInfo("th-TH");
+            var thCalendar = thTH.DateTimeFormat.Calendar;
+            var dat = new DateTime(1395, 8, 18, thCalendar);
+            Console.WriteLine("Using the Thai Buddhist Era calendar:");
+            Console.WriteLine($"Date: {dat.ToString("d", thTH)}");
+            Console.WriteLine($"Day of Week: {thCalendar.GetDayOfWeek(dat)}");
+            Console.WriteLine($"Week of year: {thCalendar.GetWeekOfYear(dat, System.Globalization.CalendarWeekRule.FirstDay, DayOfWeek.Sunday)}\n");
+
+            var greg = new System.Globalization.GregorianCalendar();
+            Console.WriteLine("Using the Gregorian calendar:");
+            Console.WriteLine($"Date: {dat:d}");
+            Console.WriteLine($"Day of Week: {dat.DayOfWeek}");
+            Console.WriteLine($"Week of year: {greg.GetWeekOfYear(dat, System.Globalization.CalendarWeekRule.FirstDay,DayOfWeek.Sunday)}");
+            // Press "Run" to see the output
+            // </Snippet5>
+        }
+    }
+}

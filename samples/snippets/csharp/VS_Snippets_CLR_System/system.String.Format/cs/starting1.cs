6dc1f65a0c091cc5ac8f0ae3d3fb24632b146f96
Ron Petrusha <ronpet@microsoft.com>
2018-01-05 13:57:15 -0800

    Address String.Format issues

--- samples/snippets/csharp/VS_Snippets_CLR_System/system.String.Format/cs/starting1.cs
+++ samples/snippets/csharp/VS_Snippets_CLR_System/system.String.Format/cs/starting1.cs
@@ -14,7 +14,6 @@ public class Example
       
       Snippet31();
       Snippet32();
-      Snippet33();
       Snippet34();
    }
 
@@ -37,26 +36,6 @@ public class Example
       // </Snippet32>
    }
 
-   private static void Snippet33()
-   {
-       // <Snippet33>
-       int[] years = { 2013, 2014, 2015 };
-       int[] population = { 1025632, 1105967, 1148203 };
-       var sb = new StringBuilder();
-       sb.Append(String.Format("{0,6} {1,15}\n\n", "Year", "Population"));
-       for (int index = 0; index < years.Length; index++)
-          sb.Append(String.Format("{0,6} {1,15:N0}\n", years[index], population[index]));
-
-       Console.WriteLine(sb);
-       // Result:
-       //      Year      Population
-       //
-       //      2013       1,025,632
-       //      2014       1,105,967
-       //      2015       1,148,203
-       // </Snippet33>
-   }
-
    private static void Snippet34()
    {
        // <Snippet34>
@@ -76,3 +55,6 @@ public class Example
        // </Snippet34>
    }
 }
+
+
+   

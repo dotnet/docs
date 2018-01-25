7297998c5016dedb4112ce8518f48efdbc82f006
Jeff Fritz <jeffreytfritz@gmail.com>
2018-01-25 10:54:03 -0500

    Added examples to System.Configuration.ConfigurationBuilder

---
+++ samples/snippets/csharp/VS_Snippets_WebNet/System.Configuration.ConfigurationBuilder/CS/SampleConfigurationBuilder.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Configuration;
+using System.Xml;
+
+namespace Samples.AspNet.Config
+{
+
+   public class SampleConfigurationBuilder : ConfigurationBuilder
+    {
+
+        public override XmlNode ProcessRawXml(XmlNode rawXml) 
+        {
+
+            string rawXmlString = rawXml.OuterXml;
+
+            if (String.IsNullOrEmpty(rawXmlString)) {
+             
+                return rawXml;
+
+            }
+
+            rawXmlString = Environment.ExpandEnvironmentVariables(rawXmlString);
+
+            XmlDocument doc = new XmlDocument();
+            doc.PreserveWhitespace = true;
+            doc.LoadXml(rawXmlString);
+            return doc.DocumentElement;
+
+        }
+
+        public override ConfigurationSection ProcessSectionInstance(ConfigurationSection configSection) 
+        {
+        
+            return configSection;
+        
+        }
+    }
+
+}

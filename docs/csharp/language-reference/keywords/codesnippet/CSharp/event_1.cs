2dbe307f2f00d0bfe80eeae4b6d58ba6aafc3e06
user135711 <31940582+user135711@users.noreply.github.com>
2018-02-12 20:58:18 -0700

    fixed race condition in C# event keyword doc
    Fixed a race condition that the other event examples already guard against.

--- docs/csharp/language-reference/keywords/codesnippet/CSharp/event_1.cs
+++ docs/csharp/language-reference/keywords/codesnippet/CSharp/event_1.cs
@@ -14,10 +14,12 @@
 
         // Wrap the event in a protected virtual method
         // to enable derived classes to raise the event.
-        protected virtual void RaiseSampleEvent()
-        {
-            // Raise the event by using the () operator.
-            if (SampleEvent != null)
-                SampleEvent(this, new SampleEventArgs("Hello"));
-        }
+    protected virtual void RaiseSampleEvent()
+    {
+        //Create a temporary reference to guard against a race condition
+        //in case the last subscriber unsubscribes causing SampleEvent to become null
+        SampleEventHandler handler = SampleEvent;
+        // Raise the event by using the () operator.
+        if (handler != null)
+            SampleEvent(this, new SampleEventArgs("Hello"));
     }

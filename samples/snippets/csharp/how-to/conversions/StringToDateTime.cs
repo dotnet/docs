17ca8293362810678d96e7975b3b311e2cd4bd94
Bill Wagner <wiwagn@microsoft.com>
2018-02-20 10:55:52 -0500

    respond to feedback.

--- samples/snippets/csharp/how-to/conversions/StringToDateTime.cs
+++ samples/snippets/csharp/how-to/conversions/StringToDateTime.cs
@@ -16,9 +16,9 @@ namespace conversions
         private static void FirstExample()
         {
             // <Snippet1>
-            string MyString = "Jan 1, 2009";
-            DateTime MyDateTime = DateTime.Parse(MyString);
-            Console.WriteLine(MyDateTime);
+            string dateInput = "Jan 1, 2009";
+            DateTime parsedDate = DateTime.Parse(dateInput);
+            Console.WriteLine(parsedDate);
             // Displays the following output on a system whose culture is en-US:
             //       1/1/2009 12:00:00 AM
             // </Snippet1>

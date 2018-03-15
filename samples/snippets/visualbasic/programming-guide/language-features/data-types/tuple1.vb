1d4436429569ac84574e5409eb1c3e5a40d50eba
John Moreno <john.planb@gmail.com>
2018-03-15 11:08:54 -0400

    Replace IIF with IF (#4665)

--- samples/snippets/visualbasic/programming-guide/language-features/data-types/tuple1.vb
+++ samples/snippets/visualbasic/programming-guide/language-features/data-types/tuple1.vb
@@ -18,14 +18,14 @@ Module Module1
         ' </Snippet1>
         ' <Snippet2>
         Console.WriteLine($"{holiday.Item1} is {holiday.Item2}" +
-                          $"{IIf(holiday.Item3, ", a national holiday", String.Empty)}")
+                          $"{If(holiday.Item3, ", a national holiday", String.Empty)}")
         ' Output: 7/4/2017 12:00:00 AM Is Independence Day, a national holiday
         ' </Snippet2>
         ' <Snippet3>
         holiday.Item1 = #01/01/2018#
         holiday.Item2 = "New Year's Day"
         Console.WriteLine($"{holiday.Item1} is {holiday.Item2}" +
-                          $"{IIf(holiday.Item3, ", a national holiday", String.Empty)}")
+                          $"{If(holiday.Item3, ", a national holiday", String.Empty)}")
         ' Output: 1/1/2018 12:00:00 AM Is New Year's Day, a national holiday
         ' </Snippet3> 
     End Sub
@@ -34,11 +34,11 @@ Module Module1
         ' <Snippet4>
         Dim holiday = (EventDate:=#07/04/2017#, Name:="Independence Day", IsHoliday:=True)
         Console.WriteLine($"{holiday.EventDate} Is {holiday.Name}" +
-                          $"{IIf(holiday.IsHoliday, ", a national holiday", String.Empty)}")
+                          $"{If(holiday.IsHoliday, ", a national holiday", String.Empty)}")
         holiday.Item1 = #01/01/2018#
         holiday.Item2 = "New Year's Day"
         Console.WriteLine($"{holiday.Item1} is {holiday.Item2}" +
-                          $"{IIf(holiday.Item3, ", a national holiday", String.Empty)}")
+                          $"{If(holiday.Item3, ", a national holiday", String.Empty)}")
         ' The example displays the following output:
         '   7/4/2017 12:00:00 AM Is Independence Day, a national holiday
         '   1/1/2018 12:00:00 AM Is New Year's Day, a national holiday

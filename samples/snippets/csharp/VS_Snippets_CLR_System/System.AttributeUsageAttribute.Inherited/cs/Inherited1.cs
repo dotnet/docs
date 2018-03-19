3667b1fb339d676166f03e83ac65e9ea0a19d51e
Bill Wagner <wiwagn@microsoft.com>
2018-03-19 10:40:40 -0400

    update for interpolated strings
    Found and updated while added the VB version

--- samples/snippets/csharp/VS_Snippets_CLR_System/System.AttributeUsageAttribute.Inherited/cs/Inherited1.cs
+++ samples/snippets/csharp/VS_Snippets_CLR_System/System.AttributeUsageAttribute.Inherited/cs/Inherited1.cs
@@ -5,49 +5,45 @@ using System.Reflection;
 [InheritedAttribute]
 public class BaseA
 {
-   [InheritedAttribute] 
-   public virtual void MethodA()   
-   {}
+    [InheritedAttribute]
+    public virtual void MethodA()
+    { }
 }
 
 public class DerivedA : BaseA
 {
-   public override void MethodA()
-   {}
-} 
+    public override void MethodA()
+    { }
+}
 
-[NotInheritedAttribute] 
+[NotInheritedAttribute]
 public class BaseB
 {
-   [NotInheritedAttribute] 
-   public virtual void MethodB()   
-   {}
+    [NotInheritedAttribute]
+    public virtual void MethodB()
+    { }
 }
 
 public class DerivedB : BaseB
 {
-   public override void MethodB()
-   {}
+    public override void MethodB()
+    { }
 }
 
 public class Example
 {
-   public static void Main()
-   {
-      Type typeA = typeof(DerivedA);
-      Console.WriteLine("DerivedA has Inherited attribute: {0}", 
-                        typeA.GetCustomAttributes(typeof(InheritedAttribute), true).Length > 0); 
-      MethodInfo memberA = typeA.GetMethod(nameof(DerivedA.MethodA));
-      Console.WriteLine("DerivedA.MemberA has Inherited attribute: {0}\n", 
-                        memberA.GetCustomAttributes(typeof(InheritedAttribute), true).Length > 0); 
-      
-      Type typeB = typeof(DerivedB);
-      Console.WriteLine("DerivedB has NotInherited attribute: {0}", 
-                        typeB.GetCustomAttributes(typeof(NotInheritedAttribute), true).Length > 0); 
-      MethodInfo memberB = typeB.GetMethod(nameof(DerivedB.MethodB));
-      Console.WriteLine("DerivedB.MemberB has NotInherited attribute: {0}", 
-                        memberB.GetCustomAttributes(typeof(NotInheritedAttribute), true).Length > 0); 
-   }
+    public static void Main()
+    {
+        Type typeA = typeof(DerivedA);
+        Console.WriteLine($"DerivedA has Inherited attribute: {typeA.GetCustomAttributes(typeof(InheritedAttribute), true).Length > 0}");
+        MethodInfo memberA = typeA.GetMethod(nameof(DerivedA.MethodA));
+        Console.WriteLine($"DerivedA.MemberA has Inherited attribute: {memberA.GetCustomAttributes(typeof(InheritedAttribute), true).Length > 0}\n");
+
+        Type typeB = typeof(DerivedB);
+        Console.WriteLine($"DerivedB has NotInherited attribute: {typeB.GetCustomAttributes(typeof(NotInheritedAttribute), true).Length > 0}");
+        MethodInfo memberB = typeB.GetMethod(nameof(DerivedB.MethodB));
+        Console.WriteLine($"DerivedB.MemberB has NotInherited attribute: {memberB.GetCustomAttributes(typeof(NotInheritedAttribute), true).Length > 0}");
+    }
 }
 // The example displays the following output:
 //       DerivedA has Inherited attribute: True
@@ -58,15 +54,15 @@ public class Example
 // </Snippet2>
 
 // <Snippet1>
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method |  
-                AttributeTargets.Property | AttributeTargets.Field, 
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method |
+                AttributeTargets.Property | AttributeTargets.Field,
                 Inherited = true)]
 public class InheritedAttribute : Attribute
-{}
+{ }
 
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method |
-                AttributeTargets.Property | AttributeTargets.Field, 
+                AttributeTargets.Property | AttributeTargets.Field,
                 Inherited = false)]
 public class NotInheritedAttribute : Attribute
-{} 
+{ }
 // </Snippet1>

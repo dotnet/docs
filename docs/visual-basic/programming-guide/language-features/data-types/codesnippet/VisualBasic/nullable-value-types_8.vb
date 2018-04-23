05968d8e92a9baa4e63514e393980ffb352bde15
Dmitry Dizhevsky <d.dizhevskiy@gmail.com>
2018-04-24 02:32:24 +0300

    Update nullable-value-types_8.vb
    Changed for readability

--- docs/visual-basic/programming-guide/language-features/data-types/codesnippet/VisualBasic/nullable-value-types_8.vb
+++ docs/visual-basic/programming-guide/language-features/data-types/codesnippet/VisualBasic/nullable-value-types_8.vb
@@ -5,5 +5,5 @@
         Dim sum2 = m + n
 
         ' Because the values of n, compare2, and sum2 are all Nothing, the
-        ' following line displays 3 * * *
+        ' following line displays: "* * *"
         Console.WriteLine(m & " * " & n & " * " & sum2 & " * " & compare2)

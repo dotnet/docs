649e5d1dbe169440a3b98de644e6f43f09ca7113
Ron Petrusha <ronpet@microsoft.com>
2018-07-16 10:34:59 -0700

    Addressed review comments

--- docs/csharp/programming-guide/exceptions/codesnippet/CSharp/using-exceptions_3.cs
+++ docs/csharp/programming-guide/exceptions/codesnippet/CSharp/using-exceptions_3.cs
@@ -1,32 +1,32 @@
-        using System;
-        using System.IO;
+using System;
+using System.IO;
 
-        public class ExceptionExample
+public class ExceptionExample
+{
+    static void Main()
+    {
+        try
         {
-            static void Main()
+            using (var sw = new StreamWriter(@"C:\test\test.txt"))
             {
-                try
-                {
-                    using(var sw = new StreamWriter(@"C:\test\test.txt"))
-                    {
-                       sw.WriteLine("Hello");
-                    }   
-                }
-                // Put the more specific exceptions first.
-                catch (DirectoryNotFoundException ex)
-                {
-                    System.Console.WriteLine(ex.ToString());  
-                }
-                catch (FileNotFoundException ex)
-                {
-                    System.Console.WriteLine(ex.ToString());  
-                }
-                // Put the least specific exception last.
-                catch (IOException ex)
-                {
-                    System.Console.WriteLine(ex.ToString());  
-                }
-
-                Console.WriteLine("Done"); 
-            }
+                sw.WriteLine("Hello");
+            }   
         }
+        // Put the more specific exceptions first.
+        catch (DirectoryNotFoundException ex)
+        {
+            Console.WriteLine(ex);  
+        }
+        catch (FileNotFoundException ex)
+        {
+            Console.WriteLine(ex);  
+        }
+        // Put the least specific exception last.
+        catch (IOException ex)
+        {
+            Console.WriteLine(ex);  
+        }
+
+        Console.WriteLine("Done"); 
+    }
+}

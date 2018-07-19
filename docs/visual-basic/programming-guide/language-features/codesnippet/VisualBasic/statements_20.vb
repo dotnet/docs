eda0065df2eabb35e6af457e7f748e355240febc
Maira Wenzel <mairaw@microsoft.com>
2018-07-19 13:35:36 -0700

    remove inner snippets

--- docs/visual-basic/programming-guide/language-features/codesnippet/VisualBasic/statements_20.vb
+++
@@ -1,5 +0,0 @@
-        Dim memoryInUse =
-          My.Computer.Info.TotalPhysicalMemory +
-          My.Computer.Info.TotalVirtualMemory -
-          My.Computer.Info.AvailablePhysicalMemory -
-          My.Computer.Info.AvailableVirtualMemory

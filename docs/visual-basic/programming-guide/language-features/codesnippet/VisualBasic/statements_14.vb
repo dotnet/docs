b038f2606c389f1468ef69a7e59c1eba0245ba81
Maira Wenzel <mairaw@microsoft.com>
2018-07-20 14:35:00 -0700

    remove inner snippets (#6560)
    * remove inner snippets
    
    * fix samples path
    
    * fix snippet reference

--- docs/visual-basic/programming-guide/language-features/codesnippet/VisualBasic/statements_14.vb
+++
@@ -1,6 +0,0 @@
-    Public Function GetUsername(ByVal username As String,
-                                ByVal delimiter As Char,
-                                ByVal position As Integer) As String
-
-        Return username.Split(delimiter)(position)
-    End Function

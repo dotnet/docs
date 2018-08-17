3588715866c4c9f914f5fbf21260879884681661
Maira Wenzel <mairaw@microsoft.com>
2018-08-17 11:08:13 -0700

    remove inner snippet (#7070)

--- docs/csharp/language-reference/keywords/codesnippet/CSharp/join-clause_3.cs
+++
@@ -1,6 +0,0 @@
-            var innerGroupJoinQuery2 =
-                from category in categories
-                join prod in products on category.ID equals prod.CategoryID into prodGroup
-                from prod2 in prodGroup
-                where prod2.UnitPrice > 2.50M
-                select prod2;

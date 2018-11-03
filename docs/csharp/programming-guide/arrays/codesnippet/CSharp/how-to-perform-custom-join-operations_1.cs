e6fb7e6f2d1b589f44d8485e4c31836a94b657a7
Daniel Chalmers <daniel.chalmers@outlook.com>
2018-11-03 13:17:28 -0500

    Update how-to-perform-custom-join-operations_1.cs

--- docs/csharp/programming-guide/arrays/codesnippet/CSharp/how-to-perform-custom-join-operations_1.cs
+++ docs/csharp/programming-guide/arrays/codesnippet/CSharp/how-to-perform-custom-join-operations_1.cs
@@ -17,25 +17,25 @@
 
                 // Specify the first data source.
                 List<Category> categories = new List<Category>()
-        { 
-            new Category(){Name="Beverages", ID=001},
-            new Category(){ Name="Condiments", ID=002},
-            new Category(){ Name="Vegetables", ID=003},         
-        };
+                {
+                    new Category { Name="Beverages", ID=001 },
+                    new Category { Name="Condiments", ID=002 },
+                    new Category { Name="Vegetables", ID=003 },
+                };
 
                 // Specify the second data source.
                 List<Product> products = new List<Product>()
-       {
-          new Product{Name="Tea",  CategoryID=001},
-          new Product{Name="Mustard", CategoryID=002},
-          new Product{Name="Pickles", CategoryID=002},
-          new Product{Name="Carrots", CategoryID=003},
-          new Product{Name="Bok Choy", CategoryID=003},
-          new Product{Name="Peaches", CategoryID=005},
-          new Product{Name="Melons", CategoryID=005},
-          new Product{Name="Ice Cream", CategoryID=007},
-          new Product{Name="Mackerel", CategoryID=012},
-        };
+                {
+                    new Product { Name="Tea",  CategoryID=001 },
+                    new Product { Name="Mustard", CategoryID=002 },
+                    new Product { Name="Pickles", CategoryID=002 },
+                    new Product { Name="Carrots", CategoryID=003 },
+                    new Product { Name="Bok Choy", CategoryID=003 },
+                    new Product { Name="Peaches", CategoryID=005 },
+                    new Product { Name="Melons", CategoryID=005 },
+                    new Product { Name="Ice Cream", CategoryID=007 },
+                    new Product { Name="Mackerel", CategoryID=012 },
+                };
                 #endregion
 
                 static void Main()
@@ -79,39 +79,39 @@
                 }
             }
             /* Output:
-        Cross Join Query:
-        1    Tea
-        1    Mustard
-        1    Pickles
-        1    Carrots
-        1    Bok Choy
-        1    Peaches
-        1    Melons
-        1    Ice Cream
-        1    Mackerel
-        2    Tea
-        2    Mustard
-        2    Pickles
-        2    Carrots
-        2    Bok Choy
-        2    Peaches
-        2    Melons
-        2    Ice Cream
-        2    Mackerel
-        3    Tea
-        3    Mustard
-        3    Pickles
-        3    Carrots
-        3    Bok Choy
-        3    Peaches
-        3    Melons
-        3    Ice Cream
-        3    Mackerel
-        Non-equijoin query:
-        1    Tea
-        2    Mustard
-        2    Pickles
-        3    Carrots
-        3    Bok Choy
-        Press any key to exit.
+            Cross Join Query:
+            1    Tea
+            1    Mustard
+            1    Pickles
+            1    Carrots
+            1    Bok Choy
+            1    Peaches
+            1    Melons
+            1    Ice Cream
+            1    Mackerel
+            2    Tea
+            2    Mustard
+            2    Pickles
+            2    Carrots
+            2    Bok Choy
+            2    Peaches
+            2    Melons
+            2    Ice Cream
+            2    Mackerel
+            3    Tea
+            3    Mustard
+            3    Pickles
+            3    Carrots
+            3    Bok Choy
+            3    Peaches
+            3    Melons
+            3    Ice Cream
+            3    Mackerel
+            Non-equijoin query:
+            1    Tea
+            2    Mustard
+            2    Pickles
+            3    Carrots
+            3    Bok Choy
+            Press any key to exit.
              */

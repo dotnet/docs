886e5c9e7267ffd07f460c8688d104ea639854c1
Daniel Chalmers <daniel.chalmers@outlook.com>
2018-11-03 16:18:09 -0500

    Format some code snippets (#8833)
    * Update how-to-initialize-a-dictionary-with-a-collection-initializer_1.cs
    
    * Update how-to-initialize-objects-by-using-an-object-initializer_2.cs
    
    * Update how-to-order-the-results-of-a-join-clause_1.cs
    
    * Update how-to-perform-custom-join-operations_1.cs
    
    * Update implicitly-typed-arrays_2.cs
    
    * Update jagged-arrays_4.cs
    
    * Update jagged-arrays_5.cs
    
    * Update auto-implemented-properties_1.cs
    
    * Update how-to-define-constants_1.cs
    
    * Update how-to-implement-and-call-a-custom-extension-method_1.cs
    
    * Update objects_1.cs
    
    * Update walkthrough-writing-queries-linq_1.cs
    
    * Update walkthrough-writing-queries-linq_1.cs
    
    * Update data-transformations-with-linq_2.cs
    
    * Update implicitly-typed-local-variables_3.cs

--- docs/csharp/programming-guide/arrays/codesnippet/CSharp/how-to-initialize-a-dictionary-with-a-collection-initializer_1.cs
+++ docs/csharp/programming-guide/arrays/codesnippet/CSharp/how-to-initialize-a-dictionary-with-a-collection-initializer_1.cs
@@ -9,8 +9,8 @@
             {
                 Dictionary<int, StudentName> students = new Dictionary<int, StudentName>()
                 {
-                    { 111, new StudentName {FirstName="Sachin", LastName="Karnik", ID=211}},
-                    { 112, new StudentName {FirstName="Dina", LastName="Salimzianova", ID=317}},
-                    { 113, new StudentName {FirstName="Andy", LastName="Ruth", ID=198}}
+                    { 111, new StudentName { FirstName="Sachin", LastName="Karnik", ID=211 } },
+                    { 112, new StudentName { FirstName="Dina", LastName="Salimzianova", ID=317 } },
+                    { 113, new StudentName { FirstName="Andy", LastName="Ruth", ID=198 } }
                 };
             }

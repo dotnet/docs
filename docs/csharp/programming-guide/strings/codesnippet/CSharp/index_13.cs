b0141086058a93604aa5448ff4e54d4068c623ae
hDoubleE <trevor.heehs@gmail.com>
2018-11-14 03:31:47 -0800

    Format String Block

---
+++ docs/csharp/programming-guide/strings/codesnippet/CSharp/index_13.cs
@@ -0,0 +1,8 @@
+int num1 = 2;
+int num2 = 4;
+
+Console.WriteLine($"If we add {num1} and {num2}, the result is {num1 + num2}");
+// Output: If we add 2 and 4, the result is 6.
+
+Console.WriteLine($"If we multiply {num1} and {num2}, the result is {num1 * num2}");
+// Output: If we multiply 2 and 4, the result is 8. 

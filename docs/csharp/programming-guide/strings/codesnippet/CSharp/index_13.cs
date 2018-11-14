ea0efef9d2b659a83e11962c502def6965371cfe
hDoubleE <trevor.heehs@gmail.com>
2018-11-14 03:38:19 -0800

    remove index_13.cs

--- docs/csharp/programming-guide/strings/codesnippet/CSharp/index_13.cs
+++
@@ -1,8 +0,0 @@
-int num1 = 2;
-int num2 = 4;
-
-Console.WriteLine($"If we add {num1} and {num2}, the result is {num1 + num2}");
-// Output: If we add 2 and 4, the result is 6.
-
-Console.WriteLine($"If we multiply {num1} and {num2}, the result is {num1 * num2}");
-// Output: If we multiply 2 and 4, the result is 8. 

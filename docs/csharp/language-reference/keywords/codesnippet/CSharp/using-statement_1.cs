7ace4e71f2a526ac45c69a146f8792c51e1cf21b
Maira Wenzel <mairaw@microsoft.com>
2018-11-28 01:50:37 -0800

    remove inner snippets + formatting

--- docs/csharp/language-reference/keywords/codesnippet/CSharp/using-statement_1.cs
+++
@@ -1,4 +0,0 @@
-        using (Font font1 = new Font("Arial", 10.0f)) 
-        {
-            byte charset = font1.GdiCharSet;
-        }
--- docs/csharp/language-reference/keywords/codesnippet/CSharp/using-statement_4.cs
+++
@@ -1,9 +0,0 @@
-            Font font2 = new Font("Arial", 10.0f);
-            using (font2) // not recommended
-            {
-                // use font2
-            }
-            // font2 is still in scope
-            // but the method call throws an exception
-            float f = font2.GetHeight(); 
-

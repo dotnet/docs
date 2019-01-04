3e1ec1872c741da3358dad3a2636a7d418f9859e
Haris Riaz <harisriaz1700@gmail.com>
2019-01-04 21:54:12 +0500

    Update unmanaged-constraint.cs

--- docs/csharp/language-reference/keywords/codesnippet/CSharp/unmanaged-constraint.cs
+++ docs/csharp/language-reference/keywords/codesnippet/CSharp/unmanaged-constraint.cs
@@ -1,4 +1,5 @@
-/*Example of a routine that does not contain a class (reference) type at any level */ 
+/* Method must be compiled in an unsafe context because it uses the sizeof operator on a type not known to be a built-in type.*/ 
+/* Without the unmanaged constraint, the sizeof operator is unavailable. */ 
 
 unsafe public static byte[] ToByteArray<T>(this T argument) where T : unmanaged
 {

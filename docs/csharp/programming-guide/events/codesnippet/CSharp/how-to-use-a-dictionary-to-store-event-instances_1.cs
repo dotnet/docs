df14665410bc949ae20434682ee6f700985ddd87
Kevinlinpr <kevinlinpr@gmail.com>
2019-01-12 05:08:31 +0800

    Solution of delegate subtraction has unpredictable
    Solution of delegate subtraction has unpredictable.

--- docs/csharp/programming-guide/events/codesnippet/CSharp/how-to-use-a-dictionary-to-store-event-instances_1.cs
+++ docs/csharp/programming-guide/events/codesnippet/CSharp/how-to-use-a-dictionary-to-store-event-instances_1.cs
@@ -1,21 +1,24 @@
-    using System;    
-    using System.Collections.Generic;    
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace ConsoleApplication1
+{
     public delegate void EventHandler1(int i);
-
     public delegate void EventHandler2(string s);
 
     public class PropertyEventsSample
     {
-        private readonly Dictionary<string, Delegate> _eventTable;
-        private readonly List<EventHandler1> _event1List = new List<EventHandler1>();
-        private readonly List<EventHandler2> _event2List = new List<EventHandler2>();
+        private readonly object _handlerStorageLock = new object();
+
+        private readonly Dictionary<string, Delegate> _handlers;
+
         public PropertyEventsSample()
         {
-            _eventTable = new Dictionary<string, Delegate>
+            _handlers = new Dictionary<string, Delegate>
             {
-                {"Event1", null},
-                {"Event2", null}
+                ["Event1"] = null,
+                ["Event2"] = null
             };
         }
 
@@ -23,23 +26,18 @@
         {
             add
             {
-                _event1List.Add(value);
-                lock (_eventTable)
+                lock (_handlerStorageLock)
                 {
-                    _eventTable["Event1"] = (EventHandler1) _eventTable["Event1"] + value;
+                    var addList = value;
+                    _handlers["Event1"] = AddOperator(_handlers["Event1"], addList);
                 }
             }
             remove
             {
-                if (!_event1List.Contains(value)) return;
-                _event1List.Remove(value);
-                lock (_eventTable)
+                lock (_handlerStorageLock)
                 {
-                    _eventTable["Event1"] = null;
-                    foreach (var event1 in _event1List)
-                    {
-                        _eventTable["Event1"] = (EventHandler1) _eventTable["Event1"] + event1;
-                    }
+                    var removeList = value;
+                    _handlers["Event1"] = RemoveOperator(_handlers["Event1"], removeList);
                 }
             }
         }
@@ -48,78 +46,150 @@
         {
             add
             {
-                _event2List.Add(value);
-                lock (_eventTable)
+                lock (_handlerStorageLock)
                 {
-                    _eventTable["Event2"] = (EventHandler2) _eventTable["Event2"] + value;
+                    var addList = value;
+                    _handlers["Event2"] = AddOperator(_handlers["Event2"], addList);
                 }
             }
             remove
             {
-                if (!_event2List.Contains(value)) return;
-                _event2List.Remove(value);
-                lock (_eventTable)
+                lock (_handlerStorageLock)
                 {
-                    _eventTable["Event2"] = null;
-                    foreach (var event2 in _event2List)
-                    {
-                        _eventTable["Event2"] = (EventHandler2) _eventTable["Event2"] + event2;
-                    }
+                    var removeList = value;
+                    _handlers["Event2"] = RemoveOperator(_handlers["Event2"], removeList);
                 }
             }
+        }
+        /// <summary>
+        /// Add addList to eventList
+        /// </summary>
+        /// <param name="eventList">the delegate list which you want to add into</param>
+        /// <param name="addList">the delegate list which you want to add</param>
+        /// <returns>Return the latest delegate list after the adding operation</returns>
+        private static Delegate AddOperator(Delegate eventList, Delegate addList)
+        {
+            return addList.GetInvocationList().Aggregate(eventList, AddEvent);
+        }
+        /// <summary>
+        /// Add each addTarget item into the eventList
+        /// </summary>
+        /// <param name="eventList">the delegate list which you want to add into</param>
+        /// <param name="addTarget">the delegate item which you want to add</param>
+        /// <returns>Return the latest delegate list after the adding operation</returns>
+        private static Delegate AddEvent(Delegate eventList, Delegate addTarget)
+        {
+            var resultList = eventList;
+            if (eventList != null)
+            {
+                if (!eventList.GetInvocationList().Contains(addTarget))
+                    resultList = Delegate.Combine(eventList, addTarget);
+            }
+            else
+                resultList = addTarget;
+            return resultList;
+        }
+        /// <summary>
+        /// Remove removeList from eventList
+        /// </summary>
+        /// <param name="eventList">the delegate list which you want to remove its item from</param>
+        /// <param name="removeList">the delegate list which you want to remove</param>
+        /// <returns>Return the latest delegate list after the removing operation</returns>
+        private static Delegate RemoveOperator(Delegate eventList, Delegate removeList)
+        {
+            return removeList.GetInvocationList().Aggregate(eventList, RemoveEvent);
+        }
+        /// <summary>
+        /// Remove each removeTarget item from the eventList
+        /// </summary>
+        /// <param name="eventList">the delegate list which you want to remove its item from</param>
+        /// <param name="removeTarget">the delegate item which you want to remove</param>
+        /// <returns>Return the latest delegate list after the removing operation</returns>
+        private static Delegate RemoveEvent(Delegate eventList, Delegate removeTarget)
+        {
+            var returnList = eventList;
+            if (eventList == null) return returnList;
+            while (true)
+            {
+                var resultList = eventList;
+                foreach (var eventItem in eventList.GetInvocationList())
+                {
+                    if (!eventItem.Equals(removeTarget)) continue;
+                    resultList = Delegate.Remove(eventList, eventItem);
+                    break;
+                }
+
+                var continueRetrieve = resultList != null && resultList.GetInvocationList().Contains(removeTarget);
+                if (continueRetrieve)
+                {
+                    eventList = resultList;
+                    continue;
+                }
+                returnList = resultList;
+                break;
+            }
+            return returnList;
         }
 
         internal void RaiseEvent1(int i)
         {
-            lock (_eventTable)
+            EventHandler1 handler1;
+            lock (_handlerStorageLock)
             {
-                var handler1 = (EventHandler1) _eventTable["Event1"];
-                handler1?.Invoke(i);
+                handler1 = (EventHandler1)_handlers["Event1"];
             }
+            handler1?.Invoke(i);
         }
 
         internal void RaiseEvent2(string s)
         {
-            lock (_eventTable)
+            EventHandler2 handler2;
+            lock (_handlerStorageLock)
             {
-                var handler2 = (EventHandler2) _eventTable["Event2"];
-                handler2?.Invoke(s);
+                handler2 = (EventHandler2)_handlers["Event2"];
             }
+            handler2?.Invoke(s);
         }
     }
 
     public static class TestClass
     {
-        private static void Delegate1Method(int i)
-        {
-            Console.WriteLine(i);
-        }
-
-        private static void Delegate2Method(string s)
-        {
-            Console.WriteLine(s);
-        }
+        private static void Delegate1Method1(int i) => Console.Write("Method1: " + i + "/");
+        private static void Delegate1Method2(int i) => Console.Write("Method2: " + i + "/");
+        private static void Delegate1Method3(int i) => Console.Write("Method3: " + i + "/");
+        private static void Delegate2Method1(string s) => Console.Write("Method1: " + s + "/");
+        private static void Delegate2Method2(string s) => Console.Write("Method2: " + s + "/");
+        private static void Delegate2Method3(string s) => Console.Write("Method3: " + s + "/");
 
         private static void Main()
         {
             var p = new PropertyEventsSample();
 
-            p.Event1 += Delegate1Method;
-            p.Event1 += Delegate1Method;
-            p.Event1 -= Delegate1Method;
-            p.RaiseEvent1(2);
+            EventHandler1 handler1Method1 = Delegate1Method1;
+            EventHandler1 handler1Method2 = Delegate1Method2;
+            EventHandler1 handler1Method3 = Delegate1Method3;
 
-            p.Event2 += Delegate2Method;
-            p.Event2 += Delegate2Method;
-            p.Event2 -= Delegate2Method;
+            EventHandler2 handler2Method1 = Delegate2Method1;
+            EventHandler2 handler2Method2 = Delegate2Method2;
+            EventHandler2 handler2Method3 = Delegate2Method3;
+
+            var combinationHandler1List = handler1Method1 + handler1Method3 + handler1Method2;
+            var combinationHandler2List = handler2Method1 + handler2Method3 + handler2Method2;
+            p.Event1 += handler1Method1;                        //Method1: 2
+            p.Event1 += combinationHandler1List;                //Method1: 2/Method3: 2/Method2: 2
+            p.Event1 += (handler1Method3 + handler1Method2);    //Method1: 2/Method3: 2/Method2: 2
+            p.Event1 -= (handler1Method2 + handler1Method3);    //Method1: 2
+            p.RaiseEvent1(2);
+            p.Event2 -= handler2Method3;                        //
+            p.Event2 += (handler2Method3 + handler2Method1);    //Method3: TestString/Method1: TestString
+            p.Event2 -= combinationHandler2List;                //
             p.RaiseEvent2("TestString");
 
-            // Keep the console window open in debug mode.
-            Console.WriteLine("Press any key to exit.");
+            Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
     }
-    /* Output:
-        2
-        TestString
-    */
+    // Output:
+    //   Method1: 2
+    //   
+}

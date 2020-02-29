f42d48a8c7f875ecdda8316e3dea2e8c75a3f528
Andy De George <thraka.junk@outlook.com>
2020-02-28 17:17:02 -0800

    Pass 1: Moved snippets for core articles

---
+++ samples/snippets/core/tutorials/creating-app-with-plugin-support/csharp/UVPlugin/UVPlugin.cs
@@ -0,0 +1,28 @@
+﻿using PluginBase;
+using System;
+using System.Runtime.InteropServices;
+
+namespace UVPlugin
+{
+    public class UVPlugin : ICommand
+    {
+        public string Name => "uv";
+
+        public string Description => "Uses the native library libuv to show its version.";
+
+        public int Execute()
+        {
+            Console.WriteLine($"Using libuv version {GetVersion()}.");
+            return 0;
+        }
+
+        [DllImport("libuv", CallingConvention = CallingConvention.Cdecl)]
+        private extern static uint uv_version();
+
+        private static Version GetVersion()
+        {
+            uint version = uv_version();
+            return new Version((int)(version & 0xFF0000) >> 16, (int)(version & 0xFF00) >> 8, (int)(version & 0xFF));
+        }
+    }
+}

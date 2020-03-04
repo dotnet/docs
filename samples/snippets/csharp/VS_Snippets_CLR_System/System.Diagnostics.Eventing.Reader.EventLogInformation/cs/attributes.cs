243fdaf6f94e727cc2e8c4bc5a691644480d1d94
Andy De George <thraka.junk@outlook.com>
2020-03-04 12:10:56 -0800

    Removed unreferenced snippets for 'samples\snippets\csharp\' PASS 6

--- samples/snippets/csharp/VS_Snippets_CLR_System/System.Diagnostics.Eventing.Reader.EventLogInformation/cs/attributes.cs
+++
@@ -1,7 +0,0 @@
-﻿// <snippet1>
-using System.Diagnostics.Eventing.Reader;
-using System.IO;
-
-EventLogInformation eventLogInformation;
-FileAttributes? fileAttributes = (FileAttributes?)eventLogInformation.Attributes;
-// </snippet1>
--- samples/snippets/csharp/VS_Snippets_CLR_System/system.resources.neutralresourceslanguageattribute/cs/example2.cs
+++
@@ -1,6 +0,0 @@
-﻿// <Snippet2>
-using System.Resources;
-
-[assembly:NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-// </Snippet2>
-public class Example {}

01c14487e6541eb149f02c381de6086a72ee3472
Andy De George <thraka.junk@outlook.com>
2020-03-04 12:50:32 -0800

    Removed unreferenced snippets for 'samples\snippets\visualbasic\' PASS 5

--- samples/snippets/visualbasic/VS_Snippets_CLR_Classic/classic ComSourceInterfacesAttribute Example/VB/source.vb
+++
@@ -1,8 +0,0 @@
-﻿' <Snippet1>
-Imports System.Runtime.InteropServices
-
-<ComSourceInterfacesAttribute("ButtonEventsLib.ButtonEvents, ButtonEventsLib")> _
-public Class Baz
-    'Insert code here.
-End Class
-' </Snippet1>

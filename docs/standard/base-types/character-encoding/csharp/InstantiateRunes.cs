b0c68c3d49d1aea991a88f40bd00548523a31ad9
Tom Dykstra <tdykstra@microsoft.com>
2020-03-17 14:17:02 -0700

    add example

--- docs/standard/base-types/character-encoding/csharp/InstantiateRunes.cs
+++ docs/standard/base-types/character-encoding/csharp/InstantiateRunes.cs
@@ -11,35 +11,37 @@ namespace RuneSamples
             // <SnippetValid>
             Rune a = new Rune('a');
             Rune b = new Rune(0x0061);
-            Rune c = new Rune(0x10421);
-            Rune d = new Rune('\ud801', '\udc21');
+            Rune c = new Rune('\u0061');
+            Rune d = new Rune(0x10421);
+            Rune e = new Rune('\ud801', '\udc21');
             // </SnippetValid>
 
             Console.WriteLine($"Rune a: {a}");
             Console.WriteLine($"Rune b: {b}");
             Console.WriteLine($"Rune c: {c}");
             Console.WriteLine($"Rune d: {d}");
+            Console.WriteLine($"Rune e: {e}");
 
             try
             {
                 // <SnippetInvalidSurrogate>
-                Rune e = new Rune('\ud801');
+                Rune f = new Rune('\ud801');
                 // </SnippetInvalidSurrogate>
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Rune e: Exception: {ex.Message}");
+                Console.WriteLine($"Rune f: Exception: {ex.Message}");
             }
 
             try
             {
                 // <SnippetInvalidHigh>
-                Rune f = new Rune(0x12345678);
+                Rune g = new Rune(0x12345678);
                 // </SnippetInvalidHigh>
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Rune f: Exception: {ex.Message}");
+                Console.WriteLine($"Rune g: Exception: {ex.Message}");
             }
         }
     }

c6d6ca239350d3f4af84eb3f250b80393b473a2f
OzBob <OzBob@users.noreply.github.com>
2020-06-04 13:51:53 +0800

    FileName can have DirectorySeparatorChar
    indenting to 4 spaces
    CreateFolders function removed, originally to cater for when the filename has a Directory Separator Char in it.

--- samples/snippets/csharp/VS_Snippets_CLR/conceptual.isolatedstorage/cs/source.cs
+++ samples/snippets/csharp/VS_Snippets_CLR/conceptual.isolatedstorage/cs/source.cs
@@ -5,52 +5,32 @@ using System.IO.IsolatedStorage;
 
 public class CreatingFilesDirectories
 {
-public static void Main()
-{
-    using (IsolatedStorageFile isoStore = IsolatedStorageFile.GetStore(IsolatedStorageScope.User | IsolatedStorageScope.Domain | IsolatedStorageScope.Assembly, null, null))
+    public static void Main()
     {
-        isoStore.CreateDirectory("TopLevelDirectory");
-        isoStore.CreateDirectory("TopLevelDirectory/SecondLevel");
-        var isofilename = "AnotherTopLevelDirectory/NewFile.txt";
-        try
+        using (IsolatedStorageFile isoStore = IsolatedStorageFile.GetStore(IsolatedStorageScope.User | IsolatedStorageScope.Domain | IsolatedStorageScope.Assembly, null, null))
         {
-            isoStore.CreateFile(isofilename);
+            isoStore.CreateDirectory("TopLevelDirectory");
+            isoStore.CreateDirectory("TopLevelDirectory/SecondLevel");
+            var isofilename = "AnotherTopLevelDirectory/NewFile.txt";
+            try
+            {
+                isoStore.CreateFile(isofilename);
+            }
+            catch (IsolatedStorageException iex)
+            {
+                Console.WriteLine(iex.ToString());
+            }
+
+            var path = Path.GetDirectoryName(isofilename);
+            isoStore.CreateDirectory(path);//Creating "AnotherTopLevelDirectory";
+            isoStore.CreateFile(isofilename);//Creating "AnotherTopLevelDirectory/NewFile.txt";
+            
+			isoStore.CreateDirectory("AnotherTopLevelDirectory/InsideDirectory");
+            isoStore.CreateFile("AnotherTopLevelDirectory/InsideDirectory/HereIAm.txt");
+
+            isoStore.CreateFile("InTheRoot.txt");
+            Console.WriteLine("Created a new file in the root.");
         }
-        catch (IsolatedStorageException iex){
-            Console.WriteLine(iex.ToString());
-        }
-
-        var path = Path.GetDirectoryName(isofilename);
-        isoStore.CreateDirectory(path);//Creating "AnotherTopLevelDirectory";
-        isoStore.CreateFile(isofilename);//Creating "AnotherTopLevelDirectory/NewFile.txt";
-        
-        isoStore.CreateDirectory("AnotherTopLevelDirectory/InsideDirectory");
-        Console.WriteLine("Created directories.");
-
-        isoStore.CreateFile("InTheRoot.txt");
-        Console.WriteLine("Created a new file in the root.");
-
-        isoStore.CreateFile("AnotherTopLevelDirectory/InsideDirectory/HereIAm.txt");
-        Console.WriteLine("Created a new file in the InsideDirectory.");
-        }
-    }
-
-    private string CreateFolders(string folderName, string fileName)
-    {
-
-        var isofilename = fileName;
-        if (string.IsNullOrEmpty(fileName)) return string.Empty;
-
-        if (!string.IsNullOrEmpty(folderName))
-        {
-            isofilename = Path.Combine(folderName, fileName);
-        }
-
-        //does path have folder?
-        var path = Path.GetDirectoryName(isofilename);
-        applicationStorageFileForUser.CreateDirectory(path);
-
-        return isofilename;
     }
 }
 //</snippet1>

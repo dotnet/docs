51f7addfd403289598373868312411d2d7a20745
Next Turn <45985406+NextTurn@users.noreply.github.com>
2020-06-11 22:42:33 +0800

    Format codes based on .editorconfig, Part 8 (#18895)
    * Format codes based on .editorconfig, Part 8
    
    * Apply suggestions from code review
    
    Co-authored-by: David Pine <david.pine@microsoft.com>
    
    Co-authored-by: David Pine <david.pine@microsoft.com>

--- samples/snippets/visualbasic/VS_Snippets_CLR/conceptual.disposable/vb/using3.vb
+++ samples/snippets/visualbasic/VS_Snippets_CLR/conceptual.disposable/vb/using3.vb
@@ -5,22 +5,22 @@ Option Strict On
 Imports System.IO
 
 Module Example
-   Public Sub Main()
-      Dim buffer(49) As Char
-''      Dim s As New StreamReader("File1.txt")
-With s As New StreamReader("File1.txt")
+    Public Sub Main()
+        Dim buffer(49) As Char
+        ''      Dim s As New StreamReader("File1.txt")
+        With s As New StreamReader("File1.txt")
       Try
-         Dim charsRead As Integer
-         Do While s.Peek() <> -1
-            charsRead = s.Read(buffer, 0, buffer.Length)         
-            ' 
-            ' Process characters read.
-            '
-         Loop
-      Finally
-         If s IsNot Nothing Then DirectCast(s, IDisposable).Dispose()
-      End Try
-End With
-   End Sub
+                Dim charsRead As Integer
+                Do While s.Peek() <> -1
+                    charsRead = s.Read(buffer, 0, buffer.Length)
+                    ' 
+                    ' Process characters read.
+                    '
+                Loop
+            Finally
+                If s IsNot Nothing Then DirectCast(s, IDisposable).Dispose()
+            End Try
+        End With
+    End Sub
 End Module
 ' </Snippet3>

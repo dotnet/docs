6eb125b1b2ade509031fd397cbb7b62d178d7951
Next Turn <45985406+NextTurn@users.noreply.github.com>
2020-06-15 23:51:36 +0800

    Format codes based on .editorconfig, Part 12 (#18941)

--- samples/snippets/visualbasic/VS_Snippets_CLR/conceptual.timespan.custom/vb/customparseexample1.vb
+++ samples/snippets/visualbasic/VS_Snippets_CLR/conceptual.timespan.custom/vb/customparseexample1.vb
@@ -3,31 +3,31 @@ Option Strict On
 
 ' <Snippet2>
 Module Example
-   Public Sub Main()
-      Dim value As String = Nothing
-      Dim interval As TimeSpan
+    Public Sub Main()
+        Dim value As String = Nothing
+        Dim interval As TimeSpan
 
-      value = "6"
-      If TimeSpan.TryParseExact(value, "%d", Nothing, interval) Then
-         Console.WriteLine("{0} --> {1}", value, interval.ToString("c"))
-      Else
-         Console.WriteLine("Unable to parse '{0}'", value)
-      End If
-      
-      value = "16:32.05"
-      If TimeSpan.TryParseExact(value, "mm\:ss\.ff", Nothing, interval) Then
-         Console.WriteLine("{0} --> {1}", value, interval.ToString("c"))
-      Else
-         Console.WriteLine("Unable to parse '{0}'", value)
-      End If
+        value = "6"
+        If TimeSpan.TryParseExact(value, "%d", Nothing, interval) Then
+            Console.WriteLine("{0} --> {1}", value, interval.ToString("c"))
+        Else
+            Console.WriteLine("Unable to parse '{0}'", value)
+        End If
 
-      value= "12.035"
-      If TimeSpan.TryParseExact(value, "ss\.fff", Nothing, interval) Then
-         Console.WriteLine("{0} --> {1}", value, interval.ToString("c"))
-      Else
-         Console.WriteLine("Unable to parse '{0}'", value)
-      End If
-   End Sub
+        value = "16:32.05"
+        If TimeSpan.TryParseExact(value, "mm\:ss\.ff", Nothing, interval) Then
+            Console.WriteLine("{0} --> {1}", value, interval.ToString("c"))
+        Else
+            Console.WriteLine("Unable to parse '{0}'", value)
+        End If
+
+        value = "12.035"
+        If TimeSpan.TryParseExact(value, "ss\.fff", Nothing, interval) Then
+            Console.WriteLine("{0} --> {1}", value, interval.ToString("c"))
+        Else
+            Console.WriteLine("Unable to parse '{0}'", value)
+        End If
+    End Sub
 End Module
 ' The example displays the following output:
 '       6 --> 6.00:00:00

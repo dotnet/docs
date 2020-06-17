a938eca540ccf5b5369c6a2af4293416d6ba7692
NextTurn <45985406+NextTurn@users.noreply.github.com>
2020-06-18 00:00:00 +0800

    Format codes based on .editorconfig, Part 19

--- samples/snippets/visualbasic/VS_Snippets_Misc/tpl_continuations/vb/attached1.vb
+++ samples/snippets/visualbasic/VS_Snippets_Misc/tpl_continuations/vb/attached1.vb
@@ -7,27 +7,27 @@ Imports System.Threading
 Imports System.Threading.Tasks
 
 Public Module Example
-   Public Sub Main()
-      Dim t = Task.Factory.StartNew( Sub()
-                                        Console.WriteLine("Running antecedent task {0}...",
-                                                          Task.CurrentId)
-                                        Console.WriteLine("Launching attached child tasks...")
-                                        For ctr As Integer = 1 To 5
-                                           Dim index As Integer = ctr
-                                           Task.Factory.StartNew( Sub(value)
-                                                                     Console.WriteLine("   Attached child task #{0} running",
-                                                                                       value)
-                                                                     Thread.Sleep(1000)
-                                                                  End Sub, index, TaskCreationOptions.AttachedToParent)
-                                        Next
-                                        Console.WriteLine("Finished launching attached child tasks...")
-                                     End Sub)
-      Dim continuation = t.ContinueWith( Sub(antecedent)
-                                            Console.WriteLine("Executing continuation of Task {0}",
-                                                              antecedent.Id)
-                                         End Sub)
-      continuation.Wait()
-   End Sub
+    Public Sub Main()
+        Dim t = Task.Factory.StartNew(Sub()
+                                          Console.WriteLine("Running antecedent task {0}...",
+                                                            Task.CurrentId)
+                                          Console.WriteLine("Launching attached child tasks...")
+                                          For ctr As Integer = 1 To 5
+                                              Dim index As Integer = ctr
+                                              Task.Factory.StartNew(Sub(value)
+                                                                        Console.WriteLine("   Attached child task #{0} running",
+                                                                                          value)
+                                                                        Thread.Sleep(1000)
+                                                                    End Sub, index, TaskCreationOptions.AttachedToParent)
+                                          Next
+                                          Console.WriteLine("Finished launching attached child tasks...")
+                                      End Sub)
+        Dim continuation = t.ContinueWith(Sub(antecedent)
+                                              Console.WriteLine("Executing continuation of Task {0}",
+                                                                antecedent.Id)
+                                          End Sub)
+        continuation.Wait()
+    End Sub
 End Module
 ' The example displays the following output:
 '       Running antecedent task 1...

8f459a1bffd34e3cc1178ce135da8230fce59b59
Next Turn <45985406+NextTurn@users.noreply.github.com>
2020-06-17 23:38:57 +0800

    Format codes based on .editorconfig, Part 17 (#18986)

--- samples/snippets/visualbasic/VS_Snippets_Data/dp l2e arraysandlistsinqueries/vb/my project/assemblyinfo.vb
+++ samples/snippets/visualbasic/VS_Snippets_Data/dp l2e arraysandlistsinqueries/vb/my project/assemblyinfo.vb
@@ -7,17 +7,17 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("L2E_ArraysAndListsInQueriesVB")> 
-<Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("Microsoft")> 
-<Assembly: AssemblyProduct("L2E_ArraysAndListsInQueriesVB")> 
-<Assembly: AssemblyCopyright("Copyright © Microsoft 2009")> 
-<Assembly: AssemblyTrademark("")> 
+<Assembly: AssemblyTitle("L2E_ArraysAndListsInQueriesVB")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Microsoft")>
+<Assembly: AssemblyProduct("L2E_ArraysAndListsInQueriesVB")>
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2009")>
+<Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("b7c87b83-43d7-4010-a57c-0284f157d65e")> 
+<Assembly: Guid("b7c87b83-43d7-4010-a57c-0284f157d65e")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>

512ee4c25279891759801a9a4701248d8eb96eb7
Andy De George <2672110+Thraka@users.noreply.github.com>
2020-06-17 19:49:00 -0700

    Add files via upload

---
+++ .github/workflows/dependencies/Out-GithubActionStatus.ps1
@@ -0,0 +1,38 @@
+<#
+
+.SYNOPSIS
+    Reads the output.json file and outputs status to GitHub Actions
+
+.DESCRIPTION
+    Reads the output.json file and outputs status to GitHub Actions
+
+.INPUTS
+    None
+
+.OUTPUTS
+    None
+
+.NOTES
+    Version:        1.0
+    Author:         adegeo@microsoft.com
+    Creation Date:  06/17/2020
+    Purpose/Change: Initial release
+#>
+
+[CmdletBinding()]
+Param(
+)
+
+$json = Get-Content output.json | ConvertFrom-Json
+
+if ((($json.ErrorCount).Count -eq 1) -and (($json.ErrorCount)[0] -eq 0)) {
+    # Good status!
+    Write-Host "::debug::All tests passed"
+}
+else {
+    $errors = $json | Where-Object ErrorCount -ne 0 | Select-Object InputFile -ExpandProperty Errors | Select-Object InputFile, Error, Line
+    
+    foreach ($er in $errors) {
+        Write-Host "::error file=$($er.InputFile)::$($er.Error) $($er.Line)"
+    }
+}

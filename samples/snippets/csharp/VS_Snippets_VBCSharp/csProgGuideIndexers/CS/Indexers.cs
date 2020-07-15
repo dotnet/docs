a45dc1654ae97925258d9a578f0ed050b73a5048
David Pine <david.pine@microsoft.com>
2020-07-15 10:53:37 -0500

    Modernize C# example a bit

--- samples/snippets/csharp/VS_Snippets_VBCSharp/csProgGuideIndexers/CS/Indexers.cs
+++ samples/snippets/csharp/VS_Snippets_VBCSharp/csProgGuideIndexers/CS/Indexers.cs
@@ -4,28 +4,22 @@
 class TempRecord
 {
     // Array of temperature values
-    private float[] temps = new float[10] { 56.2F, 56.7F, 56.5F, 56.9F, 58.8F,
-                                            61.3F, 65.9F, 62.1F, 59.2F, 57.5F };
+    private float[] temps = new float[10] 
+    {
+        56.2F, 56.7F, 56.5F, 56.9F, 58.8F,
+        61.3F, 65.9F, 62.1F, 59.2F, 57.5F
+    };
 
     // To enable client code to validate input
     // when accessing your indexer.
-    public int Length
-    {
-        get { return temps.Length; }
-    }
+    public int Length => temps.Length;
+    
     // Indexer declaration.
     // If index is out of range, the temps array will throw the exception.
     public float this[int index]
     {
-        get
-        {
-            return temps[index];
-        }
-
-        set
-        {
-            temps[index] = value;
-        }
+        get => temps[index];
+        set => temps[index] = value;
     }
 }
 
@@ -33,7 +27,8 @@ class MainClass
 {
     static void Main()
     {
-        TempRecord tempRecord = new TempRecord();
+        var tempRecord = new TempRecord();
+
         // Use the indexer's set accessor
         tempRecord[3] = 58.3F;
         tempRecord[5] = 60.1F;
@@ -41,7 +36,7 @@ class MainClass
         // Use the indexer's get accessor
         for (int i = 0; i < 10; i++)
         {
-            System.Console.WriteLine("Element #{0} = {1}", i, tempRecord[i]);
+            System.Console.WriteLine($"Element #{i} = {tempRecord[i]}");
         }
 
         // Keep the console window open in debug mode.

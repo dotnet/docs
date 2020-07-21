02cf0e3f53aa5402e824e0f507104cc0862b5016
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2020-07-21 11:58:30 +0200

    Update misleading variable name

--- samples/snippets/visualbasic/VS_Snippets_CLR/conceptual.basicio.textfiles/vb/source3.vb
+++ samples/snippets/visualbasic/VS_Snippets_CLR/conceptual.basicio.textfiles/vb/source3.vb
@@ -7,8 +7,8 @@ Class Test
             ' Open the file using a stream reader.
             Using sr As New StreamReader("TestFile.txt")
                 ' Read the stream to a string and write the string to the console.
-                Dim line = sr.ReadToEnd()
-                Console.WriteLine(line)
+                Dim fileContent = sr.ReadToEnd()
+                Console.WriteLine(fileContent)
             End Using
         Catch e As IOException
             Console.WriteLine("The file could not be read:")

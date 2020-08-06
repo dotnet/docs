f79e61db82c6b033f0336f65dc6adf4914f1398f
Bill Wagner <wiwagn@microsoft.com>
2020-08-06 15:57:47 -0400

    fix warnings.

--- docs/csharp/tour-of-csharp/snippets/shared/Program.cs
+++ docs/csharp/tour-of-csharp/snippets/shared/Program.cs
@@ -5,10 +5,19 @@
         static void Main(string[] args)
         {
             global::Example.Main();
-
+            Boxing();
             Types.Examples();
             ClassesObjects.Examples();
             Features.Examples();
+        }
+
+        private static void Boxing()
+        {
+            // <Boxing>
+            int i = 123;
+            object o = i;    // Boxing
+            int j = (int)o;  // Unboxing            
+            // </Boxing>
         }
     }
 }

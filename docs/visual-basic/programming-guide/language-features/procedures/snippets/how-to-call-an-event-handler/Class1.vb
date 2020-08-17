75fb3c97d0f04618fd891e089a7b3628ee804f4b
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2020-08-17 14:39:31 +0200

    Delete unnecessary comments

--- docs/visual-basic/programming-guide/language-features/procedures/snippets/how-to-call-an-event-handler/Class1.vb
+++ docs/visual-basic/programming-guide/language-features/procedures/snippets/how-to-call-an-event-handler/Class1.vb
@@ -1,27 +1,22 @@
-    '****************************************************************************
-    ' Special Form class to test Snippet5 -- changed Sub catchClose from Public to Protected 9/29/2005 for FxCop
-    ' Changed Protected catchClose to Private 4/6/2006 for FxCop -- jcooke
-    Public Class SpecialForm : Inherits System.Windows.Forms.Form
-        '<Snippet5>
-        ' Place these procedures inside a Form class definition.
-        Private Sub OnFormClosing(sender As Object, e As System.ComponentModel.CancelEventArgs)
-            ' Insert code to deal with impending closure of this form.
-        End Sub
+Public Class SpecialForm : Inherits System.Windows.Forms.Form
+    '<Snippet5>
+    ' Place these procedures inside a Form class definition.
+    Private Sub OnFormClosing(sender As Object, e As System.ComponentModel.CancelEventArgs)
+        ' Insert code to deal with impending closure of this form.
+    End Sub
 
-        Public Sub formOpened()
-            AddHandler Me.FormClosing, AddressOf OnFormClosing
-        End Sub
-        '</Snippet5>
-    End Class
+    Public Sub formOpened()
+        AddHandler Me.FormClosing, AddressOf OnFormClosing
+    End Sub
+    '</Snippet5>
+End Class
 
-
-    '****************************************************************************
-    '<Snippet4>
-    Public Class RaisesEvent
-        Public Event SomethingHappened()
-        Dim WithEvents happenObj As New RaisesEvent
-        Public Sub ProcessHappen() Handles happenObj.SomethingHappened
-            ' Insert code to handle somethingHappened event.
-        End Sub
-    End Class
-    '</Snippet4>
+'<Snippet4>
+Public Class RaisesEvent
+    Public Event SomethingHappened()
+    Dim WithEvents happenObj As New RaisesEvent
+    Public Sub ProcessHappen() Handles happenObj.SomethingHappened
+        ' Insert code to handle somethingHappened event.
+    End Sub
+End Class
+'</Snippet4>

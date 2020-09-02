bdeb9ab63c905141e6b267ffb529b026a56d01cf
Andy De George <67293991+adegeo@users.noreply.github.com>
2020-09-02 09:17:02 -0700

    Move desktop winforms to new repo (#20279)
    * Remove Winforms; Add redirect
    
    * Remove winforms snippets
    
    * Fix redirect
    
    * Remove CPP winforms snippets
    
    * Move snippets for standard articles
    
    * Point snippet to new ones
    
    * Fix links to versioned content in desktop guide
    
    * Update landing page
    
    * add redirects back in

---
+++ docs/standard/asynchronous-programming-patterns/snippets/component-that-supports-the-event-based-asynchronous-pattern/vb/Program.vb
@@ -0,0 +1,12 @@
+﻿Imports System
+Imports System.Windows.Forms
+
+Module Program
+    <STAThread>
+    Sub Main(args As String())
+        Application.SetHighDpiMode(HighDpiMode.SystemAware)
+        Application.EnableVisualStyles()
+        Application.SetCompatibleTextRenderingDefault(False)
+        Application.Run(New Form1())
+    End Sub
+End Module

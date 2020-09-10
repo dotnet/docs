d26642e2a79e5eca30e00bbffb17e1c6e46bc047
David Pine <david.pine@microsoft.com>
2020-09-10 12:05:21 -0500

    Added new sample source code. Clean up. (#20554)
    * Added new sample source code. Clean up.
    
    * Apply suggestions from code review
    
    Co-authored-by: Tom Dykstra <tdykstra@microsoft.com>
    
    Co-authored-by: Tom Dykstra <tdykstra@microsoft.com>

--- samples/snippets/csharp/VS_Snippets_CLR/conceptual.asyncdisposable/stacked-await-usings.cs
+++ samples/snippets/csharp/VS_Snippets_CLR/conceptual.asyncdisposable/stacked-await-usings.cs
@@ -1,13 +1,18 @@
-﻿class ExampleProgram
+﻿using System;
+using System.Threading.Tasks;
+
+class ExampleStackedProgram
 {
     static async Task Main()
     {
         var objOne = new ExampleAsyncDisposable();
-        await using objOne.ConfigureAwait(false);
-        // Interact with the objOne instance.
+        await using (objOne.ConfigureAwait(false))
+        {
+            // Interact with the objOne instance.
+        }
 
         var objTwo = new ExampleAsyncDisposable();
-        await using objTwo.ConfigureAwait(false))
+        await using (objTwo.ConfigureAwait(false))
         {
             // Interact with the objTwo instance.
         }

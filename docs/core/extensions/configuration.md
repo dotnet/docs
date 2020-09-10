2ef649d8ae0fc9d97b8d050216747c5cca5d44e9
David Pine <david.pine@microsoft.com>
2020-09-10 15:16:33 -0500

    Initial bits for .NET configuration

---
+++ docs/core/extensions/configuration.md
@@ -0,0 +1,10 @@
+---
+title: Configuration in .NET Core
+description: Learn how to use the Configuration API to configure an .NET Core app.
+author: IEvangelist
+ms.author: dapine
+ms.date: 09/10/2020
+ms.topic: overview
+---
+
+Configuration in .NET
---
+++ docs/core/extensions/snippets/configuration/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+
+namespace ConfigurationExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+    }
+}

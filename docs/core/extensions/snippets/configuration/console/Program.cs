430d0b1c04015007de8427fda0fb526e5f205d46
David Pine <david.pine@microsoft.com>
2020-09-15 08:49:10 -0500

    Remove NS comments - thanks Carol :)

--- docs/core/extensions/snippets/configuration/console/Program.cs
+++ docs/core/extensions/snippets/configuration/console/Program.cs
@@ -11,11 +11,4 @@ namespace Console.Example
         static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args);
     }
-    // Sample output:
-    //   info: Microsoft.Hosting.Lifetime[0]
-    //       Application started.Press Ctrl+C to shut down.
-    //   info: Microsoft.Hosting.Lifetime[0]
-    //       Hosting environment: Production
-    //   info: Microsoft.Hosting.Lifetime[0]
-    //       Content root path: .\configuration\bin\Debug\net5.0
 }

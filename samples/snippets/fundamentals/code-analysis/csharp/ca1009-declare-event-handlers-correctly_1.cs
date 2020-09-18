4ae9fcb4ccb7e9491f49043cbd66b4708a964106
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2020-09-18 14:41:33 -0700

    delete unneeded snippets

--- samples/snippets/fundamentals/code-analysis/csharp/ca1009-declare-event-handlers-correctly_1.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-namespace DesignLibrary
-{
-   public class AlarmEventArgs : EventArgs {}
-   public delegate void AlarmEventHandler(object sender, AlarmEventArgs e);
-}
--- samples/snippets/fundamentals/code-analysis/csharp/ca2208-instantiate-argument-exceptions-correctly_2.cs
+++
@@ -1,9 +0,0 @@
-public class Book
-{
-    public Book(string title)
-    {
-        Title = title ?? throw new ArgumentNullException(nameof(title), "All books must have a title.");
-    }
-
-    public string Title { get; }
-}

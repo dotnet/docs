927d84a063e876e6689e4658fbb52192b5864d1e
David Pine <david.pine@microsoft.com>
2020-09-24 09:09:55 -0500

    Fix #20765, possible deadlock

--- samples/snippets/csharp/VS_Snippets_Misc/tpldataflow_producerconsumer/cs/dataflowproducerconsumer.cs
+++ samples/snippets/csharp/VS_Snippets_Misc/tpldataflow_producerconsumer/cs/dataflowproducerconsumer.cs
@@ -3,100 +3,65 @@ using System;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 
-// Demonstrates a basic producer and consumer pattern that uses dataflow.
 class DataflowProducerConsumer
 {
-   // Demonstrates the production end of the producer and consumer pattern.
-   static void Produce(ITargetBlock<byte[]> target)
-   {
-      // Create a Random object to generate random data.
-      Random rand = new Random();
+    static void Produce(ITargetBlock<byte[]> target)
+    {
+        var rand = new Random();
 
-      // In a loop, fill a buffer with random data and
-      // post the buffer to the target block.
-      for (int i = 0; i < 100; i++)
-      {
-         // Create an array to hold random byte data.
-         byte[] buffer = new byte[1024];
+        for (int i = 0; i < 100; ++ i)
+        {
+            var buffer = new byte[1024];
+            rand.NextBytes(buffer);
+            target.Post(buffer);
+        }
 
-         // Fill the buffer with random bytes.
-         rand.NextBytes(buffer);
+        target.Complete();
+    }
 
-         // Post the result to the message block.
-         target.Post(buffer);
-      }
+    static async Task<int> ConsumeAsync(ISourceBlock<byte[]> source)
+    {
+        int bytesProcessed = 0;
 
-      // Set the target to the completed state to signal to the consumer
-      // that no more data will be available.
-      target.Complete();
-   }
+        while (await source.OutputAvailableAsync())
+        {
+            byte[] data = await source.ReceiveAsync();
+            bytesProcessed += data.Length;
+        }
 
-   // Demonstrates the consumption end of the producer and consumer pattern.
-   static async Task<int> ConsumeAsync(ISourceBlock<byte[]> source)
-   {
-      // Initialize a counter to track the number of bytes that are processed.
-      int bytesProcessed = 0;
+        return bytesProcessed;
+    }
 
-      // Read from the source buffer until the source buffer has no
-      // available output data.
-      while (await source.OutputAvailableAsync())
-      {
-         byte[] data = source.Receive();
+    static async Task Main()
+    {
+        var buffer = new BufferBlock<byte[]>();
+        var consumerTask = ConsumeAsync(buffer);
+        Produce(buffer);
 
-         // Increment the count of bytes received.
-         bytesProcessed += data.Length;
-      }
+        var bytesProcessed = await consumerTask;
 
-      return bytesProcessed;
-   }
-
-   static void Main(string[] args)
-   {
-      // Create a BufferBlock<byte[]> object. This object serves as the
-      // target block for the producer and the source block for the consumer.
-      var buffer = new BufferBlock<byte[]>();
-
-      // Start the consumer. The Consume method runs asynchronously.
-      var consumer = ConsumeAsync(buffer);
-
-      // Post source data to the dataflow block.
-      Produce(buffer);
-
-      // Wait for the consumer to process all data.
-      consumer.Wait();
-
-      // Print the count of bytes processed to the console.
-      Console.WriteLine("Processed {0} bytes.", consumer.Result);
-   }
+        Console.WriteLine($"Processed {bytesProcessed:#,#} bytes.");
+    }
 }
 
-/* Output:
-Processed 102400 bytes.
-*/
+// Sample  output:
+//     Processed 102,400 bytes.
 // </snippet1>
 
 class Program2
 {
    // <snippet2>
-   // Demonstrates the consumption end of the producer and consumer pattern.
    static async Task<int> ConsumeAsync(IReceivableSourceBlock<byte[]> source)
-   {
-      // Initialize a counter to track the number of bytes that are processed.
-      int bytesProcessed = 0;
-
-      // Read from the source buffer until the source buffer has no
-      // available output data.
-      while (await source.OutputAvailableAsync())
-      {
-         byte[] data;
-         while (source.TryReceive(out data))
-         {
-            // Increment the count of bytes received.
-            bytesProcessed += data.Length;
-         }
-      }
-
-      return bytesProcessed;
-   }
+    {
+        int bytesProcessed = 0;
+        while (await source.OutputAvailableAsync())
+        {
+            while (source.TryReceive(out byte[] data))
+            {
+                bytesProcessed += data.Length;
+            }
+        }
+        return bytesProcessed;
+    }
    // </snippet2>
 }

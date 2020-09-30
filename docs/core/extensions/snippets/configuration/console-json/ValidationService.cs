2a620594327866158add8e4b8a2f93923a04431d
David Pine <david.pine@microsoft.com>
2020-09-30 17:06:31 -0500

    Port over .NET Options Pattern article to finalize Microsoft Extensions fundamentals (#20813)
    * Initial bits
    
    * More updates
    
    * More updates
    
    * More fixes, and acrolinx
    
    * Added TOC entry for options article
    
    * Fixed warnings...
    
    * Apply suggestions from code review
    
    Co-authored-by: Tom Dykstra <tdykstra@microsoft.com>
    
    * Updates from review
    
    Co-authored-by: Tom Dykstra <tdykstra@microsoft.com>

---
+++ docs/core/extensions/snippets/configuration/console-json/ValidationService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ConsoleJson.Example
+{
+    public class ValidationService
+    {
+        private readonly ILogger<ValidationService> _logger;
+        private readonly IOptions<SettingsOptions> _config;
+
+        public ValidationService(
+            ILogger<ValidationService> logger,
+            IOptions<SettingsOptions> config)
+        {
+            _config = config;
+            _logger = logger;
+
+            try
+            {
+                SettingsOptions options = _config.Value;
+            }
+            catch (OptionsValidationException ex)
+            {
+                foreach (string failure in ex.Failures)
+                {
+                    _logger.LogError(failure);
+                }
+            }
+        }
+    }
+}

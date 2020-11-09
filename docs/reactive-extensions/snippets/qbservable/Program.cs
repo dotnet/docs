bc6c8d9edfed0173f57b5e3ae04693d3f253b637
David Pine <david.pine@microsoft.com>
2020-11-09 13:27:34 -0600

    Added and updated 'Qbservable' bits

---
+++ docs/reactive-extensions/snippets/qbservable/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reactive.Linq;
+
+IObservable<long> source =
+    Observable.Interval(TimeSpan.FromSeconds(1));
+IQbservable<long> qbservable = source.AsQbservable();
+
+Console.WriteLine(qbservable.ToString());
+
+using IDisposable subscription =
+    qbservable.Subscribe(Console.WriteLine);
+
+Console.ReadKey();

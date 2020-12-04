c31fce9c3670e9568d4393fb0d22c43e35d0c8c0
David Pine <david.pine@microsoft.com>
2020-12-04 10:16:43 -0600

    Let's see if the access-by-line.txt actually works

--- docs/core/extensions/snippets/configuration/console-env/Program.cs
+++ docs/core/extensions/snippets/configuration/console-env/Program.cs
@@ -6,8 +6,14 @@ namespace ConsoleEnvironment.Example
 {
     class Program
     {
-        static Task Main(string[] args) =>
-            CreateHostBuilder(args).Build().RunAsync();
+        static async Task Main(string[] args)
+        {
+            using IHost host = CreateHostBuilder(args).Build();
+
+            // Application code should start here.
+
+            await host.RunAsync();
+        }
 
         static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)

ae886c66491dcc44cebcc7d1a614ab0072b9d7b1
Brett Rowberry <brett.rowberry@gmail.com>
2020-12-25 23:50:05 -0500

    fs0037

--- samples/snippets/fsharp/compiler-messages/fs0037.fsx
+++ samples/snippets/fsharp/compiler-messages/fs0037.fsx
@@ -1,4 +1,16 @@
-// comment
-let someCode = 5
-printfn "%i" someCode
-    
+(* duplicate definition *)
+let a = 5
+let a = 6
+
+(* different identifier *)
+let b = 5
+let c = 6
+
+(* prime *)
+let d = 5
+let d' = 6
+let d'' = 7
+
+(* mutate *)
+let mutable e = 5
+e <- 6

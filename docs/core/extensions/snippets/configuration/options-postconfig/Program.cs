844ce467971bc257df81d2136a561cdaf3e3858f
David Pine <david.pine@microsoft.com>
2021-01-21 10:16:38 -0600

    Fixed another bug, misalignment of hightlighting due to using

--- docs/core/extensions/snippets/configuration/options-postconfig/Program.cs
+++ docs/core/extensions/snippets/configuration/options-postconfig/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using ExampleLibrary.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 

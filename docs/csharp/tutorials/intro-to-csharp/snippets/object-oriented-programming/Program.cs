9d5bbdc27f637c246ff1c3ea0893d852eb4f881d
Josh Gorton <46949726+jjgorton@users.noreply.github.com>
2021-01-23 02:34:57 -0500

    Adding additional code snippet
    This additional code snippet simply removes the 3rd argument of the `LineOfCreditAccount` constructor to allow for the expected error.

--- docs/csharp/tutorials/intro-to-csharp/snippets/object-oriented-programming/Program.cs
+++ docs/csharp/tutorials/intro-to-csharp/snippets/object-oriented-programming/Program.cs
@@ -24,6 +24,17 @@ namespace OOProgramming
             savings.PerformMonthEndTransactions();
             Console.WriteLine(savings.GetAccountHistory());
             // </FirstTests>
+            
+            // <InitialTestLineOfCredit>
+            var lineOfCredit = new LineOfCreditAccount("line of credit", 0);
+            // How much is too much to borrow?
+            lineOfCredit.MakeWithdrawal(1000m, DateTime.Now, "Take out monthly advance");
+            lineOfCredit.MakeDeposit(50m, DateTime.Now, "Pay back small amount");
+            lineOfCredit.MakeWithdrawal(5000m, DateTime.Now, "Emergency funds for repairs");
+            lineOfCredit.MakeDeposit(150m, DateTime.Now, "Partial restoration on repairs");
+            lineOfCredit.PerformMonthEndTransactions();
+            Console.WriteLine(lineOfCredit.GetAccountHistory());
+            // </InitialTestLineOfCredit>
 
             // <TestLineOfCredit>
             var lineOfCredit = new LineOfCreditAccount("line of credit", 0, 2000);

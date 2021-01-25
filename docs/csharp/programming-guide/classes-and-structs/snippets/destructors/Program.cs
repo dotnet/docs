62fb4a4854f4a0f5e035462d18f732ddda82b8f3
Youssef Victor <youssefvictor00@gmail.com>
2021-01-25 21:20:50 +0200

    Move fields and destructors snippets

---
+++ docs/csharp/programming-guide/classes-and-structs/snippets/destructors/Program.cs
@@ -0,0 +1,48 @@
+﻿//<Snippet1>
+class First
+{
+    ~First()
+    {
+        System.Diagnostics.Trace.WriteLine("First's destructor is called.");
+    }
+}
+
+class Second : First
+{
+    ~Second()
+    {
+        System.Diagnostics.Trace.WriteLine("Second's destructor is called.");
+    }
+}
+
+class Third : Second
+{
+    ~Third()
+    {
+        System.Diagnostics.Trace.WriteLine("Third's destructor is called.");
+    }
+}
+
+class TestDestructors
+{
+    static void Main()
+    {
+        Third t = new Third();
+    }
+}
+/* Output (to VS Output Window):
+    Third's destructor is called.
+    Second's destructor is called.
+    First's destructor is called.
+*/
+//</Snippet1>
+
+//<Snippet2>
+class Car
+{
+    ~Car()  // finalizer
+    {
+        // cleanup statements...
+    }
+}
+//</Snippet2>

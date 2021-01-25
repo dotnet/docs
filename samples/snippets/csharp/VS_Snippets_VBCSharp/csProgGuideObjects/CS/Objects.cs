134225f5c7629d1579d11cfb38b1ed4df1c44e67
Alpha Coder <usmanfareed92@gmail.com>
2021-01-25 22:06:56 +0500

    Code is missing a $ special character

--- samples/snippets/csharp/VS_Snippets_VBCSharp/csProgGuideObjects/CS/Objects.cs
+++ samples/snippets/csharp/VS_Snippets_VBCSharp/csProgGuideObjects/CS/Objects.cs
@@ -177,7 +177,7 @@ namespace CsCsrefProgrammingObjects
             {
                 var person = new Person();
 
-                Console.WriteLine("Name: {person.name}, Age: {person.age}");
+                Console.WriteLine($"Name: {person.name}, Age: {person.age}");
                 // Keep the console window open in debug mode.
                 Console.WriteLine("Press any key to exit.");
                 Console.ReadKey();

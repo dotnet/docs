643ddd6456fb361b7274170f600f8a7e16b16e1a
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2021-02-02 16:16:15 +0200

    Remove dependency on dotnet/samples repo (#19754)

---
+++ docs/core/testing/snippets/order-unit-tests/csharp/XUnit.TestProject/Attributes/TestPriorityAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XUnit.Project.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class TestPriorityAttribute : Attribute
+    {
+        public int Priority { get; private set; }
+
+        public TestPriorityAttribute(int priority) => Priority = priority;
+    }
+}

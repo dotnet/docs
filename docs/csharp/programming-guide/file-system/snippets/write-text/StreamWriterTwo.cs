d1b60d8a6d202de5c1950984bf8de022f285f135
David Pine <david.pine@microsoft.com>
2021-02-10 10:47:30 -0600

    Modernized file writing C# examples, broke up into logical examples to avoid comments serving as docs.

---
+++ docs/csharp/programming-guide/file-system/snippets/write-text/StreamWriterTwo.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+class StreamWriterTwo
+{
+    public static async Task ExampleAsync()
+    {
+        using StreamWriter file = new(@"WriteLines2.txt", true);
+        await file.WriteLineAsync("Fourth line");
+    }
+}

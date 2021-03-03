f0cba2dc1049bea0b69a42a1ffc80bb118a82b66
Petr Kulikov <petr.kulikov@gmail.com>
2021-03-03 15:19:32 +0100

    C# patterns reference: constant and discard patterns (#23045)

--- docs/csharp/language-reference/operators/snippets/patterns/Program.cs
+++ docs/csharp/language-reference/operators/snippets/patterns/Program.cs
@@ -9,6 +9,14 @@ namespace Patterns
             Console.WriteLine("--- Declaration and type pattern examples ---");
             DeclarationAndTypePattern.Examples();
             Console.WriteLine();
+
+            Console.WriteLine("--- Constant pattern ------------------------");
+            ConstantPattern.Examples();
+            Console.WriteLine();
+
+            Console.WriteLine("--- Discard pattern -------------------------");
+            DiscardPattern.Examples();
+            Console.WriteLine();
         }
     }
 }

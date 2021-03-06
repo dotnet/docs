c35bb0e01a1bdda9edc3a72d03ee68bd7101b679
Mike Howell <Mike.Howell@cin7.com>
2021-03-06 17:11:39 +1300

    added VB code example for CA2215

---
+++ docs/fundamentals/code-analysis/quality-rules/snippets/vb/all-rules/ca2215.vb
@@ -0,0 +1,46 @@
+Namespace ca2215
+
+    Public Class TypeA
+        Implements IDisposable
+
+        Protected Overridable Overloads Sub Dispose(disposing As Boolean)
+
+            If disposing Then
+                ' dispose managed resources
+            End If
+            
+            ' free native resources
+
+        End Sub ' Dispose
+
+        Public Overloads Sub Dispose() Implements IDisposable.Dispose
+
+            Dispose(True)
+            GC.SuppressFinalize(Me)
+
+        End Sub ' Dispose
+
+        ' Disposable types implement a finalizer.
+        Protected Overrides Sub Finalize()
+
+            Dispose(False)
+            MyBase.Finalize()
+
+        End Sub ' Finalize
+
+    End Class
+
+    Public Class TypeB
+        Inherits TypeA
+
+        Protected Overrides Sub Dispose(disposing As Boolean)
+
+            If Not disposing Then
+                MyBase.Dispose(False)
+            End If
+
+        End Sub ' Dispose
+
+    End Class
+
+End Namespace

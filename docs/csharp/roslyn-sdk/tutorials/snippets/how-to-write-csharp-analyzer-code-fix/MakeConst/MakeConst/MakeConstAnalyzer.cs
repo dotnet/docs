2308812820c08c3fc33fabd32c27c40ad5a835c3
Youssef Victor <youssefvictor00@gmail.com>
2021-03-08 22:48:52 +0200

    Update analyzer tutorial (#23066)
    * Initial work for analyzer tutorial
    
    * Make code fixes a separate project to match template
    
    * Update test project - test code file will be addressed in later commit
    
    * Fix paths
    
    * More edits to the article
    
    * Use var - address part of #17779
    
    * Add sln and NuGet.config
    
    * More fixes
    
    * Move snippets.5000.json
    
    * Update dependabot.yml
    
    * More fixes
    
    * Make static
    
    * Address feedback and use snippets
    
    * Forward cancellation token
    
    * Update docs/csharp/roslyn-sdk/tutorials/how-to-write-csharp-analyzer-code-fix.md
    
    * Update docs/csharp/roslyn-sdk/tutorials/how-to-write-csharp-analyzer-code-fix.md
    
    Co-authored-by: Sam Harwell <sam@tunnelvisionlabs.com>
    
    Co-authored-by: Sam Harwell <sam@tunnelvisionlabs.com>

--- docs/csharp/roslyn-sdk/tutorials/snippets/how-to-write-csharp-analyzer-code-fix/MakeConst/MakeConst/MakeConstAnalyzer.cs
+++ docs/csharp/roslyn-sdk/tutorials/snippets/how-to-write-csharp-analyzer-code-fix/MakeConst/MakeConst/MakeConstAnalyzer.cs
@@ -20,7 +20,9 @@ namespace MakeConst
         private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        // <SnippetCategory>
         private const string Category = "Usage";
+        // </SnippetCategory>
 
         private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
 
@@ -29,50 +31,59 @@ namespace MakeConst
         public override void Initialize(AnalysisContext context)
         {
              // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-             // <SnippetRegisterNodeAction>
-            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.None);
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
             context.EnableConcurrentExecution();
+            // <SnippetRegisterNodeAction>
             context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.LocalDeclarationStatement);
             // </SnippetRegisterNodeAction>
         }
 
         private void AnalyzeNode(SyntaxNodeAnalysisContext context)
         {
+            // <SnippetLocalDeclaration>
             var localDeclaration = (LocalDeclarationStatementSyntax)context.Node;
+            // </SnippetLocalDeclaration>
 
-            // does not have const
+            // <SnippetBailOutOnConst>
+            // make sure the declaration isn't already const:
             if (localDeclaration.Modifiers.Any(SyntaxKind.ConstKeyword))
             {
                 return;
             }
+            // </SnippetBailOutOnConst>
 
-            var variableTypeName = localDeclaration.Declaration.Type;
-            var variableType = context.SemanticModel.GetTypeInfo(variableTypeName).ConvertedType;
+            // <SnippetVariableConvertedType>
+            TypeSyntax variableTypeName = localDeclaration.Declaration.Type;
+            ITypeSymbol variableType = context.SemanticModel.GetTypeInfo(variableTypeName, context.CancellationToken).ConvertedType;
+            // </SnippetVariableConvertedType>
 
             // Ensure that all variables in the local declaration have initializers that
             // are assigned with constant values.
-            foreach (var variable in localDeclaration.Declaration.Variables)
+            foreach (VariableDeclaratorSyntax variable in localDeclaration.Declaration.Variables)
             {
-                var initializer = variable.Initializer;
+                EqualsValueClauseSyntax initializer = variable.Initializer;
                 if (initializer == null)
                 {
                     return;
                 }
 
-                var constantValue = context.SemanticModel.GetConstantValue(initializer.Value);
+                Optional<object> constantValue = context.SemanticModel.GetConstantValue(initializer.Value, context.CancellationToken);
                 if (!constantValue.HasValue)
                 {
                     return;
                 }
 
+                // <SnippetBailOutOnUserDefinedConversion>
                 // Ensure that the initializer value can be converted to the type of the
                 // local declaration without a user-defined conversion.
-                var conversion = context.SemanticModel.ClassifyConversion(initializer.Value, variableType);
+                Conversion conversion = context.SemanticModel.ClassifyConversion(initializer.Value, variableType);
                 if (!conversion.Exists || conversion.IsUserDefined)
                 {
                     return;
                 }
+                // </SnippetBailOutOnUserDefinedConversion>
 
+                // <SnippetHandleSpecialCases>
                 // Special cases:
                 //  * If the constant value is a string, the type of the local declaration
                 //    must be System.String.
@@ -89,23 +100,26 @@ namespace MakeConst
                 {
                     return;
                 }
+                // </SnippetHandleSpecialCases>
             }
 
             // Perform data flow analysis on the local declaration.
-            var dataFlowAnalysis = context.SemanticModel.AnalyzeDataFlow(localDeclaration);
+            DataFlowAnalysis dataFlowAnalysis = context.SemanticModel.AnalyzeDataFlow(localDeclaration);
 
-            foreach (var variable in localDeclaration.Declaration.Variables)
+            foreach (VariableDeclaratorSyntax variable in localDeclaration.Declaration.Variables)
             {
                 // Retrieve the local symbol for each variable in the local declaration
                 // and ensure that it is not written outside of the data flow analysis region.
-                var variableSymbol = context.SemanticModel.GetDeclaredSymbol(variable);
+                ISymbol variableSymbol = context.SemanticModel.GetDeclaredSymbol(variable, context.CancellationToken);
                 if (dataFlowAnalysis.WrittenOutside.Contains(variableSymbol))
                 {
                     return;
                 }
             }
 
+            // <SnippetReportDiagnostic>
             context.ReportDiagnostic(Diagnostic.Create(Rule, context.Node.GetLocation()));
+            // </SnippetReportDiagnostic>
         }
     }
 }

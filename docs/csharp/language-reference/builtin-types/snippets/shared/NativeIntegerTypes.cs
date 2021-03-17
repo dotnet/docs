c47a32608cd9466ed6de3dc1bfe29d51cba8e478
Tom Dykstra <tdykstra@microsoft.com>
2021-03-17 09:43:45 -0700

    proofread correction

--- docs/csharp/language-reference/builtin-types/snippets/shared/NativeIntegerTypes.cs
+++ docs/csharp/language-reference/builtin-types/snippets/shared/NativeIntegerTypes.cs
@@ -44,12 +44,12 @@ namespace builtin_types
                 Console.WriteLine($"size of nuint = {sizeof(nuint)}");
 
                 // output when run in a 64-bit process
-                //size of nint = 4
-                //size of nuint = 4
-
-                // output when run in a 32-bit process
                 //size of nint = 8
                 //size of nuint = 8
+
+                // output when run in a 32-bit process
+                //size of nint = 4
+                //size of nuint = 4
                 // </SizeOf>
             }
         }

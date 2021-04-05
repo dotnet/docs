f5feb8eba3d10f0d0e9cb16ce72eab09dcce3bba
Bill Wagner <wiwagn@microsoft.com>
2021-04-05 12:48:31 -0400

    Create unsafe code section - include information on function pointers (#23559)
    * move unsafe code content to language reference
    
    First commit: Move the remaining unsafe code items from the programming guide to the language reference. This makes a new section for unsafe code.
    
    * consolidate unsafe reference into a single article.
    
    * First draft of function pointers section, including samples
    
    * proofread
    
    * fix build warnings.
    
    * run clean repo to replace redirects added
    
    * run clean repo to replace relative links.
    
    * fix incorrect redirect, run tools again.
    
    * fix remaining warnings
    
    * Apply suggestions from code review
    
    Co-authored-by: Genevieve Warren <24882762+gewarren@users.noreply.github.com>
    
    * fix incorrect link redirects
    
    Noted in code review.
    
    Co-authored-by: Genevieve Warren <24882762+gewarren@users.noreply.github.com>

--- docs/csharp/language-reference/snippets/unsafe-code/Program.cs
+++ docs/csharp/language-reference/snippets/unsafe-code/Program.cs
@@ -9,6 +9,12 @@ namespace UnsafeCodePointers
         {
             Console.WriteLine("=================    Fixed Memory Examples ======================");
             FixedKeywordExamples.Examples();
+
+            Console.WriteLine("=================    Conversion Examples   ======================");
+            ClassConvert.Conversions();
+
+            Console.WriteLine("===================   Function pointers   =======================");
+            FunctionPointers.PointerExamples();
         }
     }
 }

7a231e8ce212709084d510f82e6f3051c144adee
David Pine <david.pine@microsoft.com>
2021-04-19 20:54:23 -0500

    Use explicit scoping of disposable implementations

--- docs/standard/security/snippets/decrypting-data/csharp/aes-decrypt.cs
+++ docs/standard/security/snippets/decrypting-data/csharp/aes-decrypt.cs
@@ -4,47 +4,41 @@ using System.Security.Cryptography;
 
 try
 {
-    using FileStream fileStream = new("TestData.txt", FileMode.Open);
-
-    // Create a new instance of the default Aes implementation class
-    using Aes aes = Aes.Create();
-
-    // Reads IV value from beginning of the file.
-    byte[] iv = new byte[aes.IV.Length];
-    int numBytesToRead = aes.IV.Length;
-    int numBytesRead = 0;
-    while (numBytesToRead > 0)
+    using (FileStream fileStream = new("TestData.txt", FileMode.Open))
     {
-        int n = fileStream.Read(iv, numBytesRead, numBytesToRead);
+        using (Aes aes = Aes.Create())
+        {
+            byte[] iv = new byte[aes.IV.Length];
+            int numBytesToRead = aes.IV.Length;
+            int numBytesRead = 0;
+            while (numBytesToRead > 0)
+            {
+                int n = fileStream.Read(iv, numBytesRead, numBytesToRead);
+                if (n == 0) break;
 
-        // Break when the end of the file is reached.
-        if (n == 0) break;
+                numBytesRead += n;
+                numBytesToRead -= n;
+            }
 
-        numBytesRead += n;
-        numBytesToRead -= n;
+            byte[] key =
+            {
+                0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16
+            };
+
+            using (CryptoStream cryptoStream = new(
+               fileStream,
+               aes.CreateDecryptor(key, iv),
+               CryptoStreamMode.Read))
+            {
+                using (StreamReader decryptReader = new(cryptoStream))
+                {
+                    string decryptedMessage = await decryptReader.ReadToEndAsync();
+                    Console.WriteLine($"The decrypted original message: {decryptedMessage}");
+                }
+            }
+        }
     }
-
-    // Decryption key must be the same value that was used
-    // to encrypt the stream.
-    byte[] key =
-    {
-        0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
-        0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16
-    };
-
-    // Create a CryptoStream, pass it the file stream, and decrypt
-    // it with the Aes class using the key and IV.
-    using CryptoStream cryptoStream = new(
-       fileStream,
-       aes.CreateDecryptor(key, iv),
-       CryptoStreamMode.Read);
-
-    // Read the stream.
-    using StreamReader decryptReader = new(cryptoStream);
-
-    string decryptedMessage = await decryptReader.ReadToEndAsync();
-
-    Console.WriteLine($"The decrypted original message: {decryptedMessage}");
 }
 catch (Exception ex)
 {
--- docs/standard/security/snippets/encrypting-data/csharp/aes-encrypt.cs
+++ docs/standard/security/snippets/encrypting-data/csharp/aes-encrypt.cs
@@ -6,35 +6,27 @@ try
 {
     using (FileStream fileStream = new("TestData.txt", FileMode.OpenOrCreate))
     {
-
-        // Create an instance of the default Aes implementation class
-        // and configure the encryption key.
-        using Aes aes = Aes.Create();
-
-        // Encryption key used to encrypt the stream.
-        // The same value must be used to encrypt and decrypt the stream.
-        byte[] key =
+        using (Aes aes = Aes.Create())
         {
-            0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
-            0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16
-        };
-        aes.Key = key;
-
-        // Stores IV at the beginning of the file.
-        // This information will be used for decryption.
-        byte[] iv = aes.IV;
-        fileStream.Write(iv, 0, iv.Length);
-
-        // Create a CryptoStream, pass it the FileStream, and encrypt
-        // it with the Aes class.
-        using (CryptoStream cryptoStream = new(
-            fileStream,
-            aes.CreateEncryptor(),
-            CryptoStreamMode.Write))
-        {
-            using (StreamWriter encryptWriter = new(cryptoStream))
+            byte[] key =
             {
-                encryptWriter.WriteLine("Hello World!");
+                0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16
+            };
+            aes.Key = key;
+
+            byte[] iv = aes.IV;
+            fileStream.Write(iv, 0, iv.Length);
+
+            using (CryptoStream cryptoStream = new(
+                fileStream,
+                aes.CreateEncryptor(),
+                CryptoStreamMode.Write))
+            {
+                using (StreamWriter encryptWriter = new(cryptoStream))
+                {
+                    encryptWriter.WriteLine("Hello World!");
+                }
             }
         }
     }

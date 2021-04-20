dfa6277b5395044d29406dd14985c068b5970a6b
David Pine <david.pine@microsoft.com>
2021-04-19 21:22:11 -0500

    Fix a bug in the encrypt sample, that was causing the decrypt to fail (#23816)
    * Fix a bug in the encrypt sample, that was causing the decrypt to fail
    
    * Apply suggestions from code review
    
    Co-authored-by: Youssef Victor <youssefvictor00@gmail.com>
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * Corrected updated names
    
    * Addressed feedback from peer review
    
    * Use explicit scoping of disposable implementations
    
    Co-authored-by: Youssef Victor <youssefvictor00@gmail.com>

--- docs/standard/security/snippets/encrypting-data/vb/aes-encrypt.vb
+++ docs/standard/security/snippets/encrypting-data/vb/aes-encrypt.vb
@@ -4,33 +4,36 @@ Imports System.Security.Cryptography
 
 Module Module1
     Sub Main()
-        'Encryption key used to encrypt the stream.
-        'The same value must be used to encrypt and decrypt the stream.
-        Dim key As Byte() = {&H1, &H2, &H3, &H4, &H5, &H6, &H7, &H8, &H9, &H10, &H11, &H12, &H13, &H14, &H15, &H16}
-
         Try
-            'Create a file stream
-            Using myStream As FileStream = New FileStream("TestData.txt", FileMode.OpenOrCreate)
+            ' Create a file stream
+            Using fileStream As New FileStream("TestData.txt", FileMode.OpenOrCreate)
 
-                'Create a new instance of the default Aes implementation class  
-                ' and configure encryption key.  
+                ' Create a new instance of the default Aes implementation class  
+                ' and configure encryption key.
                 Using aes As Aes = Aes.Create()
+                    'Encryption key used to encrypt the stream.
+                    'The same value must be used to encrypt and decrypt the stream.
+                    Dim key As Byte() = {
+                        &H1, &H2, &H3, &H4, &H5, &H6, &H7, &H8,
+                        &H9, &H10, &H11, &H12, &H13, &H14, &H15, &H16
+                    }
+
                     aes.Key = key
 
-                    'Stores IV at the beginning of the file.
-                    'This information will be used for decryption.
+                    ' Stores IV at the beginning of the file.
+                    ' This information will be used for decryption.
                     Dim iv As Byte() = aes.IV
-                    myStream.Write(iv, 0, iv.Length)
+                    fileStream.Write(iv, 0, iv.Length)
 
-                    'Create a CryptoStream, pass it the FileStream, and encrypt
-                    'it with the Aes class.  
-                    Using cryptStream As New CryptoStream(myStream, aes.CreateEncryptor(), CryptoStreamMode.Write)
+                    ' Create a CryptoStream, pass it the FileStream, and encrypt
+                    ' it with the Aes class.
+                    Using cryptoStream As New CryptoStream(fileStream, aes.CreateEncryptor(), CryptoStreamMode.Write)
 
-                        'Create a StreamWriter for easy writing to the
-                        'file stream.  
-                        Using sWriter As New StreamWriter(cryptStream)
+                        ' Create a StreamWriter for easy writing to the
+                        ' file stream.
+                        Using sWriter As New StreamWriter(cryptoStream)
 
-                            'Write to the stream.  
+                            'Write to the stream.
                             sWriter.WriteLine("Hello World!")
                         End Using
                     End Using

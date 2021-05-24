1f07094b7f787635586d41876255c55e2b3cbc86
David Pine <david.pine@microsoft.com>
2021-05-24 12:49:16 -0500

    Corrected names

--- docs/core/extensions/snippets/workers/background-service/Program.cs
+++ docs/core/extensions/snippets/workers/background-service/Program.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace App.WorkerService
+namespace App.BackgroundService
 {
     public class Program
     {
--- docs/core/extensions/snippets/workers/background-service/Worker.cs
+++ docs/core/extensions/snippets/workers/background-service/Worker.cs
@@ -4,7 +4,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace App.WorkerService
+namespace App.BackgroundService
 {
     public class Worker : BackgroundService
     {

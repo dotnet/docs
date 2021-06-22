29d1e9026b02acb1e79201e3375c0bdf4086664d
Bill Wagner <wiwagn@microsoft.com>
2021-06-22 15:22:20 -0400

    update samples and edit pass
    Before fixing the issue on iterators / async iterators, update the samples and do a general edit pass.

---
+++ docs/csharp/snippets/iterators/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace iterators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            firstExample(new int[] {1,2,3,4,5});
+        }
+
+        public static void firstExample<T>(IEnumerable<T> collection)
+        {
+            // <ForeachExample>
+            foreach (var item in collection)
+            {
+            Console.WriteLine(item.ToString());
+            }
+            // </ForeachExample>
+        }
+
+        public static void InsideForeach(IEnumerable<int> collection)
+        {
+            // <InsideForeach>
+            IEnumerator<int> enumerator = collection.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var item = enumerator.Current;
+                Console.WriteLine(item.ToString());
+            }
+            // </InsideForeach>
+        }
+    }
+}

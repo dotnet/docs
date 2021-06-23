4e7872ca722624cbefc43a2731f64361dda58101
David Pine <david.pine@microsoft.com>
2021-06-23 13:11:18 -0500

    Use script instead and avoid attempt at JS

---
+++ .github/workflows/dependencies/run-dotnet-whatsnew.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+# This script runs the .NET CLI, invoking the what's new global tool
+#     $1 is the <github.repository_owner>
+#     $2 is the <github.repository>
+#     $3 is the savedir
+
+declare -r STARTDATE=$(date "+%F" -d "$(date +'%Y-%m-01') -1 month");
+declare -r ENDDATE=$(date "+%F" -d "$STARTDATE +1 month -1 day");
+
+dotnet whatsnew \
+    --owner $1 \
+    --repo $2 \
+    --startdate $STARTDATE \
+    --enddate $ENDDATE \
+    --savedir $3

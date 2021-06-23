7cb475d58216d7f4883fc2fb9bc45f3700da0974
David Pine <david.pine@microsoft.com>
2021-06-23 14:20:26 -0500

    Echo dates, and use hardcoded config args (#24807)

--- .github/workflows/dependencies/run-dotnet-whatsnew.sh
+++ .github/workflows/dependencies/run-dotnet-whatsnew.sh
@@ -10,6 +10,8 @@ set -e
 declare -r STARTDATE=$(date "+%F" -d "$(date +'%Y-%m-01') -1 month");
 declare -r ENDDATE=$(date "+%F" -d "$STARTDATE +1 month -1 day");
 
+echo "From $STARTDATE to $ENDDATE"
+
 dotnet whatsnew \
     --owner $1 \
     --repo $2 \

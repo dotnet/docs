2d34267348bae034e6a5254131a14c94bbf4141e
Tom Dykstra <tdykstra@microsoft.com>
2021-06-24 14:01:05 -0700

    switch --> if

--- docs/standard/serialization/snippets/system-text-json-how-to/csharp/RoundtripDataTable.cs
+++ docs/standard/serialization/snippets/system-text-json-how-to/csharp/RoundtripDataTable.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -8,7 +9,7 @@ namespace RoundtripDataTable
 {
     public class WeatherForecast
     {
-        public DateTimeOffset Date { get; set; }
+        public DateTime Date { get; set; }
         public int TemperatureCelsius { get; set; }
         public string Summary { get; set; }
     }
@@ -46,7 +47,7 @@ namespace RoundtripDataTable
             }
 
             // Serialize the DataTable and display the JSON
-            jsonString = JsonSerializer.Serialize(weatherForecasts, options);
+            jsonString = JsonSerializer.Serialize(weatherForecastTable, options);
             Console.WriteLine(jsonString);
         }
     }
@@ -70,64 +71,66 @@ namespace RoundtripDataTable
                 foreach (DataColumn col in value.Columns)
                 {
                     string key = col.ColumnName.Trim();
-                    string valueString = dr[col].ToString();
-                    switch (col.DataType.FullName)
+
+                    if (col.DataType == typeof(bool))
                     {
-                        case "System.Guid":
-                            jsonWriter.WriteString(key, valueString);
-                            break;
-                        case "System.Char":
-                        case "System.String":
-                            jsonWriter.WriteString(key, valueString);
-                            break;
-                        case "System.Boolean":
-                            Boolean.TryParse(valueString, out bool boolValue);
-                            jsonWriter.WriteBoolean(key, boolValue);
-                            break;
-                        case "System.DateTime":
-                            if (DateTime.TryParse(valueString, out DateTime dateValue))
-                            {
-                                jsonWriter.WriteString(key, dateValue);
-                            }
-                            else
-                            {
-                                jsonWriter.WriteString(key, "");
-                            }
-                            break;
-                        case "System.TimeSpan":
-                            if (DateTime.TryParse(valueString, out DateTime timeSpanValue))
-                            {
-                                jsonWriter.WriteString(key, timeSpanValue.ToString());
-                            }
-                            else
-                            {
-                                jsonWriter.WriteString(key, "");
-                            }
-                            break;
-                        case "System.Byte":
-                        case "System.SByte":
-                        case "System.Decimal":
-                        case "System.Double":
-                        case "System.Single":
-                        case "System.Int16":
-                        case "System.Int32":
-                        case "System.Int64":
-                        case "System.UInt16":
-                        case "System.UInt32":
-                        case "System.UInt64":
-                            if (long.TryParse(valueString, out long intValue))
-                            {
-                                jsonWriter.WriteNumber(key, intValue);
-                            }
-                            else
-                            {
-                                double.TryParse(valueString, out double doubleValue);
-                                jsonWriter.WriteNumber(key, doubleValue);
-                            }
-                            break;
-                        default:
-                            jsonWriter.WriteString(key, valueString);
-                            break;
+                        jsonWriter.WriteBoolean(key, (bool)dr[col]);
+                    }
+                    else if (col.DataType == typeof(byte))
+                    {
+                        jsonWriter.WriteNumber(key, (byte)dr[col]);
+                    }
+                    else if (col.DataType == typeof(sbyte))
+                    {
+                        jsonWriter.WriteNumber(key, (sbyte)dr[col]);
+                    }
+                    else if (col.DataType == typeof(decimal))
+                    {
+                        jsonWriter.WriteNumber(key, (decimal)dr[col]);
+                    }
+                    else if (col.DataType == typeof(double))
+                    {
+                        jsonWriter.WriteNumber(key, (double)dr[col]);
+                    }
+                    else if (col.DataType == typeof(float))
+                    {
+                        jsonWriter.WriteNumber(key, (float)dr[col]);
+                    }
+                    else if (col.DataType == typeof(short))
+                    {
+                        jsonWriter.WriteNumber(key, (short)dr[col]);
+                    }
+                    else if (col.DataType == typeof(int))
+                    {
+                        jsonWriter.WriteNumber(key, (int)dr[col]);
+                    }
+                    else if (col.DataType == typeof(long))
+                    {
+                        jsonWriter.WriteNumber(key, (long)dr[col]);
+                    }
+                    else if (col.DataType == typeof(ushort))
+                    {
+                        jsonWriter.WriteNumber(key, (ushort)dr[col]);
+                    }
+                    else if (col.DataType == typeof(uint))
+                    {
+                        jsonWriter.WriteNumber(key, (uint)dr[col]);
+                    }
+                    else if (col.DataType == typeof(ulong))
+                    {
+                        jsonWriter.WriteNumber(key, (ulong)dr[col]);
+                    }
+                    else if (col.DataType == typeof(DateTime))
+                    {
+                        jsonWriter.WriteString(key, (DateTime)dr[col]);
+                    }
+                    else if (col.DataType == typeof(Guid))
+                    {
+                        jsonWriter.WriteString(key, (Guid)dr[col]);
+                    }
+                    else
+                    {
+                        jsonWriter.WriteString(key, dr[col].ToString());
                     }
                 }
                 jsonWriter.WriteEndObject();
@@ -156,6 +159,7 @@ namespace RoundtripDataTable
                 }
                 firstPass = false;
             }
+
             return dataTable;
         }
 
@@ -178,13 +182,13 @@ namespace RoundtripDataTable
                 case JsonValueKind.False:
                     return typeof(System.Boolean);
                 case JsonValueKind.Undefined:
-                    return null;
+                    throw new NotSupportedException();
                 case JsonValueKind.Object:
                     return typeof(System.Object);
                 case JsonValueKind.Array:
                     return typeof(System.Array);
                 case JsonValueKind.Null:
-                    return null;
+                    throw new NotSupportedException();
                 default:
                     return typeof(System.Object);
             }
@@ -194,8 +198,9 @@ namespace RoundtripDataTable
         {
             switch (jsonElement.ValueKind)
             {
-                case JsonValueKind.Object:      // (these need special handling)?
+                case JsonValueKind.Object:
                 case JsonValueKind.Array:
+                    throw new NotSupportedException();
                 case JsonValueKind.String:
                     if (jsonElement.TryGetGuid(out Guid guidValue))
                     {
@@ -241,37 +246,37 @@ namespace RoundtripDataTable
 //output:
 //[
 //  {
-//    "Date": "2019-08-01T00:00:00-07:00",
+//    "Date": "2019-08-01T00:00:00",
 //    "TemperatureCelsius": 25,
 //    "Summary": "Hot"
 //  },
 //  {
-//    "Date": "2019-08-02T00:00:00-07:00",
+//    "Date": "2019-08-02T00:00:00",
 //    "TemperatureCelsius": 20,
 //    "Summary": "Warm"
 //  },
 //  {
-//    "Date": "2019-08-03T00:00:00-07:00",
+//    "Date": "2019-08-03T00:00:00",
 //    "TemperatureCelsius": 10,
 //    "Summary": "Cold"
 //  }
 //]
-//8/1/2019 12:00:00 AM -07:00 25 Hot
-//8/2/2019 12:00:00 AM -07:00 20 Warm
-//8/3/2019 12:00:00 AM -07:00 10 Cold
+//8/1/2019 12:00:00 AM 25 Hot
+//8/2/2019 12:00:00 AM 20 Warm
+//8/3/2019 12:00:00 AM 10 Cold
 //[
 //  {
-//    "Date": "2019-08-01T00:00:00-07:00",
+//    "Date": "8/1/2019 12:00:00 AM",
 //    "TemperatureCelsius": 25,
 //    "Summary": "Hot"
 //  },
 //  {
-//    "Date": "2019-08-02T00:00:00-07:00",
+//    "Date": "8/2/2019 12:00:00 AM",
 //    "TemperatureCelsius": 20,
 //    "Summary": "Warm"
 //  },
 //  {
-//    "Date": "2019-08-03T00:00:00-07:00",
+//    "Date": "8/3/2019 12:00:00 AM",
 //    "TemperatureCelsius": 10,
 //    "Summary": "Cold"
 //  }

8852576dbc65b4a0cba7d7af41c2f43b3310928b
Tom Dykstra <tdykstra@microsoft.com>
2021-06-28 09:26:48 -0700

    Document IAsyncEnumerable support (#24783)

---
+++ docs/standard/serialization/snippets/system-text-json-supported-collection-types/csharp/IAsyncEnumerableDeserializeNonStreaming.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace IAsyncEnumerableDeserializeNonStreaming
+{
+    public class MyPoco
+    {
+        public IAsyncEnumerable<int> Data { get; set; }
+    }
+
+    public class Program
+    {
+        public static async Task Main()
+        {
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes(@"{""Data"":[0,1,2,3,4]}"));
+            var result = await JsonSerializer.DeserializeAsync<MyPoco>(stream);
+            await foreach (int item in result.Data)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+// output:
+//0
+//1
+//2
+//3
+//4

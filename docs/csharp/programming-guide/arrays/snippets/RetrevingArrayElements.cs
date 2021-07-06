105f0b1951aeff7e57a872a3dc4746ac22db9f23
Rubens Vinicius Apolinário <vinicius.ap@live.com>
2021-07-06 13:33:56 -0300

    Add example for Retreving Array Elements (#24993)
    * Add example for Retreving Array Elements
    
    * Update docs/csharp/programming-guide/arrays/single-dimensional-arrays.md
    
    Co-authored-by: Bill Wagner <wiwagn@microsoft.com>
    
    * Taking off the example of exception as sugested
    
    Co-authored-by: Bill Wagner <wiwagn@microsoft.com>

---
+++ docs/csharp/programming-guide/arrays/snippets/RetrevingArrayElements.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace arrays
+{
+    public class RetrevingArrayElements
+    {
+        public static void Examples()
+        {
+            Retreving();
+        }
+
+        private static void Retreving()
+        {
+            //<RetrevingDataArray>
+            string[] weekDays2 = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+
+            Console.WriteLine(weekDays2[0]);
+            Console.WriteLine(weekDays2[1]);
+            Console.WriteLine(weekDays2[2]);
+            Console.WriteLine(weekDays2[3]);
+            Console.WriteLine(weekDays2[4]);
+            Console.WriteLine(weekDays2[5]);
+            Console.WriteLine(weekDays2[6]);
+
+            /*Output:
+            Sun
+            Mon
+            Tue
+            Wed
+            Thu
+            Fri
+            Sat
+            */
+            //</RetrevingDataArray>
+        }
+    }
+}

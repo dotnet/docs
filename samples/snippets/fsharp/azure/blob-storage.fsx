6d2b6bdd23ff7f020e8782d807aeb6277e782219
zedy <zedy@wicresoft.com>
2021-07-09 17:44:30 +0800

    Updating blob storage to the new SDKs

--- samples/snippets/fsharp/azure/blob-storage.fsx
+++ samples/snippets/fsharp/azure/blob-storage.fsx
@@ -1,8 +1,10 @@
 open System
 open System.IO
-open Microsoft.Azure // Namespace for CloudConfigurationManager
-open Microsoft.Azure.Storage // Namespace for CloudStorageAccount
-open Microsoft.Azure.Storage.Blob // Namespace for Blob storage types
+open Azure.Storage.Blobs // Namespace for Blob storage types
+open Azure.Storage.Blobs.Models
+open Azure.Storage.Blobs.Specialized
+open System.Text
+open System.Collections.Generic
 
 //
 // Get your connection string.
@@ -14,12 +16,6 @@ let storageConnString = "..." // fill this in from your storage account
 let storageConnString = 
     CloudConfigurationManager.GetSetting("StorageConnectionString")
 *)
-//
-// Parse the connection string.
-//
-
-// Parse the connection string and return a reference to the storage account.
-let storageAccount = CloudStorageAccount.Parse(storageConnString)
 
 //
 // Create some local dummy data.
@@ -33,76 +29,61 @@ File.WriteAllText(localFile, "some data")
 // Create the blob service client.
 //
 
-let blobClient = storageAccount.CreateCloudBlobClient()
+let service = new BlobServiceClient(storageConnString);
 
 //
 // Create a container.
 //
 
  // Retrieve a reference to a container.
-let container = blobClient.GetContainerReference("mydata")
+let container = service.GetBlobContainerClient("myContainer");
 
 // Create the container if it doesn't already exist.
 container.CreateIfNotExists()
 
-let permissions = BlobContainerPermissions(PublicAccess=BlobContainerPublicAccessType.Blob)
-container.SetPermissions(permissions)
+let permissions = PublicAccessType.Blob
+container.SetAccessPolicy(permissions)
 
 //
 // Upload a blob into a container.
 //
 
 // Retrieve reference to a blob named "myblob.txt".
-let blockBlob = container.GetBlockBlobReference("myblob.txt")
+let blockBlob = container.GetBlockBlobClient("myblob.txt")
 
 // Create or overwrite the "myblob.txt" blob with contents from the local file.
-do blockBlob.UploadFromFile(localFile)
-
-
+let fileStream = new FileStream(localFile, FileMode.Open, FileAccess.Read, FileShare.Read)
+let offset = 0
+let length = int(fileStream.Length)
+let bytes = Array.zeroCreate<byte>(length)
+fileStream.Read(bytes, offset, bytes.Length)
+fileStream.Close();
+let stream = new MemoryStream(bytes)
+do blockBlob.Upload(stream)
 
 //
 // List the blobs in a container.
 //
 
-// Loop over items within the container and output the length and URI.
-for item in container.ListBlobs(null, false) do
-    match item with 
-    | :? CloudBlockBlob as blob -> 
-        printfn "Block blob of length %d: %O" blob.Properties.Length blob.Uri
-
-    | :? CloudPageBlob as pageBlob ->
-        printfn "Page blob of length %d: %O" pageBlob.Properties.Length pageBlob.Uri
-
-    | :? CloudBlobDirectory as directory ->
-        printfn "Directory: %O" directory.Uri
-
-    | _ ->
-        printfn "Unknown blob type: %O" (item.GetType())
-
-// Loop over items within the container and output the length and URI.
-for item in container.ListBlobs(null, true) do
-    match item with 
-    | :? CloudBlockBlob as blob -> 
-        printfn "Block blob of length %d: %O" blob.Properties.Length blob.Uri
-
-    | _ ->
-        printfn "Unexpected blob type: %O" (item.GetType())
+// Loop over items within the container and output the name.
+for item in container.GetBlobs() do
+    printfn "Blob name: %s" item.Name 
 
 //
 // Download Blobs.
 //
 
 // Retrieve reference to a blob named "myblob.txt".
-let blobToDownload = container.GetBlockBlobReference("myblob.txt")
+let blobToDownload = container.GetBlobClient("myblob.txt")
 
 // Save blob contents to a file.
 do
     use fileStream = File.OpenWrite(__SOURCE_DIRECTORY__ + "/path/download.txt")
-    blobToDownload.DownloadToStream(fileStream)
+    blobToDownload.DownloadTo(fileStream)
 
 let text =
     use memoryStream = new MemoryStream()
-    blobToDownload.DownloadToStream(memoryStream)
+    blobToDownload.DownloadTo(memoryStream)
     Text.Encoding.UTF8.GetString(memoryStream.ToArray())
 
 //
@@ -110,7 +91,7 @@ let text =
 //
 
 // Retrieve reference to a blob named "myblob.txt".
-let blobToDelete = container.GetBlockBlobReference("myblob.txt")
+let blobToDelete = container.GetBlobClient("myblob.txt")
 
 // Delete the blob.
 blobToDelete.Delete()
@@ -119,7 +100,7 @@ blobToDelete.Delete()
 // List blobs in pages asynchronously
 //
 
-let ListBlobsSegmentedInFlatListing(container:CloudBlobContainer) =
+let ListBlobsSegmentedInFlatListing(container:BlobContainerClient) =
     async {
 
         // List blobs to the console window, with paging.
@@ -130,40 +111,22 @@ let ListBlobsSegmentedInFlatListing(container:CloudBlobContainer) =
         // When the continuation token is null, the last page has been 
         // returned and execution can exit the loop.
 
-        let rec loop continuationToken (i:int) = 
-            async {
-                let! ct = Async.CancellationToken
-                // This overload allows control of the page size. You can return
-                // all remaining results by passing null for the maxResults 
-                // parameter, or by calling a different overload.
-                let! resultSegment = 
-                    container.ListBlobsSegmentedAsync(
-                        "", true, BlobListingDetails.All, Nullable 10, 
-                        continuationToken, null, null, ct) 
-                    |> Async.AwaitTask
+        let blobs = new List<BlobItem>()
 
-                if (resultSegment.Results |> Seq.length > 0) then
-                    printfn "Page %d:" i
+        for page in container.GetBlobs().AsPages() do
+            blobs.AddRange(page.Values)
 
-                for blobItem in resultSegment.Results do
-                    printfn "\t%O" blobItem.StorageUri.PrimaryUri
+        for blobItem in blobs do 
+            printf "The BlobItem is : %s " blobItem.Name
+        printfn ""
 
-                printfn ""
-
-                // Get the continuation token.
-                let continuationToken = resultSegment.ContinuationToken
-                if (continuationToken <> null) then
-                    do! loop continuationToken (i+1)
-            }
-    
-        do! loop null 1
     }
 
 // Create some dummy data by uploading the same file over and over again
 for i in 1 .. 100 do
-    let blob  = container.GetBlockBlobReference("myblob" + string i + ".txt")
+    let blob  = container.GetBlobClient("myblob" + string i + ".txt")
     use fileStream = System.IO.File.OpenRead(localFile)
-    blob.UploadFromFile(localFile)
+    blob.Upload(localFile)
 
 ListBlobsSegmentedInFlatListing container |> Async.RunSynchronously
 
@@ -172,32 +135,34 @@ ListBlobsSegmentedInFlatListing container |> Async.RunSynchronously
 //
 
 // Get a reference to a container.
-let appendContainer = blobClient.GetContainerReference("my-append-blobs")
+let appendContainer = service.GetBlobContainerClient("my-append-blobs")
 
 // Create the container if it does not already exist.
 appendContainer.CreateIfNotExists() |> ignore
 
 // Get a reference to an append blob.
-let appendBlob = appendContainer.GetAppendBlobReference("append-blob.log")
+let appendBlob = appendContainer.GetAppendBlobClient("append-blob.log")
 
 // Create the append blob. Note that if the blob already exists, the 
 // CreateOrReplace() method will overwrite it. You can check whether the 
 // blob exists to avoid overwriting it by using CloudAppendBlob.Exists().
-appendBlob.CreateOrReplace()
+appendBlob.CreateIfNotExists()
 
 let numBlocks = 10
 
 // Generate an array of random bytes.
 let rnd = new Random()
-let bytes = Array.zeroCreate<byte>(numBlocks)
-rnd.NextBytes(bytes)
+let bytesArray = Array.zeroCreate<byte>(numBlocks)
+rnd.NextBytes(bytesArray)
 
 // Simulate a logging operation by writing text data and byte data to the 
 // end of the append blob.
 for i in 0 .. numBlocks - 1 do
-    let msg = sprintf "Timestamp: %u \tLog Entry: %d\n" DateTime.UtcNow bytes.[i]
-    appendBlob.AppendText(msg)
+    let msg = sprintf "Timestamp: %A \tLog Entry: %d\n" DateTime.UtcNow bytesArray.[i]
+    let array = Encoding.ASCII.GetBytes(msg);
+    let stream = new MemoryStream(array)
+    appendBlob.AppendBlock(stream)
 
 // Read the append blob to the console window.
-let downloadedText = appendBlob.DownloadText()
+let downloadedText = appendBlob.DownloadContent().ToString()
 printfn "%s" downloadedText

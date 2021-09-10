75fe84f081f3a478ed33b5f5c315b2ad63348541
David Pine <david.pine@microsoft.com>
2021-09-10 07:50:05 -0500

    Address peer feedback

--- docs/csharp/programming-guide/concepts/linq/snippets/extensions/LinqExtensions.cs
+++ docs/csharp/programming-guide/concepts/linq/snippets/extensions/LinqExtensions.cs
@@ -1,24 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace linq
+namespace Custom.Linq.Extensions
 {
     // <LinqExtensionClass>
-    public static class LINQExtension
+    public static class EnumerableExtension
     {
         public static double Median(this IEnumerable<double>? source)
         {
-            if (!(source?.Any() ?? false))
+            if (source is null || !source.Any())
             {
                 throw new InvalidOperationException("Cannot compute median for a null or empty set.");
             }
 
-            var sortedList = (from number in source
-                              orderby number
-                              select number).ToList();
+            var sortedList =
+                source.OrderBy(number => number).ToList();
 
             int itemIndex = sortedList.Count / 2;
 
@@ -39,15 +36,15 @@ namespace linq
     public static class OtherExtensions
     {
         // <IntOverload>
-        //int overload
+        // int overload
         public static double Median(this IEnumerable<int> source) =>
-            (from num in source select (double)num).Median();
+            (from number in source select (double)number).Median();
         // </IntOverload>
 
         // <GenericOverload>
-        // Generic overload.
-        public static double Median<T>(this IEnumerable<T> numbers,
-                               Func<T, double> selector) =>
+        // generic overload
+        public static double Median<T>(
+            this IEnumerable<T> numbers, Func<T, double> selector) =>
             (from num in numbers select selector(num)).Median();
         // </GenericOverload>
 
@@ -56,18 +53,17 @@ namespace linq
         // The method returns every other element of a sequence.
         public static IEnumerable<T> AlternateElements<T>(this IEnumerable<T> source)
         {
-            int i = 0;
-            foreach (var element in source)
+            int index = 0;
+            foreach (T element in source)
             {
-                if (i % 2 == 0)
+                if (index % 2 == 0)
                 {
                     yield return element;
                 }
-                i++;
+
+                index++;
             }
         }
         // </SequenceElement>
-
-
     }
 }
---
+++ docs/csharp/programming-guide/concepts/linq/snippets/extensions/Program.cs
@@ -0,0 +1,63 @@
+﻿using Custom.Linq.Extensions;
+
+// <MedianUsage>
+double[] numbers = { 1.9, 2, 8, 4, 5.7, 6, 7.2, 0 };
+var query = numbers.Median();
+
+Console.WriteLine($"double: Median = {query}");
+// This code produces the following output:
+//     double: Median = 4.85
+// </MedianUsage>
+
+// <OverloadUsage>
+double[] numbers1 = { 1.9, 2, 8, 4, 5.7, 6, 7.2, 0 };
+var query1 = numbers1.Median();
+
+Console.WriteLine($"double: Median = {query1}");
+
+int[] numbers2 = { 1, 2, 3, 4, 5 };
+var query2 = numbers2.Median();
+
+Console.WriteLine($"int: Median = {query2}");
+// This code produces the following output:
+//     double: Median = 4.85
+//     int: Median = 3
+// </OverloadUsage>
+
+// <GenericUsage>
+int[] numbers3 = { 1, 2, 3, 4, 5 };
+
+/*
+    You can use the num => num lambda expression as a parameter for the Median method
+    so that the compiler will implicitly convert its value to double.
+    If there is no implicit conversion, the compiler will display an error message.
+*/
+var query3 = numbers3.Median(num => num);
+
+Console.WriteLine($"int: Median = {query3}");
+
+string[] numbers4 = { "one", "two", "three", "four", "five" };
+
+// With the generic overload, you can also use numeric properties of objects.
+var query4 = numbers4.Median(str => str.Length);
+
+Console.WriteLine($"string: Median = {query4}");
+// This code produces the following output:
+//     int: Median = 3
+//     string: Median = 4
+// </GenericUsage>
+
+// <SequenceUsage>
+string[] strings = { "a", "b", "c", "d", "e" };
+
+var query5 = strings.AlternateElements();
+
+foreach (var element in query5)
+{
+    Console.WriteLine(element);
+}
+// This code produces the following output:
+//     a
+//     c
+//     e
+// </SequenceUsage>

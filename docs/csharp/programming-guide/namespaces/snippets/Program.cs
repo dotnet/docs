949dbd29b6377230f4ec4f5dc91b8fff0f8aaea4
Bill Wagner <wiwagn@microsoft.com>
2021-09-10 15:47:54 -0400

    remove the outdated articles

--- docs/csharp/programming-guide/namespaces/snippets/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-// <Snippet31>
-Console.WriteLine("Hello, World!");
-//</Snippet31>
-
-//<Snippet30>
-System.Console.WriteLine("Hello, World!");
-//</Snippet30>
-

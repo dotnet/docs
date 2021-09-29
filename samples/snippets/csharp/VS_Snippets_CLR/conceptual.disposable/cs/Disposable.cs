402bbea278fb9c215c0f8ad15353e79cdfac2b52
David Pine <david.pine@microsoft.com>
2021-09-29 09:06:34 -0500

    Corrected issues introduced from #26192 and add enable implicit usings in new LINQ bits

--- samples/snippets/csharp/VS_Snippets_CLR/conceptual.disposable/cs/Disposable.cs
+++ samples/snippets/csharp/VS_Snippets_CLR/conceptual.disposable/cs/Disposable.cs
@@ -16,18 +16,20 @@ public class Disposable : IDisposable
     // <SnippetDisposeBool>
     protected virtual void Dispose(bool disposing)
     {
-    	if (_disposed)    	
-    		return;       	
-    		
-    	// A block that frees unmanaged resources.	
-    	
-    	if(disposing) 
-    	{
-	    	// Deterministic call…
-    		// A conditional block that frees managed resources.    	
-	    }	    
-	    
-	    _disposed = true;	    
+        if (_disposed)
+        {
+            return;
+        }
+
+        if (disposing)
+        {
+            // TODO: dispose managed state (managed objects).
+        }
+
+        // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+        // TODO: set large fields to null.
+
+        _disposed = true;
     }
     // </SnippetDisposeBool>
 }

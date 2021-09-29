dotnet/docs

commit d7088311618f757fd46b11f95d084aa51165a3dd

Implement dispose method fixes (#26192)
* NotOverridable instead of NonInheritable

* Some clarification on object resource freeing up

* Dubious statement removal

* Clarification of Dispose(), Dispose(bool) introduction

* Csprojs fix

* Cascade dispose calls Foo simplification

* Double blank line removal

* ImplementTheDisposePattern fixes

* ImplementTheDisposePatternForADerivedClass fixes

* Duplicate examples removal

* File examples removal

Co-authored-by: Jiří <Jiri.Cepelka@Volny.cz>

samples/snippets/csharp/VS_Snippets_CLR/conceptual.disposable/cs/Disposable.cs
@@ -2,6 +2,7 @@
 
 public class Disposable : IDisposable
 {
+    private bool _disposed;
     // <SnippetDispose>
     public void Dispose()
     {
@@ -15,6 +16,18 @@ public class Disposable : IDisposable
     // <SnippetDisposeBool>
     protected virtual void Dispose(bool disposing)
     {
+    	if (_disposed)    	
+    		return;       	
+    		
+    	// A block that frees unmanaged resources.	
+    	
+    	if(disposing) 
+    	{
+	    	// Deterministic call…
+    		// A conditional block that frees managed resources.    	
+	    }	    
+	    
+	    _disposed = true;	    
     }
     // </SnippetDisposeBool>
 }

samples/snippets/csharp/VS_Snippets_CLR/conceptual.disposable/cs/Foo.cs
@@ -2,15 +2,7 @@
 
 public sealed class Foo : IDisposable
 {
-    private readonly IDisposable _bar;
-
-    public Foo()
-    {
-        _bar = new Bar();
-    }
-
-    public void Dispose()
-    {
-        _bar?.Dispose();
-    }
+    // Model simplified for brevity sake.
+    private readonly IDisposable _bar = new Bar();
+    public void Dispose() => _bar.Dispose();
 }

samples/snippets/csharp/VS_Snippets_CLR_System/system.idisposable/cs/base1.cs
@@ -23,8 +23,7 @@ class BaseClassWithSafeHandle : IDisposable
 
         if (disposing)
         {
-           // Dispose managed state (managed objects).
-            _safeHandle?.Dispose();
+            _safeHandle.Dispose();
         }
 
         _disposed = true;

samples/snippets/csharp/VS_Snippets_CLR_System/system.idisposable/cs/base2.cs
@@ -24,11 +24,11 @@ class BaseClassWithFinalizer : IDisposable
 
         if (disposing)
         {
-            // TODO: dispose managed state (managed objects).
+        	// Dispose managed objects that implement IDisposable.
+	        // Assign null to managed objects that consume large amounts of memory or consume scarce resources.
         }
 
-        // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-        // TODO: set large fields to null.
+        // Free unmanaged resources (unmanaged objects).
 
         _disposed = true;
     }

samples/snippets/csharp/VS_Snippets_CLR_System/system.idisposable/cs/derived1.cs
@@ -20,8 +20,7 @@ class DerivedClassWithSafeHandle : BaseClassWithSafeHandle
 
         if (disposing)
         {
-           // Dispose managed state (managed objects).
-            _safeHandle?.Dispose();
+            _safeHandle.Dispose();
         }
 
         _disposed = true;

samples/snippets/csharp/VS_Snippets_CLR_System/system.idisposable/cs/derived2.cs
@@ -15,11 +15,11 @@
 
         if (disposing)
         {
-            // TODO: dispose managed state (managed objects).
+            // Dispose managed objects that implement IDisposable.
+	        // Assign null to managed objects that consume large amounts of memory or consume scarce resources.
         }
 
-        // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-        // TODO: set large fields to null.
+        // Free unmanaged resources (unmanaged objects).
         _disposed = true;
 
         // Call the base class implementation.

samples/snippets/csharp/VS_Snippets_CLR/conceptual.disposable/cs/DisposableStreamResource.cs
@@ -1,79 +1,0 @@
-﻿using Microsoft.Win32.SafeHandles;
-using System;
-using System.Runtime.InteropServices;
-
-public class DisposableStreamResource : IDisposable
-{
-    // Define constants.
-    protected const uint GENERIC_READ = 0x80000000;
-    protected const uint FILE_SHARE_READ = 0x00000001;
-    protected const uint OPEN_EXISTING = 3;
-    protected const uint FILE_ATTRIBUTE_NORMAL = 0x80;
-    private const int INVALID_FILE_SIZE = unchecked((int)0xFFFFFFFF);
-
-    // Define Windows APIs.
-    [DllImport("kernel32.dll", EntryPoint = "CreateFileW", CharSet = CharSet.Unicode)]
-    protected static extern SafeFileHandle CreateFile(
-        string lpFileName, uint dwDesiredAccess,
-        uint dwShareMode, IntPtr lpSecurityAttributes,
-        uint dwCreationDisposition, uint dwFlagsAndAttributes,
-        IntPtr hTemplateFile);
-
-    [DllImport("kernel32.dll")]
-    private static extern int GetFileSize(
-        SafeFileHandle hFile, out int lpFileSizeHigh);
-
-    // Define locals.
-    private bool _disposed = false;
-    private readonly SafeFileHandle? _safeHandle;
-    private readonly int _upperWord;
-
-    public DisposableStreamResource(string fileName)
-    {
-        if (string.IsNullOrWhiteSpace(fileName))
-        {
-            throw new ArgumentException("The fileName cannot be null or an empty string");
-        }
-
-        _safeHandle = CreateFile(
-            fileName, GENERIC_READ, FILE_SHARE_READ, IntPtr.Zero,
-            OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, IntPtr.Zero);
-
-        // Get file size.
-        Size = GetFileSize(_safeHandle, out _upperWord);
-        if (Size == INVALID_FILE_SIZE)
-        {
-            Size = -1;
-        }
-        else if (_upperWord > 0)
-        {
-            Size = (((long)_upperWord) << 32) + Size;
-        }
-    }
-
-    public long Size { get; }
-
-    public void Dispose()
-    {
-        Dispose(true);
-        GC.SuppressFinalize(this);
-    }
-
-    protected virtual void Dispose(bool disposing)
-    {
-        if (_disposed)
-        {
-            return;
-        }
-
-        // Dispose of managed resources here.
-        if (disposing)
-        {
-            _safeHandle?.Dispose();
-        }
-
-        // Dispose of any unmanaged resources not wrapped in safe handles.
-
-        _disposed = true;
-    }
-}

samples/snippets/csharp/VS_Snippets_CLR/conceptual.disposable/cs/DisposableStreamResource2.cs
@@ -1,63 +1,0 @@
-﻿using Microsoft.Win32.SafeHandles;
-using System;
-using System.Runtime.InteropServices;
-
-public class DisposableStreamResource2 : DisposableStreamResource
-{
-    // Define additional constants.
-    protected const uint GENERIC_WRITE = 0x40000000;
-    protected const uint OPEN_ALWAYS = 4;
-
-    // Define additional APIs.
-    [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
-    protected static extern bool WriteFile(
-        SafeFileHandle safeHandle, string lpBuffer,
-        int nNumberOfBytesToWrite, out int lpNumberOfBytesWritten,
-        IntPtr lpOverlapped);
-
-    // To detect redundant calls
-    private bool _disposed = false;
-    private bool _created = false;
-    private SafeFileHandle _safeHandle;
-    private readonly string _fileName;
-
-    public DisposableStreamResource2(string fileName) : base(fileName) => _fileName = fileName;
-
-    public void WriteFileInfo()
-    {
-        if (!_created)
-        {
-            _safeHandle = CreateFile(
-                @".\FileInfo.txt", GENERIC_WRITE, 0, IntPtr.Zero,
-                OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, IntPtr.Zero);
-
-            _created = true;
-        }
-
-        string output = $"{_fileName}: {Size:N0} bytes\n";
-        _ = WriteFile(_safeHandle, output, output.Length, out _, IntPtr.Zero);
-    }
-
-    protected override void Dispose(bool disposing)
-    {
-        if (_disposed)
-        {
-            return;
-        }
-
-        // Release any managed resources here.
-        if (disposing)
-        {
-            // Dispose managed state (managed objects).
-            _safeHandle?.Dispose();
-        }
-
-        // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-        // TODO: set large fields to null.
-
-        _disposed = true;
-
-        // Call the base class implementation.
-        base.Dispose(disposing);
-    }
-}

commit 402bbea278fb9c215c0f8ad15353e79cdfac2b52

Corrected issues introduced from #26192 and add enable implicit usings in new LINQ bits

docs/csharp/programming-guide/concepts/linq/snippets/extensions/linq.csproj
@@ -4,6 +4,7 @@
     <OutputType>Exe</OutputType>
     <TargetFramework>net6.0</TargetFramework>
     <nullable>enable</nullable>
+    <ImplicitUsings>true</ImplicitUsings>
   </PropertyGroup>
 
 </Project>

docs/csharp/programming-guide/concepts/linq/snippets/partition/partition.csproj
@@ -4,6 +4,7 @@
     <OutputType>Exe</OutputType>
     <TargetFramework>net6.0</TargetFramework>
     <Nullable>enable</Nullable>
+    <ImplicitUsings>true</ImplicitUsings>
   </PropertyGroup>
 
 </Project>

docs/csharp/programming-guide/concepts/linq/snippets/projection/projection.csproj
@@ -4,6 +4,7 @@
     <OutputType>Exe</OutputType>
     <TargetFramework>net6.0</TargetFramework>
     <Nullable>enable</Nullable>
+    <ImplicitUsings>true</ImplicitUsings>
   </PropertyGroup>
 
 </Project>

docs/csharp/programming-guide/concepts/linq/snippets/set-operators/set-operators.csproj
@@ -3,7 +3,8 @@
   <PropertyGroup>
     <OutputType>Exe</OutputType>
     <TargetFramework>net6.0</TargetFramework>
-    <nullable>enable</nullable>
+    <Nullable>enable</Nullable>
+    <ImplicitUsings>true</ImplicitUsings>
   </PropertyGroup>
 
 </Project>

samples/snippets/csharp/VS_Snippets_CLR/conceptual.disposable/cs/Disposable.cs
@@ -16,18 +16,20 @@ public class Disposable : IDisposable
     // <SnippetDisposeBool>
     protected virtual void Dispose(bool disposing)
     {
-    	if (_disposed)    	
-    		return;       	
-    		
-    	// A block that frees unmanaged resources.	
-    	
-    	if(disposing) 
-    	{
-	    	// Deterministic call…
-    		// A conditional block that frees managed resources.    	
-	    }	    
-	    
-	    _disposed = true;	    
+        if (_disposed)
+        {
+            return;
+        }
+
+        if (disposing)
+        {
+            // TODO: dispose managed state (managed objects).
+        }
+
+        // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+        // TODO: set large fields to null.
+
+        _disposed = true;
     }
     // </SnippetDisposeBool>
 }

samples/snippets/csharp/VS_Snippets_CLR/conceptual.disposable/cs/Foo.cs
@@ -2,7 +2,12 @@
 
 public sealed class Foo : IDisposable
 {
-    // Model simplified for brevity sake.
-    private readonly IDisposable _bar = new Bar();
-    public void Dispose() => _bar.Dispose();
+    private readonly IDisposable _bar;
+
+    public Foo()
+    {
+        _bar = new Bar();
+    }
+
+    public void Dispose() => _bar?.Dispose();
 }

samples/snippets/csharp/VS_Snippets_CLR_System/system.idisposable/cs/base1.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 class BaseClassWithSafeHandle : IDisposable
 {
     // To detect redundant calls
-    private bool _disposed = false;
+    private bool _disposedValue;
 
     // Instantiate a SafeHandle instance.
     private SafeHandle _safeHandle = new SafeFileHandle(IntPtr.Zero, true);
@@ -16,16 +16,14 @@ class BaseClassWithSafeHandle : IDisposable
     // Protected implementation of Dispose pattern.
     protected virtual void Dispose(bool disposing)
     {
-        if (_disposed)
+        if (!_disposedValue)
         {
-            return;
-        }
+            if (disposing)
+            {
+                _safeHandle.Dispose();
+            }
 
-        if (disposing)
-        {
-            _safeHandle.Dispose();
+            _disposedValue = true;
         }
-
-        _disposed = true;
     }
 }

samples/snippets/csharp/VS_Snippets_CLR_System/system.idisposable/cs/base2.cs
@@ -3,7 +3,7 @@
 class BaseClassWithFinalizer : IDisposable
 {
     // To detect redundant calls
-    private bool _disposed = false;
+    private bool _disposedValue;
 
     ~BaseClassWithFinalizer() => Dispose(false);
 
@@ -17,19 +17,16 @@ class BaseClassWithFinalizer : IDisposable
     // Protected implementation of Dispose pattern.
     protected virtual void Dispose(bool disposing)
     {
-        if (_disposed)
+        if (!_disposedValue)
         {
-            return;
+            if (disposing)
+            {
+                // TODO: dispose managed state (managed objects)
+            }
+
+            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            // TODO: set large fields to null
+            _disposedValue = true;
         }
-
-        if (disposing)
-        {
-        	// Dispose managed objects that implement IDisposable.
-	        // Assign null to managed objects that consume large amounts of memory or consume scarce resources.
-        }
-
-        // Free unmanaged resources (unmanaged objects).
-
-        _disposed = true;
     }
 }

samples/snippets/csharp/VS_Snippets_CLR_System/system.idisposable/cs/derived1.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 class DerivedClassWithSafeHandle : BaseClassWithSafeHandle
 {
     // To detect redundant calls
-    private bool _disposed = false;
+    private bool _disposedValue;
 
     // Instantiate a SafeHandle instance.
     private SafeHandle _safeHandle = new SafeFileHandle(IntPtr.Zero, true);
@@ -13,17 +13,15 @@ class DerivedClassWithSafeHandle : BaseClassWithSafeHandle
     // Protected implementation of Dispose pattern.
     protected override void Dispose(bool disposing)
     {
-        if (_disposed)
+        if (!_disposedValue)
         {
-            return;
-        }
+            if (disposing)
+            {
+                _safeHandle.Dispose();
+            }
 
-        if (disposing)
-        {
-            _safeHandle.Dispose();
+            _disposed = true;
         }
-
-        _disposed = true;
 
         // Call base class implementation.
         base.Dispose(disposing);

samples/snippets/csharp/VS_Snippets_CLR_System/system.idisposable/cs/derived2.cs
@@ -1,26 +1,24 @@
 ﻿class DerivedClassWithFinalizer : BaseClassWithFinalizer
 {
     // To detect redundant calls
-    bool _disposed = false;
+    private bool _disposedValue;
 
     ~DerivedClassWithFinalizer() => this.Dispose(false);
 
     // Protected implementation of Dispose pattern.
     protected override void Dispose(bool disposing)
     {
-        if (_disposed)
+        if (!_disposedValue)
         {
-            return;
-        }
+            if (disposing)
+            {
+                // TODO: dispose managed state (managed objects).
+            }
 
-        if (disposing)
-        {
-            // Dispose managed objects that implement IDisposable.
-	        // Assign null to managed objects that consume large amounts of memory or consume scarce resources.
+            // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+            // TODO: set large fields to null.
+            _disposedValue = true;
         }
-
-        // Free unmanaged resources (unmanaged objects).
-        _disposed = true;
 
         // Call the base class implementation.
         base.Dispose(disposing);
62fa0f958b8bada00d1db66c62858cb9e2990c82
Bill Wagner <wiwagn@microsoft.com>
2021-10-20 21:19:37 -0400

    Csharp10 interpolated strings (#26456)
    * language reference updates made
    
    * add outline
    
    * first draft of tutorial finished
    
    Code completed, first draft of article is done.
    
    * Apply suggestions from code review
    
    Co-authored-by: Genevieve Warren <24882762+gewarren@users.noreply.github.com>
    
    * fix build warnings
    
    * grammar updates
    
    * Update docs/csharp/whats-new/tutorials/interpolated-string-handler.md
    
    * Add notes to what's new page
    
    * Apply suggestions from code review
    
    Co-authored-by: Fred Silberberg <fred@silberberg.xyz>
    
    * Update tutorial per offline discussion
    
    - Add discussion of short circuiting
    - Discussing additional formatting overloads
    
    * Fix #26566
    
    It's in the same area, so add the necessary link
    
    Co-authored-by: Genevieve Warren <24882762+gewarren@users.noreply.github.com>
    Co-authored-by: Fred Silberberg <fred@silberberg.xyz>

---
+++ docs/csharp/whats-new/tutorials/snippets/interpolated-string-handler/Logger-v1.cs
@@ -0,0 +1,25 @@
+﻿namespace interpolated_string_handler.Version1
+{
+    // <InitialLogger>
+    public enum LogLevel
+    {
+        Off,
+        Critical,
+        Error,
+        Warning,
+        Information,
+        Trace
+    }
+
+    public class Logger
+    {
+        public LogLevel EnabledLevel { get; init; } = LogLevel.Error;
+
+        public void LogMessage(LogLevel level, string msg)
+        {
+            if (EnabledLevel < level) return;
+            Console.WriteLine(msg);
+        }
+    }
+    // </InitialLogger>
+}

b22ee24d649495836d2d307840f93d4a6da553b7
Tom Dykstra <tdykstra@microsoft.com>
2021-10-25 16:47:30 -0700

    draft

---
+++ docs/csharp/programming-guide/concepts/async/snippets/multiple-tasks-6-0/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

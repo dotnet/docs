191932aa2133d3166c040cbe36c6011d12c2e8c1
Tom Dykstra <tdykstra@microsoft.com>
2021-10-29 14:20:12 -0700

    implicit usings

--- docs/standard/serialization/snippets/system-text-json-how-to-6-0/csharp/Program.cs
+++ docs/standard/serialization/snippets/system-text-json-how-to-6-0/csharp/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SystemTextJsonSamples
+﻿namespace SystemTextJsonSamples
 {
     public class Program
     {

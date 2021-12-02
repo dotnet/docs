f79c3ac775ca287f650e8fa61ca3904a2289bcd0
David Pine <david.pine@microsoft.com>
2021-12-02 11:16:21 -0600

    Add `TypeForwardedTo` attribute example source code (#27353)
    * Format and edit pass, add new examples of type forwarding
    
    * Adjust wording and descriptions.
    
    * Strengthen transition
    
    * Finalize

---
+++ docs/standard/assembly/snippets/type-forwarders/before/Utility/Example.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Common.Objects;
+
+public class Example
+{
+    public string Message { get; init; } = "Hi friends!";
+
+    public Guid Id { get; init; } = Guid.NewGuid();
+
+    public DateOnly Date { get; init; } = DateOnly.FromDateTime(DateTime.Today);
+
+    public sealed override string ToString() =>
+        $"[{Id} - {Date}]: {Message}";
+}

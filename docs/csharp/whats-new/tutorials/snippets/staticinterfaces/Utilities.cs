01b15a0bae84348956b997632b611a6e4e549383
Bill Wagner <wiwagn@microsoft.com>
2021-12-13 15:25:14 -0500

    Update docs/csharp/whats-new/tutorials/snippets/staticinterfaces/Utilities.cs
    Co-authored-by: Günther Foidl <gue@korporal.at>

--- docs/csharp/whats-new/tutorials/snippets/staticinterfaces/Utilities.cs
+++ docs/csharp/whats-new/tutorials/snippets/staticinterfaces/Utilities.cs
@@ -2,6 +2,6 @@
 {
     // <MidPoint>
     public static T MidPoint<T>(T left, T right)
-        where T : INumber<T> => (left + right) / (T.One + T.One);
+        where T : INumber<T> => (left + right) / (T.One + T.One);  // note: the addition of left, and right may overflow here, it's just for demonstration purposes
     // </MidPoint>
 }

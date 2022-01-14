ff2c4d7ef66a0a4001b612804122a7f526bda227
Andy De George (adegeo) <adegeo@microsoft.com>
2022-01-13 17:13:30 -0800

    Update for snippets WPF

---
+++ docs/core/porting/snippets/upgrade-assistant-wpf-framework/csharp/WebSiteRatings/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using MahApps.Metro.Controls;
+using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebSiteRatings
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            ViewModels.SiteCollection siteCollection = new ViewModels.SiteCollection();
+            siteCollection.Load();
+            DataContext = siteCollection;
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var siteCollection = (ViewModels.SiteCollection)DataContext;
+
+            if (siteCollection.SelectedSite != null)
+                browser.Source = new Uri(siteCollection.SelectedSite.Url);
+            else
+                browser.NavigateToString("<body></body>");
+        }
+    }
+}

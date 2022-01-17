7311a6f1d53800f1a52cf4826ccc0693fb04a2a7
lindexi <lindexi_gd@163.com>
2022-01-17 21:13:30 +0800

    Fix code format

--- samples/snippets/csharp/VS_Snippets_CLR/conceptual.regularexpressions.design/cs/lazy1.cs
+++ samples/snippets/csharp/VS_Snippets_CLR/conceptual.regularexpressions.design/cs/lazy1.cs
@@ -4,29 +4,29 @@ using System.Text.RegularExpressions;
 
 public class Example
 {
-   public static void Main()
-   {
-      string greedyPattern = @".+(\d+)\.";
-      string lazyPattern = @".+?(\d+)\.";
-      string input = "This sentence ends with the number 107325.";
-      Match match;
+    public static void Main()
+    {
+        string greedyPattern = @".+(\d+)\.";
+        string lazyPattern = @".+?(\d+)\.";
+        string input = "This sentence ends with the number 107325.";
+        Match match;
 
-      // Match using greedy quantifier .+.
-      match = Regex.Match(input, greedyPattern);
-      if (match.Success)
-         Console.WriteLine("Number at end of sentence (greedy): {0}",
-                           match.Groups[1].Value);
-      else
-         Console.WriteLine("{0} finds no match.", greedyPattern);
+        // Match using greedy quantifier .+.
+        match = Regex.Match(input, greedyPattern);
+        if (match.Success)
+            Console.WriteLine("Number at end of sentence (greedy): {0}",
+                              match.Groups[1].Value);
+        else
+            Console.WriteLine("{0} finds no match.", greedyPattern);
 
-      // Match using lazy quantifier .+?.
-      match = Regex.Match(input, lazyPattern);
-      if (match.Success)
-         Console.WriteLine("Number at end of sentence (lazy): {0}",
-                           match.Groups[1].Value);
-      else
-         Console.WriteLine("{0} finds no match.", lazyPattern);
-   }
+        // Match using lazy quantifier .+?.
+        match = Regex.Match(input, lazyPattern);
+        if (match.Success)
+            Console.WriteLine("Number at end of sentence (lazy): {0}",
+                              match.Groups[1].Value);
+        else
+            Console.WriteLine("{0} finds no match.", lazyPattern);
+    }
 }
 // The example displays the following output:
 //       Number at end of sentence (greedy): 5

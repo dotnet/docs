927d101c36348a58aa99ae440def9df89f04b100
Zev Spitz <shivisi@hotmail.com>
2022-02-01 21:34:06 +0200

    Fix #27774

--- samples/snippets/visualbasic/programming-guide/language-features/data-types/tuples/tuple-returns.vb
+++ samples/snippets/visualbasic/programming-guide/language-features/data-types/tuples/tuple-returns.vb
@@ -23,7 +23,7 @@ Module TupleReturns
         Dim number As Integer
         Dim result = Integer.TryParse(numericString, number)
         Console.WriteLine($"{If(result, $"Success: {number:N0}", "Failure")}")
-        '      Output: 123,456
+        '      Output: Success: 123,456
         ' </StandardMethodCall>
     End Sub
 

4b6d86058c439852f1bab9cbf37a52a56c511a96
Bill Wagner <wiwagn@microsoft.com>
2022-02-01 11:08:50 -0500

    Cover Static abstract interfaces (#27503)
    * Finish first draft
    
    * Proofread and check samples
    
    * fix build warnings
    
    * Apply suggestions from code review
    
    Co-authored-by: Günther Foidl <gue@korporal.at>
    
    * Update docs/csharp/whats-new/tutorials/snippets/staticinterfaces/Utilities.cs
    
    Co-authored-by: Günther Foidl <gue@korporal.at>
    
    * Respond to feedback.
    
    Apply suggestions and updates from review.
    
    * Add note on potential overflow
    
    * Apply suggestions from code review
    
    Co-authored-by: Andy (Steve) De George <67293991+adegeo@users.noreply.github.com>
    
    * respond to feedback
    
    * add new tutorial to TOC
    
    * Add link for feedback.
    
    * pin to 60.100
    
    * pin to 6.0.0
    
    * delete global.json
    
    This didn't work in our CI environment.
    
    We don't want to load all the old builds, so wait for the fix on 6.0.101 to merge this.
    
    * Apply suggestions from code review
    
    Co-authored-by: Youssef Victor <youssefvictor00@gmail.com>
    
    * improve wording.
    
    * respond to feedback.
    
    * update experimental package.
    
    Co-authored-by: Günther Foidl <gue@korporal.at>
    Co-authored-by: Andy (Steve) De George <67293991+adegeo@users.noreply.github.com>
    Co-authored-by: Youssef Victor <youssefvictor00@gmail.com>

---
+++ docs/csharp/whats-new/tutorials/snippets/staticinterfaces/RepeatSequence.cs
@@ -0,0 +1,10 @@
+﻿public struct RepeatSequence : IGetNext<RepeatSequence>
+{
+    private const char Ch = 'A';
+    public string Text = new string(Ch, 1);
+
+    public static RepeatSequence operator ++(RepeatSequence other)
+        => other with { Text = other.Text + Ch };
+
+    public override string ToString() => Text;
+}

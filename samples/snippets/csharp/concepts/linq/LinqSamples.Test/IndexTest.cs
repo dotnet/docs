ba824c5cbf8494873a4f42a739aa552c8bd92cc3
Zev Spitz <shivisi@hotmail.com>
2022-02-13 04:06:58 +0200

    index

---
+++ samples/snippets/csharp/concepts/linq/LinqSamples.Test/IndexTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using Xunit;
+
+namespace LinqSamples.Test
+{
+    public class IndexTest
+    {
+        StringWriter sw = new StringWriter();
+        public IndexTest() => Console.SetOut(sw);
+
+        [Fact]
+        public void IntroTest()
+        {
+            Index.Intro();
+            Assert.Equal("97 92 81 ", sw.ToString());
+        }
+    }
+}
--- samples/snippets/csharp/concepts/linq/LinqSamples/Program.cs
+++ samples/snippets/csharp/concepts/linq/LinqSamples/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using l = LinqSamples;
+
+l.Index.Intro();
+
+

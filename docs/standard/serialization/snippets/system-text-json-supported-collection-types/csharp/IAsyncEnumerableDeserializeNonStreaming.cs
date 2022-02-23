d650876fa61a8234bc38cf5acdefc6244b971f7e
Tom Dykstra <tdykstra@microsoft.com>
2022-02-23 12:42:02 -0800

    nrt

--- docs/standard/serialization/snippets/system-text-json-supported-collection-types/csharp/IAsyncEnumerableDeserializeNonStreaming.cs
+++ docs/standard/serialization/snippets/system-text-json-supported-collection-types/csharp/IAsyncEnumerableDeserializeNonStreaming.cs
@@ -1,27 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
+﻿using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 
-namespace IAsyncEnumerableDeserializeNonStreaming
+namespace IAsyncEnumerableDeserializeNonStreaming;
+
+public class MyPoco
 {
-    public class MyPoco
-    {
-        public IAsyncEnumerable<int> Data { get; set; }
-    }
+    public IAsyncEnumerable<int>? Data { get; set; }
+}
 
-    public class Program
+public class Program
+{
+    public static async Task Main()
     {
-        public static async Task Main()
+        var stream = new MemoryStream(Encoding.UTF8.GetBytes(@"{""Data"":[0,1,2,3,4]}"));
+        MyPoco? result = await JsonSerializer.DeserializeAsync<MyPoco>(stream)!;
+        await foreach (int item in result!.Data!)
         {
-            var stream = new MemoryStream(Encoding.UTF8.GetBytes(@"{""Data"":[0,1,2,3,4]}"));
-            var result = await JsonSerializer.DeserializeAsync<MyPoco>(stream);
-            await foreach (int item in result.Data)
-            {
-                Console.WriteLine(item);
-            }
+            Console.WriteLine(item);
         }
     }
 }

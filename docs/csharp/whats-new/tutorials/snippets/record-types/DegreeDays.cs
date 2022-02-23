47f0997c857936309f2846b3223339b20df2a5a4
Tom Dykstra <tdykstra@microsoft.com>
2022-02-23 14:01:36 -0800

    update snippet projects

--- docs/csharp/whats-new/tutorials/snippets/record-types/DegreeDays.cs
+++ docs/csharp/whats-new/tutorials/snippets/record-types/DegreeDays.cs
@@ -1,29 +1,26 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Text;
 
-namespace record_types
+namespace record_types;
+
+public abstract record DegreeDays(double BaseTemperature, IEnumerable<DailyTemperature> TempRecords)
 {
-    public abstract record DegreeDays(double BaseTemperature, IEnumerable<DailyTemperature> TempRecords)
+    // <AddPrintMembers>
+    protected virtual bool PrintMembers(StringBuilder stringBuilder)
     {
-        // <AddPrintMembers>
-        protected virtual bool PrintMembers(StringBuilder stringBuilder)
-        {
-            stringBuilder.Append($"BaseTemperature = {BaseTemperature}");
-            return true;
-        }
-        // </AddPrintMembers>
+        stringBuilder.Append($"BaseTemperature = {BaseTemperature}");
+        return true;
     }
+    // </AddPrintMembers>
+}
 
-    public record HeatingDegreeDays(double BaseTemperature, IEnumerable<DailyTemperature> TempRecords)
-        : DegreeDays(BaseTemperature, TempRecords)
-    {
-        public double DegreeDays => TempRecords.Where(s => s.Mean < BaseTemperature).Sum(s => BaseTemperature - s.Mean);
-    }
+public record HeatingDegreeDays(double BaseTemperature, IEnumerable<DailyTemperature> TempRecords)
+    : DegreeDays(BaseTemperature, TempRecords)
+{
+    public double DegreeDays => TempRecords.Where(s => s.Mean < BaseTemperature).Sum(s => BaseTemperature - s.Mean);
+}
 
-    public sealed record CoolingDegreeDays(double BaseTemperature, IEnumerable<DailyTemperature> TempRecords)
-        : DegreeDays(BaseTemperature, TempRecords)
-    {
-        public double DegreeDays => TempRecords.Where(s => s.Mean > BaseTemperature).Sum(s => s.Mean - BaseTemperature);
-    }
+public sealed record CoolingDegreeDays(double BaseTemperature, IEnumerable<DailyTemperature> TempRecords)
+    : DegreeDays(BaseTemperature, TempRecords)
+{
+    public double DegreeDays => TempRecords.Where(s => s.Mean > BaseTemperature).Sum(s => s.Mean - BaseTemperature);
 }

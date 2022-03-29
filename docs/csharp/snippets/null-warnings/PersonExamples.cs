eb324e7f74c9611f8335af10fbe9964e93566816
Bill Wagner <wiwagn@microsoft.com>
2022-03-29 16:02:19 -0400

    Raise prominence on null warnings for initialization
    Fixes #28531
    
    The nullable docs did point to the `MemberNotNull` and `MemberNotNull` attributes for private initialization. But, there weren't clear examples, and a discussion of the alternative of constructor chaining.
    
    Also, from the overview article, link to the article on resolving nullable warnings more prominiently.

--- docs/csharp/snippets/null-warnings/PersonExamples.cs
+++ docs/csharp/snippets/null-warnings/PersonExamples.cs
@@ -47,3 +47,50 @@ namespace fourthExample
     }
     // </NullableMember>
 }
+
+namespace initializationExample
+{
+    // <ConstructorChainingAndMemberNotNull>
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public Person() : this("John", "Doe") { }
+    }
+
+    public class Student : Person
+    {
+        public string Major { get; set; }
+
+        public Student(string firstName, string lastName, string major)
+            : base(firstName, lastName)
+        {
+            SetMajor(major);
+        }
+
+        public Student(string firstName, string lastName) :
+            base(firstName, lastName)
+        {
+            SetMajor();
+        }
+
+        public Student()
+        {
+            SetMajor();
+        }
+
+        [MemberNotNull(nameof(Major))]
+        private void SetMajor(string? major = default)
+        {
+            Major = major ?? "Undeclared";
+        }
+    }
+        // </ConstructorChainingAndMemberNotNull>
+}

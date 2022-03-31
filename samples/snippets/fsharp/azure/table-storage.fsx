b2674ac58fb51e74e54d4230dc3147651b0ed675
isaac <isaac.abraham@gmail.com>
2022-03-31 22:58:31 +0200

    Fix up F# azure storage table sample

--- samples/snippets/fsharp/azure/table-storage.fsx
+++ samples/snippets/fsharp/azure/table-storage.fsx
@@ -1,31 +1,31 @@
+#r "nuget:Azure.Data.Tables" // Load the Azure Data Tables nuget package
+
 open System
 open Azure
 open Azure.Data.Tables // Namespace for Table storage types
-open System.Linq
-open System.Collections.Generic
 
 //
 // Get your connection string.
 //
 
-let storageConnString = "..." // fill this in from your storage account
+let storageConnString = "UseDevelopmentStorage=true" // fill this in from your storage account
 
 //
 // Create the Table service client.
 //
 
 // Create the table client.
-let tableClient = TableServiceClient(storageConnString)
+let tableClient = TableServiceClient storageConnString
 
 //
 // Create a table.
 //
 
 // Retrieve a reference to the table.
-let table = tableClient.GetTableClient("people")
+let table = tableClient.GetTableClient "people"
 
 // Create the table if it doesn't exist.
-ignore(table.CreateIfNotExists())
+table.CreateIfNotExists() |> ignore
 
 //
 // Add an entity to a table. The last name is used as a partition key.
@@ -33,26 +33,11 @@ ignore(table.CreateIfNotExists())
 
 type Customer(firstName, lastName, email: string, phone: string) =
     interface ITableEntity with
-        member this.ETag
-            with get (): ETag = 
-                raise (System.NotImplementedException())
-            and set (v: ETag): unit = 
-                raise (System.NotImplementedException())
-        member this.PartitionKey
-            with get (): string = 
-                raise (System.NotImplementedException())
-            and set (v: string): unit = 
-                raise (System.NotImplementedException())
-        member this.RowKey
-            with get (): string = 
-                raise (System.NotImplementedException())
-            and set (v: string): unit = 
-                raise (System.NotImplementedException())
-        member this.Timestamp
-            with get (): Nullable<DateTimeOffset> = 
-                raise (System.NotImplementedException())
-            and set (v: Nullable<DateTimeOffset>): unit = 
-                raise (System.NotImplementedException())
+        member val ETag = ETag "" with get, set
+        member val PartitionKey = "" with get, set
+        member val RowKey = "" with get, set
+        member val Timestamp = Nullable() with get, set
+
     new() = Customer(null, null, null, null)
     member val Email = email with get, set
     member val PhoneNumber = phone with get, set
@@ -60,28 +45,23 @@ type Customer(firstName, lastName, email: string, phone: string) =
     member val RowKey = firstName with get, set
 
 let customer = Customer("Walter", "Harp", "Walter@contoso.com", "425-555-0101")
-ignore(table.AddEntity(customer))
+table.AddEntity customer |> ignore
 
 //
 // Insert a batch of entities. All must have the same partition key.
 //
 
-let customer1 =
-    Customer("Jeff", "Smith", "Jeff@contoso.com", "425-555-0102")
+let customers =
+    [
+        Customer("Jeff", "Smith", "Jeff@contoso.com", "425-555-0102")
+        Customer("Ben", "Smith", "Ben@contoso.com", "425-555-0103")
+    ]
 
-let customer2 =
-    Customer("Ben", "Smith", "Ben@contoso.com", "425-555-0103")
-
-let entityList = [ customer1; customer2]
-
-// Create the batch.
-let addEntitiesBatch = List<TableTransactionAction>()
-
-// Add the entities to be added to the batch.
-addEntitiesBatch.AddRange(entityList.Select(fun e -> TableTransactionAction(TableTransactionActionType.Add, e)))
-
-// Submit the batch.
-let response = table.SubmitTransactionAsync(addEntitiesBatch).ConfigureAwait(false)
+// Add the entities to be added to the batch and submit it in a transaction.
+let response =
+    customers
+    |> List.map (fun customer -> TableTransactionAction(TableTransactionActionType.Add, customer))
+    |> table.SubmitTransaction
 
 //
 // Retrieve all entities in a partition.
@@ -89,23 +69,23 @@ let response = table.SubmitTransactionAsync(addEntitiesBatch).ConfigureAwait(fal
 
 let results = table.Query<Customer>("PartitionKey eq 'Smith'")
 
-for customer in results do 
+for customer in results do
     printfn $"customer: {customer.RowKey} {customer.PartitionKey}"
 
 //
 // Retrieve a range of entities in a partition.
 //
 
-let rangeResult = table.Query<Customer>("PartitionKey eq 'Smith' and RowKey lt 'M'")
+let rangeResult = table.Query<Customer>("PartitionKey eq 'Smith' and RowKey lt 'J'")
 
-for customer in rangeResult do 
+for customer in rangeResult do
     printfn $"customer: {customer.RowKey} {customer.PartitionKey}"
 
 //
 // Retrieve a single entity.
 //
 
-let singleResult = table.GetEntityAsync<Customer>("Smith", "Ben").Result.Value
+let singleResult = table.GetEntity<Customer>("Smith", "Ben").Value
 
 // Show the result
 printfn $"customer: {singleResult.RowKey} {singleResult.PartitionKey}"
@@ -116,10 +96,10 @@ printfn $"customer: {singleResult.RowKey} {singleResult.PartitionKey}"
 
 try
     singleResult.PhoneNumber <- "425-555-0103"
-    ignore(table.UpdateEntity(singleResult, new ETag("etag"), TableUpdateMode.Replace))
-    Console.WriteLine("Update succeeeded")
+    table.UpdateEntity(singleResult, ETag "etag", TableUpdateMode.Replace) |> ignore
+    printfn "Update succeeeded"
 with e ->
-    Console.WriteLine("Update failed")
+    printfn "Update failed"
 
 //
 // Upsert an entity.
@@ -127,19 +107,20 @@ with e ->
 
 try
     singleResult.PhoneNumber <- "425-555-0104"
-    ignore(table.UpsertEntity(singleResult, TableUpdateMode.Replace))
-    Console.WriteLine("Update succeeeded")
+    table.UpsertEntity(singleResult, TableUpdateMode.Replace) |> ignore
+    printfn "Update succeeeded"
 with e ->
-    Console.WriteLine("Update failed")
+    printfn "Update failed"
 
 //
 // Query a subset of entity properties.
 //
 
-let subsetResults = query{
-    for customer in table.Query<Customer>() do 
-    select customer.Email
-}
+let subsetResults =
+    query {
+        for customer in table.Query<Customer>() do
+        select customer.Email
+    } |> Seq.toArray
 
 //
 // Retrieve entities in pages asynchronously.
@@ -147,19 +128,19 @@ let subsetResults = query{
 
 let pagesResults = table.Query<Customer>()
 
-for page in pagesResults.AsPages() do 
-    printfn "This is a new page!" 
-    for qEntity in page.Values do
-        printfn $"customer: {qEntity.RowKey} {qEntity.PartitionKey}"
+for page in pagesResults.AsPages() do
+    printfn "This is a new page!"
+    for customer in page.Values do
+        printfn $"customer: {customer.RowKey} {customer.PartitionKey}"
 
 //
 // Delete an entity.
 //
 
-ignore(table.DeleteEntity("Smith", "Ben"))
+table.DeleteEntity("Smith", "Ben")
 
 //
 // Delete a table.
 //
 
-ignore(table.Delete())
+table.Delete()

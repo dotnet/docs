da77bd64bac9a50b46ed82d4c4d2f02d6b7edeed
Bill Wagner <wiwagn@microsoft.com>
2022-04-05 16:38:51 -0400

    update all samples.

--- docs/csharp/language-reference/operators/null-parameter-check.md
+++ docs/csharp/language-reference/operators/null-parameter-check.md
@@ -26,7 +26,7 @@ generates code similar to the following example:
 
 It's intended for library authors to provide runtime checks even when APIs have been annotated for nullable reference types. Some libraries support projects that aren't nullable enabled. Some projects support multiple versions including some that predate nullable reference types. These checks can simplify the necessary validation. You should only add the `!!` operator on those parameters that require a null-check for safety. In the preceding example, the `message` parameter doesn't have a runtime null check.
 
-## Detailed behavior
+## Where `!!` can be applied
 
 There are a few rules that govern where you can add the `!!` operator on a parameter declaration:
 
@@ -37,13 +37,53 @@ The following example demonstrates the preceding rules:
 
 :::code language="csharp" source="./snippets/shared/NullParameterChecks.cs" id="NoAbstractMethods":::
 
-In almost all cases, the code the compiler generates for `!!` on an argument is consistent with the preceding example. However, the null check is inserted before any code you write in the method. There are three scenarios where the behavior of `!!` is observably different than a hand-written null check:
+## Constructors and !!
 
-- The compiler generated null-check in constructors appears before the compiler generated calls to field initializers and before the call to any other constructor or base constructor.
-- Compiler generated null checks on `async` methods throw immediately rather than returning a faulted task.
-- Compiler generated null checks on iterator methods throw immediately rather than throw when returned iterator is enumerated.
+In almost all cases, the code the compiler generates for `!!` on an argument is consistent with the preceding example. However, the null check is inserted before any code you write in the method. There are observable differences between a hand-coded null check and adding `!!` on the parameter of a constructor. When you hand-write the null check in the body of a constructor, that null check runs after any field initializers, constructor chains, and base class constructors are called. Consider the following code:
 
-You can't catch the <xref:System.ArgumentNullException?displayProperty=nameWithType> thrown by the null check added by the compiler.
+:::code language="csharp" source="./snippets/shared/NullParameterChecks.cs" id="HandCodedConstruction":::
+
+If you call `new D1(null!)`, you'll see the following output:
+
+```dotnetcli
+Creating identity string
+In B constructor
+Parameterless D constructor
+```
+
+The field initializer runs, the base constructor runs, and the chained constructor runs. All of them run before the check. Change the constructor to use the simplified null check operator, `!!` and none of the output will be printed:
+
+:::code language="csharp" source="./snippets/shared/NullParameterChecks.cs" id="SimplifiedConstructorCheck":::
+
+The compiler synthesizes the null check before the synthesized code that runs the field initializers and calls the chained and base constructors.
+
+## iterator methods and !!
+
+Iterator methods behave different when the `!!` operator provides the null check. Consider the following iterator method:
+
+:::code language="csharp" source="./snippets/shared/NullParameterChecks.cs" id="IteratorMethod":::
+
+When you call that method passing in a null argument as shows in the following example:
+
+:::code language="csharp" source="./snippets/shared/NullParameterChecks.cs" id="Enumerate":::
+
+The message "Call iterator" is displayed before the exception is thrown. That's because the null check doesn't run until the first element in the sequence is requested. Instead, if you use the `!!` operator, the exception is thrown when `CharsIn` is called, not when the first element of the sequence is requested.
+
+:::code language="csharp" source="./snippets/shared/NullParameterChecks.cs" id="IteratorMethodSimplified":::
+
+## async methods and !!
+
+Async methods and async iterators also change the source of the exception. Consider the following method with a hand written null check:
+
+:::code language="csharp" source="./snippets/shared/NullParameterChecks.cs" id="AsyncMethod":::
+
+If you call that method using `null` as the argument, the return value is a `Task<int>` in the <xref:System.Threading.Tasks.TaskStatus.Faulted?displayProperty=nameWithType> state. The exception will be thrown when the `Task` is awaited. If you switch to a simplified null check, the exception is thrown when `SumCharsIn` is called, not when the returned task is awaited.
+
+Finally, the same behavior occurs for an async enumerator method. In the following code, the null check runs when the first element is retrieved using `await foreach`:
+
+:::code language="csharp" source="./snippets/shared/NullParameterChecks.cs" id="AsyncIteratorMethod":::
+
+If you replace the hand-written null check with a `!!`, the exception is thrown as soon as `CharsInAsync` is called, even if the sequence isn't enumerated yet.
 
 ## Null parameter checks and nullable types
 
--- docs/csharp/language-reference/operators/snippets/shared/NullParameterCheck.cs
+++ docs/csharp/language-reference/operators/snippets/shared/NullParameterCheck.cs
@@ -1,7 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace operators;
@@ -84,4 +85,250 @@ public interface IInterfaceMethods
 public class Container
 {
     void M(string? name!!) { }
+}
+
+// <HandCodedConstruction>
+public class B
+{
+    public B()
+    {
+        Console.WriteLine("In B constructor");
+    }
+}
+
+public class D : B
+{
+    private string identity = CreateNextIdentityString();
+    private string data;
+
+    public D()
+    {
+        Console.WriteLine("Parameterless D constructor");
+    }
+    public D(string arg) : this()
+    {
+        if (arg is null) throw new ArgumentNullException(nameof(arg));
+        data = arg;
+    }
+
+    private static string CreateNextIdentityString()
+    {
+        Console.WriteLine("Creating identity string");
+        return DateTime.Now.Ticks.ToString();
+    }
+}
+// </HandCodedConstruction>
+
+public class Simplified
+{
+    public class D : B
+    {
+        private string identity = CreateNextIdentityString();
+        private string data;
+
+        public D()
+        {
+            Console.WriteLine("Parameterless D constructor");
+        }
+
+        // <SimplifiedConstructorCheck>
+        public D(string arg!!) : this()
+        {
+            data = arg;
+        }
+        // </SimplifiedConstructorCheck>
+
+        private static string CreateNextIdentityString()
+        {
+            Console.WriteLine("Creating identity string");
+            return DateTime.Now.Ticks.ToString();
+        }
+    }
+
+    public class Characterizer
+    {
+        // <IteratorMethodSimplified>
+        public IEnumerable<int> CharsIn(string data!!)
+        {
+            foreach (var ch in data)
+            {
+                yield return ch;
+            }
+        }
+        // </IteratorMethodSimplified>
+
+        // async
+
+        public async Task<int> SumCharsIn(string data!!)
+        {
+            await Task.Delay(500);
+            int sum = data.Sum(x => x);
+            return sum;
+        }
+
+        // async enumerable
+        public async IAsyncEnumerable<int> CharsInAsync(string data!!)
+        {
+            foreach (var ch in data)
+            {
+                if (ch % 5 == 0)
+                    await Task.Delay(500);
+                yield return ch;
+            }
+        }
+    }
+
+}
+
+public class Characterizer
+{
+    // <IteratorMethod>
+    public IEnumerable<int> CharsIn(string data)
+    {
+        if (data is null) throw new ArgumentNullException(nameof(data));
+
+        foreach (var ch in data)
+        {
+            yield return ch;
+        }
+    }
+    // </IteratorMethod>
+
+    // async
+    // <AsyncMethod>
+    public async Task<int> SumCharsIn(string data)
+    {
+        if (data is null) throw new ArgumentNullException(nameof(data));
+
+        await Task.Delay(500);
+        int sum = data.Sum(x => x);
+        return sum;
+    }
+    // </AsyncMethod>
+
+    // async enumerable
+    // <AsyncIteratorMethod>
+    public async IAsyncEnumerable<int> CharsInAsync(string data)
+    {
+        if (data is null) throw new ArgumentNullException(nameof(data));
+
+        foreach (var ch in data)
+        {
+            if (ch % 5 == 0)
+                await Task.Delay(500);
+            yield return ch;
+        }
+    }
+    // </AsyncIteratorMethod>
+}
+
+public static class NullChecks
+{
+    public static async Task Examples()
+    {
+        try
+        {
+            Console.WriteLine("Hand coded null check");
+            _ = new D(null!);
+        } catch (ArgumentNullException)
+        {
+            Console.WriteLine();
+        }
+        try
+        {
+            Console.WriteLine("Simplified null check");
+            _ = new Simplified.D(null!);
+        }
+        catch (ArgumentNullException)
+        {
+            Console.WriteLine();
+        }
+
+
+        var characters = new Characterizer();
+        try
+        {
+            // <Enumerate>
+            Console.WriteLine("Call iterator");
+            var sequence = characters.CharsIn(null!);
+            Console.WriteLine("Enumerate values");
+            foreach (var c in sequence)
+                Console.WriteLine(c);
+            // </Enumerate>
+        }
+        catch (ArgumentNullException)
+        {
+            Console.WriteLine("Exception caught");
+            Console.WriteLine();
+        }
+
+        try 
+        { 
+            Console.WriteLine("Create task");
+            var sumTask = characters.SumCharsIn(null!);
+            Console.WriteLine("await task");
+            Console.WriteLine(await sumTask);
+        }
+        catch (ArgumentNullException)
+        {
+            Console.WriteLine("Exception caught");
+            Console.WriteLine();
+        }
+
+        try
+        { 
+            Console.WriteLine("Call async iterator");
+            var sequenceAsync = characters.CharsInAsync(null!);
+            Console.WriteLine("Enumerate values");
+            await foreach (var c in sequenceAsync)
+                Console.WriteLine(c);
+        }
+        catch (ArgumentNullException)
+        {
+            Console.WriteLine("Exception caught");
+            Console.WriteLine();
+        }
+
+        var characters2 = new Simplified.Characterizer();
+        try
+        {
+            Console.WriteLine("Call iterator");
+            var sequence = characters2.CharsIn(null!);
+            Console.WriteLine("Enumerate values");
+            foreach (var c in sequence)
+                Console.WriteLine(c);
+        }
+        catch (ArgumentNullException)
+        {
+            Console.WriteLine("Exception caught");
+            Console.WriteLine();
+        }
+
+        try
+        {
+            Console.WriteLine("Create task");
+            var sumTask = characters2.SumCharsIn(null!);
+            Console.WriteLine("await task");
+            Console.WriteLine(await sumTask);
+        }
+        catch (ArgumentNullException)
+        {
+            Console.WriteLine("Exception caught");
+            Console.WriteLine();
+        }
+
+        try
+        {
+            Console.WriteLine("Call async iterator");
+            var sequenceAsync = characters2.CharsInAsync(null!);
+            Console.WriteLine("Enumerate values");
+            await foreach (var c in sequenceAsync)
+                Console.WriteLine(c);
+        }
+        catch (ArgumentNullException)
+        {
+            Console.WriteLine("Exception caught");
+            Console.WriteLine();
+        }
+    }
 }

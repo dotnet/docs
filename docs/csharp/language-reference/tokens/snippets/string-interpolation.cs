eb65f77851f2f183614eb63602a6ed08256451e4
David Pine <david.pine@microsoft.com>
2022-04-20 09:55:58 -0500

    Apply suggestions from code review

--- docs/csharp/language-reference/tokens/snippets/string-interpolation.cs
+++ docs/csharp/language-reference/tokens/snippets/string-interpolation.cs
@@ -122,7 +122,6 @@ public class StringInterpolation
         int Y = 3;
 
         var pointMessage = $$"""The point {{{X}}, {{Y}}} is {{Math.Sqrt(X * X + Y * Y)}} from the origin""";
-        
         Console.WriteLine(pointMessage);
         // output:  The point {2, 3} is 3.605551275463989 from the origin.
         // </RawInterpolatedLiteralStringWithBraces>

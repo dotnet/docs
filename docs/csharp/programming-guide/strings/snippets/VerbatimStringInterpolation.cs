18b4729e224bc1e6175ef55b91695105b5167967
Sean Killeen <SeanKilleen@gmail.com>
2022-06-09 10:58:29 -0400

    update ID for sample

--- docs/csharp/programming-guide/strings/snippets/VerbatimStringInterpolation.cs
+++ docs/csharp/programming-guide/strings/snippets/VerbatimStringInterpolation.cs
@@ -5,7 +5,7 @@ public static class VerbatimInterpolation
 {
     public static void VerbatimInterpolationExample()
     {
-        // <StringInterpolation>
+        // <VerbatimStringInterpolation>
         var jh = (firstName: "Jupiter", lastName: "Hammon", born: 1711, published: 1761);
         Console.WriteLine($@"{jh.firstName} {jh.lastName}
             was an African American poet born in {jh.born}.");
@@ -17,6 +17,6 @@ public static class VerbatimInterpolation
         // was an African American poet born in 1711.
         // He was first published in 1761
         // at the age of 50.
-        // </StringInterpolation>
+        // </VerbatimStringInterpolation>
     }
 }

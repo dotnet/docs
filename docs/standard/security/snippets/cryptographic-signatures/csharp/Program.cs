63281487ca7c1d6d9b052a439d27ba6c0c554375
David Pine <david.pine@microsoft.com>
2022-08-09 08:24:20 -0500

    Heavy refactor, address #30272

---
+++ docs/standard/security/snippets/cryptographic-signatures/csharp/Program.cs
@@ -0,0 +1,42 @@
+﻿// <generate>
+using System.Security.Cryptography;
+using System.Text;
+
+byte[] hash;
+
+SHA256 alg = SHA256.Create();
+byte[] data = Encoding.ASCII.GetBytes("Hello, from the .NET Docs!");
+hash = alg.ComputeHash(data);
+
+RSAParameters sharedParameters;
+byte[] signedHash;
+
+using (RSA rsa = RSA.Create())
+{
+    sharedParameters = rsa.ExportParameters(false);
+
+    RSAPKCS1SignatureFormatter rsaFormatter = new(rsa);
+    rsaFormatter.SetHashAlgorithm(nameof(SHA256));
+
+    signedHash = rsaFormatter.CreateSignature(hash);
+}
+// </generate>
+
+// <verify>
+using (RSA rsa = RSA.Create())
+{
+    rsa.ImportParameters(sharedParameters);
+
+    RSAPKCS1SignatureDeformatter rsaDeformatter = new(rsa);
+    rsaDeformatter.SetHashAlgorithm(nameof(SHA256));
+
+    if (rsaDeformatter.VerifySignature(hash, signedHash))
+    {
+        Console.WriteLine("The signature is valid.");
+    }
+    else
+    {
+        Console.WriteLine("The signature is not valid.");
+    }
+}
+// </verify>

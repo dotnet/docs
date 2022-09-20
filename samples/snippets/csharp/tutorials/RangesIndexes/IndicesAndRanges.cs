96c5c8b11312ffd96c6def7b32332ac3796cd68b
David Pine <david.pine@microsoft.com>
2022-09-20 13:21:49 -0500

    Added a table to help more quickly mentally model range operators (#31223)
    * Added a table to help more quickly mentally model range operators
    
    * Fix a few of the interactive bits that I broke
    
    * Added a bit more text
    
    * Move note about implicit converstions
    
    * Added a bit more info
    
    * A few more tweaks
    
    * Use explicit type
    
    * Added missing updates to the ranges-indexes.md file
    
    * Apply suggestions from code review
    
    Co-authored-by: Petr Kulikov <petr.kulikov@gmail.com>
    
    * Updates from peer suggestions
    
    * Adjusted content from feedback
    
    Co-authored-by: Petr Kulikov <petr.kulikov@gmail.com>

--- samples/snippets/csharp/tutorials/RangesIndexes/IndicesAndRanges.cs
+++ samples/snippets/csharp/tutorials/RangesIndexes/IndicesAndRanges.cs
@@ -153,16 +153,16 @@ namespace RangesIndexes
             // <SnippetIndicesAndRanges_JaggedArrays>
             var jagged = new int[10][]
             {
-               new int[10] { 0,  1, 2, 3, 4, 5, 6, 7, 8, 9},
-               new int[10] { 10,11,12,13,14,15,16,17,18,19},
-               new int[10] { 20,21,22,23,24,25,26,27,28,29},
-               new int[10] { 30,31,32,33,34,35,36,37,38,39},
-               new int[10] { 40,41,42,43,44,45,46,47,48,49},
-               new int[10] { 50,51,52,53,54,55,56,57,58,59},
-               new int[10] { 60,61,62,63,64,65,66,67,68,69},
-               new int[10] { 70,71,72,73,74,75,76,77,78,79},
-               new int[10] { 80,81,82,83,84,85,86,87,88,89},
-               new int[10] { 90,91,92,93,94,95,96,97,98,99},
+               new int[10] {  0, 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+               new int[10] { 10,11,12,13,14,15,16,17,18,19 },
+               new int[10] { 20,21,22,23,24,25,26,27,28,29 },
+               new int[10] { 30,31,32,33,34,35,36,37,38,39 },
+               new int[10] { 40,41,42,43,44,45,46,47,48,49 },
+               new int[10] { 50,51,52,53,54,55,56,57,58,59 },
+               new int[10] { 60,61,62,63,64,65,66,67,68,69 },
+               new int[10] { 70,71,72,73,74,75,76,77,78,79 },
+               new int[10] { 80,81,82,83,84,85,86,87,88,89 },
+               new int[10] { 90,91,92,93,94,95,96,97,98,99 },
             };
 
             var selectedRows = jagged[3..^3];
@@ -178,6 +178,25 @@ namespace RangesIndexes
             }
             // </SnippetIndicesAndRanges_JaggedArrays>
             return 0;
+        }
+
+        internal void ImplicitRangeOperatorConversion()
+        {
+            // <ImplicitRangeOperatorConversion>
+            Range implicitRange = 3..^5;
+
+            Range explicitRange = new(
+                start: new Index(value: 3, fromEnd: false),
+                end: new Index(value: 5, fromEnd: true));
+
+            if (implicitRange.Equals(explicitRange))
+            {
+                Console.WriteLine(
+                    $"The implicit range '{implicitRange}' equals the explicit range '{explicitRange}'");
+            }
+            // Sample output:
+            //     The implicit range '3..^5' equals the explicit range '3..^5'
+            // </ImplicitRangeOperatorConversion>
         }
     }
 }

c5e186b4bcf8026ae46db7f96bd61ad99adf0a84
Brent <bbberger@gmail.com>
2022-09-26 15:31:21 -0400

    Update ca2101.vb
    Fix ca2101.vb missing End Function

--- docs/fundamentals/code-analysis/quality-rules/snippets/vb/all-rules/ca2101.vb
+++ docs/fundamentals/code-analysis/quality-rules/snippets/vb/all-rules/ca2101.vb
@@ -1,8 +1,18 @@
+Imports System
 Imports System.Runtime.InteropServices
 
-Class NativeMethods
-    <DllImport("advapi32.dll", CharSet:=CharSet.Auto)>
-    Friend Shared Function RegCreateKey(ByVal key As IntPtr, ByVal subKey As String, <Out> ByRef result As IntPtr) As Integer
-    <DllImport("advapi32.dll", CharSet:=CharSet.Unicode)>
-    Friend Shared Function RegCreateKey2(ByVal key As IntPtr, ByVal subKey As String, <Out> ByRef result As IntPtr) As Integer
-End Class
+Namespace ca2101
+    '<snippet2>
+    Friend Class NativeMethods
+        ' Violates rule: SpecifyMarshalingForPInvokeStringArguments.
+        <DllImport("advapi32.dll", CharSet:=CharSet.Auto)>
+        Friend Shared Function RegCreateKey(ByVal key As IntPtr, ByVal subKey As String, <Out> ByRef result As IntPtr) As Integer
+        End Function
+
+        ' Satisfies rule: SpecifyMarshalingForPInvokeStringArguments.
+        <DllImport("advapi32.dll", CharSet:=CharSet.Unicode)>
+        Friend Shared Function RegCreateKey2(ByVal key As IntPtr, ByVal subKey As String, <Out> ByRef result As IntPtr) As Integer
+        End Function
+    End Class
+    '</snippet2>
+End Namespace

2670e0d411831ce5e473d44d7c43c3bdbbe73cde
David Pine <david.pine@microsoft.com>
2022-10-10 14:55:49 -0500

    More fixes and updates, and adjusted headings

--- docs/standard/base-types/snippets/regular-expression-source-generators/Program.cs
+++ docs/standard/base-types/snippets/regular-expression-source-generators/Program.cs
@@ -1,33 +1,58 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Text;
+using System.Text.RegularExpressions;
 
 static partial class Program
 { 
     private static readonly Regex s_abcOrDefGeneratedRegex = AbcOrDefGeneratedRegex();
 
-    [GeneratedRegex("abc|def", RegexOptions.IgnoreCase | RegexOptions.Compiled, "en-US")]
+    [GeneratedRegex(
+        pattern: "abc|def",
+        options: RegexOptions.IgnoreCase | RegexOptions.Compiled,
+        cultureName: "en-US")]
     private static partial Regex AbcOrDefGeneratedRegex();
 
     private static void EvaluateText(string text)
     {
         if (s_abcOrDefGeneratedRegex.IsMatch(text))
         {
+            Console.WriteLine($"""
+                ✅ "{text}" matches "{s_abcOrDefGeneratedRegex}" pattern.
+                """);
+        }
+        else
+        {
+            Console.WriteLine($"""
+                ❌ "{text}" doesn't match "{s_abcOrDefGeneratedRegex}" pattern.
+                """);
         }
     }
 
     private static void Main()
     {
-        EvaluateText("deftones");
+        Console.OutputEncoding = Encoding.UTF8;
+
+        new List<string> { "Incubus", "Deftones", "Tool" }.ForEach(EvaluateText);
+
+        // Sample output:
+        //   ❌ "Incubus" doesn't match "abc|def" pattern.
+        //   ✅ "Deftones" matches "abc|def" pattern.
+        //   ❌ "Tool" doesn't match "abc|def" pattern.
+
+        var abcOrDefRegex = new Regex(pattern: "abc|def", options: RegexOptions.IgnoreCase);
     }
 }
 
-static partial class Program
+static file partial class Program
 {
-    [GeneratedRegex(@"(a|bc)d")]
+    [GeneratedRegex(pattern: @"(a|bc)d")]
     private static partial Regex Example();
 
-    [GeneratedRegex("[ab]*[bc]")]
+    [GeneratedRegex(pattern: "[ab]*[bc]")]
     private static partial Regex AnotherExample();
 
-    [GeneratedRegex("Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday")]
+    [GeneratedRegex(pattern: "Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday")]
     private static partial Regex DaysOfWeek();
+
+    [GeneratedRegex(pattern: "")]
+    private static partial Regex Blank();
 }

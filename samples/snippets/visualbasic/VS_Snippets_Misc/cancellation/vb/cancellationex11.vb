a52d4e721d2efc4edb46eb455d0baec9546b9960
JJS <jjs@joritos.de>
2022-10-11 17:10:08 +0200

    Fix vs example by moving check out of loop (#31643)

--- samples/snippets/visualbasic/VS_Snippets_Misc/cancellation/vb/cancellationex11.vb
+++ samples/snippets/visualbasic/VS_Snippets_Misc/cancellation/vb/cancellationex11.vb
@@ -42,24 +42,23 @@ Class CancelByPolling
     '<snippet3>
     Shared Sub NestedLoops(ByVal rect As Rectangle, ByVal token As CancellationToken)
         For x As Integer = 0 To rect.columns
+            ' Assume that we know that the inner loop is very fast.
+            ' Therefore, checking once per row in the outer loop is sufficient.
             For y As Integer = 0 To rect.rows
                 ' Simulating work.
                 Thread.SpinWait(5000)
                 Console.Write("0' end block,1' end block ", x, y)
             Next
-
-            ' Assume that we know that the inner loop is very fast.
-            ' Therefore, checking once per row is sufficient.
-            If token.IsCancellationRequested = True Then
-                ' Cleanup or undo here if necessary...
-                Console.WriteLine(vbCrLf + "Cancelling after row 0' end block.", x)
-                Console.WriteLine("Press any key to exit.")
-                ' then...
-                Exit For
-                ' ...or, if using Task:
-                ' token.ThrowIfCancellationRequested()
-            End If
         Next
+
+        If token.IsCancellationRequested = True Then
+            ' Cleanup or undo here if necessary...
+            Console.WriteLine(vbCrLf + "Cancelling after row 0' end block.", x)
+            Console.WriteLine("Press any key to exit.")
+
+            ' If using Task:
+            ' token.ThrowIfCancellationRequested()
+        End If
     End Sub
     '</snippet3>
 End Class

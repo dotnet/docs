8fdabe7c424b06180013ed34f3dcebfa2f6fa080
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2022-10-12 15:54:24 -0700

    delete unused snippets

--- samples/snippets/visualbasic/VS_Snippets_VBCSharp/VbRadconService/VB/MyNewService.Designer-add-parameter.vb
+++
@@ -1,4 +0,0 @@
-﻿Shared Sub Main(ByVal cmdArgs() As String)
-    Dim ServicesToRun() As System.ServiceProcess.ServiceBase = New System.ServiceProcess.ServiceBase() {New MyNewService(cmdArgs)}
-    System.ServiceProcess.ServiceBase.Run(ServicesToRun)
-End Sub
--- samples/snippets/visualbasic/VS_Snippets_Wpf/HostingWpfUserControlInWf/VisualBasic/HostingWpfUserControlInWf/UserControl1.xaml.vb
+++
@@ -1,9 +0,0 @@
-﻿' Interaction logic for UserControl1.xaml
-Partial Public Class UserControl1
-    Inherits UserControl
-
-    Public Sub New()
-        InitializeComponent()
-    End Sub
-
-End Class

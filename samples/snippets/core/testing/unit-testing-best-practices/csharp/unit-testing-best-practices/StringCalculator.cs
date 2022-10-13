197e6ef122704aa3ece0b81779ac9b0f5fe3ec3c
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2022-10-13 09:51:07 -0700

    restore necessary projects

---
+++ samples/snippets/core/testing/unit-testing-best-practices/csharp/unit-testing-best-practices/StringCalculator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace UnitTestingBestPractices
+{
+    public class StringCalculator
+    {
+        private const int MAXIMUM_RESULT = 1000;
+
+        public int Add(string numbers)
+        {
+            if (numbers == null)
+            {
+                throw new ArgumentNullException(nameof(numbers));
+            }
+
+            int result;
+            if (numbers.Contains(","))
+            {
+                result = HandleMultipleNumbers(numbers);
+            }
+            else
+            {
+                result = HandleSingleNumber(numbers);
+            }
+
+            return ValidateResult(result);
+        }
+
+        private static int HandleMultipleNumbers(string numbers)
+        {
+            var sum = 0;
+
+            var numbersArray = numbers.Split(',');
+            foreach (var number in numbersArray)
+            {
+                sum += HandleSingleNumber(number);
+            }
+
+            return ValidateResult(sum);
+        }
+
+        private static int HandleSingleNumber(string number)
+        {
+            if (string.IsNullOrEmpty(number))
+            {
+                return 0;
+            }
+
+            var result = int.TryParse(number, out var parsedNumber);
+            if (result == false)
+            {
+                throw new ArgumentException($"Unable to parse {nameof(number)} as an Int32.", nameof(number));
+            }
+
+            return parsedNumber;
+        }
+
+        private static int ValidateResult(int sum)
+        {
+            if (sum > MAXIMUM_RESULT)
+            {
+                throw new OverflowException();
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ samples/snippets/csharp/VS_Snippets_Wpf/HostingAxInWpf/CSharp/HostingAxInWpf/app.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace HostingAxInWpf
+{
+    /// <summary>
+    /// Interaction logic for app.xaml
+    /// </summary>
+
+    public partial class app : Application
+    {
+        void AppStartup(object sender, StartupEventArgs args)
+        {
+            Window mainWindow = new Window();
+            mainWindow.Show();
+        }
+    }
+}

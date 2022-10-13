68d6c5edb590036f8fdcf30e6e7720837394279f
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2022-10-13 10:03:02 -0700

    restore needed projects

---
+++ docs/csharp/roslyn-sdk/tutorials/snippets/how-to-write-csharp-analyzer-code-fix/MakeConstTestProject/MakeConstTestProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MakeConstTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int j = 2;
+            int k = i + j;
+
+            // uncomment for analyzer test:
+            //int x = "abc";
+
+            object s = "abc";
+
+            string s2 = "abc";
+
+            var item = "xyz";
+        }
+    }
+}

a649eb19c0c27143f08035b1a48073c4993440c4
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2022-10-12 21:05:44 -0700

    restore two necessary files

---
+++ samples/snippets/csharp/VS_Snippets_VBCSharp/VbRadconService/CS/Program-add-parameter.cs
@@ -0,0 +1,9 @@
+﻿static void Main(string[] args)
+{
+    ServiceBase[] ServicesToRun;
+    ServicesToRun = new ServiceBase[]
+    {
+        new MyNewService(args)
+    };
+    ServiceBase.Run(ServicesToRun);
+}

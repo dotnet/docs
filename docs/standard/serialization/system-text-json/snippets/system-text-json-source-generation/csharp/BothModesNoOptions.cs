e047748ed459e3a1ffc6ecb98dc46005c9b95562
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2022-10-24 12:40:14 -0700

    Source generation for STJ update (#31971)

--- docs/standard/serialization/system-text-json/snippets/system-text-json-source-generation/csharp/BothModesNoOptions.cs
+++ docs/standard/serialization/system-text-json/snippets/system-text-json-source-generation/csharp/BothModesNoOptions.cs
@@ -51,6 +51,19 @@ namespace BothModesNoOptions
             // output:
             //Date=8/1/2019 12:00:00 AM
 
+            // <DeserializeWithOptions>
+            var sourceGenOptions = new JsonSerializerOptions
+            {
+                TypeInfoResolver = SourceGenerationContext.Default
+            };
+            weatherForecast = JsonSerializer.Deserialize(
+                jsonString, typeof(WeatherForecast), sourceGenOptions)
+                as WeatherForecast;
+            // </DeserializeWithOptions>
+            Console.WriteLine($"Date={weatherForecast?.Date}");
+            // output:
+            //Date=8/1/2019 12:00:00 AM
+
             // <SerializeWithTypeInfo>
             jsonString = JsonSerializer.Serialize(
                 weatherForecast!, SourceGenerationContext.Default.WeatherForecast);
@@ -63,6 +76,19 @@ namespace BothModesNoOptions
             jsonString = JsonSerializer.Serialize(
                 weatherForecast, typeof(WeatherForecast), SourceGenerationContext.Default);
             // </SerializeWithContext>
+            Console.WriteLine(jsonString);
+            // output:
+            //{"Date":"2019-08-01T00:00:00","TemperatureCelsius":25,"Summary":"Hot"}
+
+            // <SerializeWithOptions>
+            sourceGenOptions = new JsonSerializerOptions
+            {
+                TypeInfoResolver = SourceGenerationContext.Default
+            };
+
+            jsonString = JsonSerializer.Serialize(
+                weatherForecast, typeof(WeatherForecast), sourceGenOptions);
+            // </SerializeWithOptions>
             Console.WriteLine(jsonString);
             // output:
             //{"Date":"2019-08-01T00:00:00","TemperatureCelsius":25,"Summary":"Hot"}

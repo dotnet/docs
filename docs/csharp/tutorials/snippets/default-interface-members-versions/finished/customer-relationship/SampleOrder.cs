2b52afdb0112b4e02a47b8bb469b2977f4a18f9e
Bill Wagner <wiwagn@microsoft.com>
2022-11-02 10:06:30 -0400

    Retire the "What's new in C# 8" article (#32101)
    * import range content
    
    Some of the range content from the what's new article needed to move to the appropriate tutorial.
    
    Move exploration tutorials
    
    * move C# 8 new feature tutorials
    
    They're now just features.

---
+++ docs/csharp/tutorials/snippets/default-interface-members-versions/finished/customer-relationship/SampleOrder.cs
@@ -0,0 +1,11 @@
+﻿namespace customer_relationship;
+
+public class SampleOrder : IOrder
+{
+    public SampleOrder(DateTime purchase, decimal cost) =>
+        (Purchased, Cost) = (purchase, cost);
+
+    public DateTime Purchased { get; }
+
+    public decimal Cost { get; }
+}

300ca380b8b38902ef5e8d52f3f348184938e71d
David Pine <david.pine@microsoft.com>
2022-12-06 11:54:42 -0600

    Sweeping updates to fix up the minimal Orleans article (#32895)
    * Sweeping updates to fix up the min article
    
    * Remove tip
    
    * Collapse table

---
+++ docs/orleans/tutorials-and-samples/snippets/minimal/Client/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using GrainInterfaces;
+
+try
+{
+    using IHost host = await StartClientAsync();
+    var client = host.Services.GetRequiredService<IClusterClient>();
+
+    await DoClientWorkAsync(client);
+    Console.ReadKey();
+
+    await host.StopAsync();
+
+    return 0;
+}
+catch (Exception e)
+{
+    Console.WriteLine($$"""
+        Exception while trying to run client: {{e.Message}}
+        Make sure the silo the client is trying to connect to is running.
+        Press any key to exit.
+        """);
+    
+    Console.ReadKey();
+    return 1;
+}
+
+static async Task<IHost> StartClientAsync()
+{
+    var builder = new HostBuilder()
+        .UseOrleansClient(client =>
+        {
+            client.UseLocalhostClustering();
+        })
+        .ConfigureLogging(logging => logging.AddConsole());
+
+    var host = builder.Build();
+    await host.StartAsync();
+
+    Console.WriteLine("Client successfully connected to silo host \n");
+
+    return host;
+}
+
+static async Task DoClientWorkAsync(IClusterClient client)
+{
+    var friend = client.GetGrain<IHello>(0);
+    var response = await friend.SayHello("Good morning, HelloGrain!");
+
+    Console.WriteLine($"\n\n{response}\n\n");
+}

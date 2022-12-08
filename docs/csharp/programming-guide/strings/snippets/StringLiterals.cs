f39228be60eebc9898f85f55708b93f90f98f8a1
Bill Wagner <wiwagn@microsoft.com>
2022-12-08 12:41:37 -0500

    Edit + build fixes

--- docs/csharp/programming-guide/strings/snippets/StringLiterals.cs
+++ docs/csharp/programming-guide/strings/snippets/StringLiterals.cs
@@ -6,9 +6,22 @@ public class Literals
 {
     public static void StringLiterals()
     {
+        //<EscapeSequences>
+        string columns = "Column 1\tColumn 2\tColumn 3";
+        //Output: Column 1        Column 2        Column 3
+
+        string rows = "Row 1\r\nRow 2\r\nRow 3";
+        /* Output:
+            Row 1
+            Row 2
+            Row 3
+        */
+
         //<VerbatimLiterals>
         string title = "\"The \u00C6olean Harp\", by Samuel Taylor Coleridge";
         //Output: "The Æolean Harp", by Samuel Taylor Coleridge
+        //</EscapeSequences>
+
         string filePath = @"C:\Users\scoleridge\Documents\";
         //Output: C:\Users\scoleridge\Documents\
 
@@ -24,6 +37,10 @@ public class Literals
         string quote = @"Her name was ""Sara.""";
         //Output: Her name was "Sara."
         //</VerbatimLiterals>
+        System.Console.WriteLine(columns);
+        System.Console.WriteLine(rows);
+        System.Console.WriteLine(title);
+
         System.Console.WriteLine(filePath);
         System.Console.WriteLine(text);
         System.Console.WriteLine(quote);

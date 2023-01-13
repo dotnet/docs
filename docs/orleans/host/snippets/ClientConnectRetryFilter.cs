d6fd9d37ae908e0a6ffc1ba1e82b0f91dc226fff
David Pine <david.pine@microsoft.com>
2023-01-13 13:55:05 -0600

    Updates to refactoring and pivots

--- docs/orleans/host/snippets/ClientConnectRetryFilter.cs
+++ docs/orleans/host/snippets/ClientConnectRetryFilter.cs
@@ -1,9 +1,24 @@
 internal sealed class ClientConnectRetryFilter : IClientConnectionRetryFilter
 {
-    public Task<bool> ShouldRetryConnectionAttempt(
+    private int _retryCount = 0;
+    private readonly int _maxRetry = 5;
+    private readonly int _delay = 1_000;
+
+    public async Task<bool> ShouldRetryConnectionAttempt(
         Exception exception,
         CancellationToken cancellationToken)
     {
-        return Task.FromResult(true);
+        if (_retryCount >= _retryCount)
+        {
+            return false;
+        }
+
+        if (exception is SiloUnavailableException siloUnavailableException)
+        {
+            await Task.Delay(++ _retryCount * _delay, cancellationToken);
+            return true;
+        }
+
+        return false;
     }
 }

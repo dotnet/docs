ad27411215530a65ca894fb777cb944c046f4950
Bill Wagner <wiwagn@microsoft.com>
2023-02-07 11:34:06 -0500

    Update `ref` performance article(s) for C# 11 (#32997)
    * Move existing topics
    
    - Add a new top level node for "Advanced topics". Under that node are the Roslyn SDK and the new performance node.
    - Move existing topic.
    - Add redirects
    - Add new outline
    
    fix current build warnings
    
    * fix redirection warnings
    
    * fix the small related issues
    
    Making these edits first will make it easier to chop this up and reorganize without losing the context in these issues.
    
    * add some notes to the outline.
    
    * FInish first draft
    
    Ignore the old-stuff.md file. That's a checkpoint so I don't lose any text until it's converted
    
    * proofread
    
    * Add structure for tutorial
    
    * fix warnings
    
    * tutorial rough draft finished.
    
    * fix warnings
    
    * remove old sample
    
    * proofread and updates
    
    * final proofread
    
    * add image reference.
    
    * remove blank line
    
    * move final image
    
    * Apply suggestions from code review
    
    Co-authored-by: Cam Soper <cam.soper@microsoft.com>
    
    * Respond to feedback
    
    Add steps missing from the tutorial.
    
    * respond to feedback.
    
    * respond to feedback.
    
    ---------
    
    Co-authored-by: Cam Soper <cam.soper@microsoft.com>

---
+++ docs/csharp/advanced-topics/performance/snippets/ref-tutorial/IntruderAlert-finished/AverageMeasurement.cs
@@ -0,0 +1,34 @@
+﻿namespace IntruderAlert;
+
+public struct AverageMeasurement
+{
+    private double sumCO2 = 0;
+    private double sumO2 = 0;
+    private double sumTemperature = 0;
+    private double sumHumidity = 0;
+    private int totalMeasurements = 0;
+
+    public AverageMeasurement() { }
+
+    public readonly double CO2 => sumCO2 / totalMeasurements;
+    public readonly double O2 => sumO2 / totalMeasurements;
+    public readonly double Temperature => sumTemperature / totalMeasurements;
+    public readonly double Humidity => sumHumidity / totalMeasurements;
+
+    public void AddMeasurement(in SensorMeasurement datum)
+    {
+        totalMeasurements++;
+        sumCO2 += datum.CO2;
+        sumO2 += datum.O2;
+        sumTemperature += datum.Temperature;
+        sumHumidity+= datum.Humidity;
+    }
+
+    public readonly override string ToString() => $"""
+        Average measurements:
+            Temp:      {Temperature:F3}
+            Humidity:  {Humidity:P3}
+            Oxygen:    {O2:P3}
+            CO2 (ppm): {CO2:F3}
+        """;
+}

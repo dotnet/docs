78dafbbeaeea065185f1a09187e82f3bce401022
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2023-02-14 15:51:48 -0800

    update snippets to compile in .net 7

--- samples/snippets/csharp/VS_Snippets_CLR/Exception.Throwing/CS/throw.cs
+++ samples/snippets/csharp/VS_Snippets_CLR/Exception.Throwing/CS/throw.cs
@@ -1,32 +1,32 @@
-﻿// <Snippet1>
-using System;
-using System.IO;
-
-public class ProcessFile
+﻿namespace Example
 {
-    public static void Main()
+    public class ProcessFile
     {
-        FileStream fs;
-        try
+        public static void Main()
         {
-            // Opens a text tile.
-            fs = new FileStream(@"C:\temp\data.txt", FileMode.Open);
-            var sr = new StreamReader(fs);
+            // <Snippet1>
+            FileStream? fs = null;
+            try
+            {
+                // Opens a text tile.
+                fs = new FileStream(@"C:\temp\data.txt", FileMode.Open);
+                var sr = new StreamReader(fs);
 
-            // A value is read from the file and output to the console.
-            string line = sr.ReadLine();
-            Console.WriteLine(line);
-        }
-        catch(FileNotFoundException e)
-        {
-            Console.WriteLine($"[Data File Missing] {e}");
-            throw new FileNotFoundException(@"[data.txt not in c:\temp directory]", e);
-        }
-        finally
-        {
-            if (fs != null)
-                fs.Close();
+                // A value is read from the file and output to the console.
+                string? line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine($"[Data File Missing] {e}");
+                throw new FileNotFoundException(@"[data.txt not in c:\temp directory]", e);
+            }
+            finally
+            {
+                if (fs != null)
+                    fs.Close();
+            }
+            // </Snippet1>
         }
     }
 }
-// </Snippet1>

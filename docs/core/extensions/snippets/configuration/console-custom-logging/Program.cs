a2f89c505d3cb677aa0abaddf81723a21fc3d958
David Pine <david.pine@microsoft.com>
2023-02-18 14:41:32 -0600

    Add info about why `IsEnabled` is called (#34140)
    * Fix #33978
    
    * Fix xref
    
    * Apply suggestions from code review
    
    Co-authored-by: Genevieve Warren <24882762+gewarren@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Genevieve Warren <24882762+gewarren@users.noreply.github.com>

--- docs/core/extensions/snippets/configuration/console-custom-logging/Program.cs
+++ docs/core/extensions/snippets/configuration/console-custom-logging/Program.cs
@@ -20,6 +20,6 @@ logger.LogDebug(1, "Does this line get hit?");    // Not logged
 logger.LogInformation(3, "Nothing to see here."); // Logs in ConsoleColor.DarkGreen
 logger.LogWarning(5, "Warning... that was odd."); // Logs in ConsoleColor.DarkCyan
 logger.LogError(7, "Oops, there was an error.");  // Logs in ConsoleColor.DarkRed
-logger.LogTrace(5!, "== 120.");                   // Not logged
+logger.LogTrace(5, "== 120.");                    // Not logged
 
 await host.RunAsync();

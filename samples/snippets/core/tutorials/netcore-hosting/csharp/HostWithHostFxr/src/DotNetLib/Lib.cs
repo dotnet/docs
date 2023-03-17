4260f893c564b7a5c1ac8c8b5512a613124dd186
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2023-03-16 21:37:48 -0700

    readd necessary files

---
+++ samples/snippets/core/tutorials/netcore-hosting/csharp/HostWithHostFxr/src/DotNetLib/Lib.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DotNetLib
+{
+    public static class Lib
+    {
+        private static int s_CallCount = 1;
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct LibArgs
+        {
+            public IntPtr Message;
+            public int Number;
+        }
+
+        public static int Hello(IntPtr arg, int argLength)
+        {
+            if (argLength < System.Runtime.InteropServices.Marshal.SizeOf(typeof(LibArgs)))
+            {
+                return 1;
+            }
+
+            LibArgs libArgs = Marshal.PtrToStructure<LibArgs>(arg);
+            Console.WriteLine($"Hello, world! from {nameof(Lib)} [count: {s_CallCount++}]");
+            PrintLibArgs(libArgs);
+            return 0;
+        }
+
+        public delegate void CustomEntryPointDelegate(LibArgs libArgs);
+        public static void CustomEntryPoint(LibArgs libArgs)
+        {
+            Console.WriteLine($"Hello, world! from {nameof(CustomEntryPoint)} in {nameof(Lib)}");
+            PrintLibArgs(libArgs);
+        }
+
+#if NET5_0
+        [UnmanagedCallersOnly]
+        public static void CustomEntryPointUnmanaged(LibArgs libArgs)
+        {
+            CustomEntryPoint(libArgs);
+        }
+#endif
+
+        private static void PrintLibArgs(LibArgs libArgs)
+        {
+            string message = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+                ? Marshal.PtrToStringUni(libArgs.Message)
+                : Marshal.PtrToStringUTF8(libArgs.Message);
+
+            Console.WriteLine($"-- message: {message}");
+            Console.WriteLine($"-- number: {libArgs.Number}");
+        }
+    }
+}

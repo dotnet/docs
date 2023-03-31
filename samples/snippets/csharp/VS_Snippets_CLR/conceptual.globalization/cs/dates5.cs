1bf68adb1602824636cc6b6bc7c967cee25235e8
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2023-03-31 07:48:13 -0700

    remove binary serialization examples from globalization article (#34842)

--- samples/snippets/csharp/VS_Snippets_CLR/conceptual.globalization/cs/dates5.cs
+++ samples/snippets/csharp/VS_Snippets_CLR/conceptual.globalization/cs/dates5.cs
@@ -3,16 +3,17 @@ using System;
 
 public class Example7
 {
-   public static void Main7()
-   {
-      DateTime date1 = DateTime.SpecifyKind(new DateTime(2013, 3, 9, 10, 30, 0),
-                                            DateTimeKind.Local);
-      TimeSpan interval = new TimeSpan(48, 0, 0);
-      DateTime date2 = date1 + interval;
-      Console.WriteLine("{0:g} + {1:N1} hours = {2:g}",
-                        date1, interval.TotalHours, date2);
-   }
+    public static void Main7()
+    {
+        DateTime date1 = DateTime.SpecifyKind(new DateTime(2013, 3, 9, 10, 30, 0),
+                                              DateTimeKind.Local);
+        TimeSpan interval = new TimeSpan(48, 0, 0);
+        DateTime date2 = date1 + interval;
+        Console.WriteLine("{0:g} + {1:N1} hours = {2:g}",
+                          date1, interval.TotalHours, date2);
+    }
 }
+
 // The example displays the following output:
 //        3/9/2013 10:30 AM + 48.0 hours = 3/11/2013 10:30 AM
 // </Snippet8>

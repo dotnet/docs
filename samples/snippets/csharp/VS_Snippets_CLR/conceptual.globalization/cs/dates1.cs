ed16adeabc9609ab2405b9e82f0020602af64b0a
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2023-03-30 21:21:14 -0700

    remove binary serialization examples from globalization article

--- samples/snippets/csharp/VS_Snippets_CLR/conceptual.globalization/cs/dates1.cs
+++ samples/snippets/csharp/VS_Snippets_CLR/conceptual.globalization/cs/dates1.cs
@@ -5,25 +5,26 @@ using System.Threading;
 
 public class Example3
 {
-   static DateTime[] dates = { new DateTime(2012, 10, 11, 7, 06, 0),
+    static DateTime[] dates = { new DateTime(2012, 10, 11, 7, 06, 0),
                         new DateTime(2012, 10, 11, 18, 19, 0) };
 
-   public static void Main3()
-   {
-      Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("hr-HR");
-      ShowDayInfo();
-      Console.WriteLine();
-      Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-GB");
-      ShowDayInfo();
-   }
+    public static void Main3()
+    {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("hr-HR");
+        ShowDayInfo();
+        Console.WriteLine();
+        Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-GB");
+        ShowDayInfo();
+    }
 
-   private static void ShowDayInfo()
-   {
-      Console.WriteLine("Date: {0:D}", dates[0]);
-      Console.WriteLine("   Sunrise: {0:T}", dates[0]);
-      Console.WriteLine("   Sunset:  {0:T}", dates[1]);
-   }
+    private static void ShowDayInfo()
+    {
+        Console.WriteLine("Date: {0:D}", dates[0]);
+        Console.WriteLine("   Sunrise: {0:T}", dates[0]);
+        Console.WriteLine("   Sunset:  {0:T}", dates[1]);
+    }
 }
+
 // The example displays the following output:
 //       Date: 11. listopada 2012.
 //          Sunrise: 7:06:00

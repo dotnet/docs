8330514a6904db51f09fcf8f29879f3b8fc7198f
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2023-04-05 15:39:38 -0700

    reinstate snippets

---
+++ samples/snippets/csharp/VS_Snippets_CFX/cfx_wf_gettingstarted/cs/readint.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Activities;
+
+namespace ActivityLibrary1
+{
+    //<snippet1>
+    public sealed class ReadInt : NativeActivity<int>
+    {
+        [RequiredArgument]
+        public InArgument<string> BookmarkName { get; set; }
+
+        protected override void Execute(NativeActivityContext context)
+        {
+            string name = BookmarkName.Get(context);
+
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException("BookmarkName cannot be an Empty string.",
+                    "context");
+            }
+
+            context.CreateBookmark(name, new BookmarkCallback(OnReadComplete));
+        }
+
+        // NativeActivity derived activities that do asynchronous operations by calling
+        // one of the CreateBookmark overloads defined on System.Activities.NativeActivityContext
+        // must override the CanInduceIdle property and return true.
+        protected override bool CanInduceIdle
+        {
+            get { return true; }
+        }
+
+        void OnReadComplete(NativeActivityContext context, Bookmark bookmark, object state)
+        {
+            this.Result.Set(context, Convert.ToInt32(state));
+        }
+    }
+    //</snippet1>
+}

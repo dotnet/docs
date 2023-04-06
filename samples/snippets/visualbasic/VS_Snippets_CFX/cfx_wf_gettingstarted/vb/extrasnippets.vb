89a3e621f5b191ed6fd1f571e9b3a0d16f1143c2
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2023-04-05 18:16:00 -0700

    reset

---
+++ samples/snippets/visualbasic/VS_Snippets_CFX/cfx_wf_gettingstarted/vb/extrasnippets.vb
@@ -0,0 +1,81 @@
+﻿Imports System.Activities
+Imports System.Threading
+'<snippet14>
+Imports System.Activities.DurableInstancing
+'</snippet14>
+
+Public Class ExtraSnippets
+    Sub snippet2()
+        '<snippet2>
+        WorkflowInvoker.Invoke(New Workflow1())
+        '</snippet2>
+    End Sub
+
+    Sub snippet4()
+        '<snippet4>
+        Dim syncEvent As New AutoResetEvent(False)
+
+        Dim wfApp As New WorkflowApplication(New Workflow1())
+
+        wfApp.Completed = _
+            Sub(e As WorkflowApplicationCompletedEventArgs)
+                syncEvent.Set()
+            End Sub
+
+        wfApp.Aborted = _
+            Sub(e As WorkflowApplicationAbortedEventArgs)
+                Console.WriteLine(e.Reason)
+                syncEvent.Set()
+            End Sub
+
+        wfApp.OnUnhandledException = _
+            Function(e As WorkflowApplicationUnhandledExceptionEventArgs)
+                Console.WriteLine(e.UnhandledException)
+                Return UnhandledExceptionAction.Terminate
+            End Function
+
+        wfApp.Run()
+
+        '<snippet10>
+        syncEvent.WaitOne()
+        '</snippet10>
+        '</snippet4>
+    End Sub
+
+    Sub Step4Snippets()
+        Dim idleEvent As New AutoResetEvent(False)
+
+        '<snippet13>
+        Const connectionString As String = "Server=.\\SQLEXPRESS;Initial Catalog=Persistence;Integrated Security=SSPI"
+        '</snippet13>
+
+        '<snippet15>
+        Dim inputs As New Dictionary(Of String, Object)
+        inputs.Add("MaxNumber", 100)
+
+        Dim wfApp As New WorkflowApplication(New Workflow1(), inputs)
+
+        Dim store As New SqlWorkflowInstanceStore(connectionString)
+        wfApp.InstanceStore = store
+        '</snippet15>
+
+        '<snippet16>
+        ' Replace the Idle handler with a PersistableIdle handler.
+        'wfApp.Idle = _
+        '    Sub(e As WorkflowApplicationIdleEventArgs)
+        '        idleEvent.Set()
+        '    End Sub
+
+        wfApp.PersistableIdle = _
+            Function(e As WorkflowApplicationIdleEventArgs)
+                idleEvent.Set()
+                Return PersistableIdleAction.Persist
+            End Function
+        '</snippet16>
+
+        '<snippet17>
+        Console.WriteLine("Press any key to continue . . .")
+        Console.ReadKey()
+        '</snippet17>
+    End Sub
+End Class
---
+++ samples/snippets/visualbasic/VS_Snippets_CFX/cfx_wf_gettingstarted/vb/readint.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Activities
+
+'<snippet1>
+Public NotInheritable Class ReadInt
+    Inherits NativeActivity(Of Integer)
+
+    <RequiredArgument()>
+    Property BookmarkName() As InArgument(Of String)
+
+    Protected Overrides Sub Execute(ByVal context As NativeActivityContext)
+        Dim name As String
+        name = BookmarkName.Get(context)
+
+        If name = String.Empty Then
+            Throw New ArgumentException("BookmarkName cannot be an Empty string.",
+                "BookmarkName")
+        End If
+
+        context.CreateBookmark(name, New BookmarkCallback(AddressOf OnReadComplete))
+    End Sub
+
+    ' NativeActivity derived activities that do asynchronous operations by calling 
+    ' one of the CreateBookmark overloads defined on System.Activities.NativeActivityContext 
+    ' must override the CanInduceIdle property and return True.
+    Protected Overrides ReadOnly Property CanInduceIdle As Boolean
+        Get
+            Return True
+        End Get
+    End Property
+
+    Sub OnReadComplete(ByVal context As NativeActivityContext, ByVal bookmark As Bookmark, ByVal state As Object)
+        Result.Set(context, Convert.ToInt32(state))
+    End Sub
+
+End Class
+'</snippet1>

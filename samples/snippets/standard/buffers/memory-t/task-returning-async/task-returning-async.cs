d79fa92dae9d3f77e765635f9c26150baf0924bc
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2023-04-19 20:14:25 -0700

    add calling code

--- samples/snippets/standard/buffers/memory-t/task-returning-async/task-returning-async.cs
+++ samples/snippets/standard/buffers/memory-t/task-returning-async/task-returning-async.cs
@@ -18,4 +18,31 @@ public class Example
         });
     }
     // </Snippet1>
+
+    // user code
+    public static void Main()
+    {
+        using (var owner = MemoryPool<char>.Shared.Rent())
+        {
+            var memory = owner.Memory;
+            var span = memory.Span;
+            while (true)
+            {
+                int value = Int32.Parse(Console.ReadLine());
+                if (value < 0)
+                    return;
+
+                int numCharsWritten = ToBuffer(value, span);
+                Log(memory.Slice(0, numCharsWritten));
+            }
+        }
+    }
+
+    private static int ToBuffer(int value, Span<char> span)
+    {
+        string strValue = value.ToString();
+        int length = strValue.Length;
+        strValue.AsSpan().CopyTo(span.Slice(0, length));
+        return length;
+    }
 }

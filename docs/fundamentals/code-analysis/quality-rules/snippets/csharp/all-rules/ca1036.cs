6de01661c4aa1bc039a4bd267d9e2ea7b2677209
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2023-04-26 09:55:03 -0700

    respond to feedback

--- docs/fundamentals/code-analysis/quality-rules/snippets/csharp/all-rules/ca1036.cs
+++ docs/fundamentals/code-analysis/quality-rules/snippets/csharp/all-rules/ca1036.cs
@@ -39,13 +39,12 @@ namespace ca1036
                 return 1;
             }
 
-            RatingInformation? other = obj as RatingInformation; // avoid double casting
-            if (other is null)
+            if (obj is RatingInformation other)
             {
-                throw new ArgumentException("A RatingInformation object is required for comparison.", "obj");
+                return CompareTo(other);
             }
 
-            return CompareTo(other);
+            throw new ArgumentException("A RatingInformation object is required for comparison.", "obj");
         }
 
         public int CompareTo(RatingInformation? other)
@@ -76,12 +75,12 @@ namespace ca1036
         // Omitting Equals violates rule: OverrideMethodsOnComparableTypes.
         public override bool Equals(object? obj)
         {
-            RatingInformation? other = obj as RatingInformation; //avoid double casting
-            if (other is null)
+            if (obj is RatingInformation other)
             {
-                return false;
+                return this.CompareTo(other) == 0;
             }
-            return this.CompareTo(other) == 0;
+
+            return false;
         }
 
         // Omitting getHashCode violates rule: OverrideGetHashCodeOnOverridingEquals.

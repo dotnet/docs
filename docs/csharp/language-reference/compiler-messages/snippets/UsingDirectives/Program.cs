73df1467a7e18e9a1c7ab7a4089ee4759c0de53b
Bill Wagner <wiwagn@microsoft.com>
2023-04-28 15:15:48 -0400

    reorder for compiler errors
    Reorder the using examples so that only one produced an error before alias any type is released.

--- docs/csharp/language-reference/compiler-messages/snippets/UsingDirectives/Program.cs
+++ docs/csharp/language-reference/compiler-messages/snippets/UsingDirectives/Program.cs
@@ -2,12 +2,6 @@
 global using System.Text;
 // </GlobalUsing>
 
-// <UsingAlias>
-using JSON = System.Text.Json;
-using ValueMap = System.Collections.Generic.Dictionary<string, decimal>;
-using TimedData = (System.DateTime timeRecorded, decimal value);
-// </UsingAlias>
-
 // <UsingStatic>
 using static System.Console;
 // </UsingStatic>
@@ -16,8 +10,11 @@ using static System.Console;
 using static unsafe UnsafeExamples.UnsafeType;
 // </UsingUnsafeStatic>
 
-//using NullableString = System.String?;
-using NullableInt = System.Int32?;
+// <UsingAlias>
+using JSON = System.Text.Json;
+using ValueMap = System.Collections.Generic.Dictionary<string, decimal>;
+using TimedData = (System.DateTime timeRecorded, decimal value);
+// </UsingAlias>
 
 Console.WriteLine("Using directives");
 

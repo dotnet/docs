e226428e61cd3db409f036390683c95c6f92929f
Petr Kulikov <petr.kulikov@gmail.com>
2023-06-19 17:31:23 +0200

    Consolidated and revised the ref local reference (#35869)
    * Consolidated and revised the ref local reference
    
    * Fixed anchors in links
    
    * Fix interactive snippet

---
+++ docs/csharp/language-reference/statements/snippets/declarations/ReferenceVariables.cs
@@ -0,0 +1,60 @@
+public static class ReferenceVariables
+{
+    public static void Examples()
+    {
+        AliasToLocalVariable();
+        RefReassign();
+        RefReadonly();
+    }
+
+    private static void AliasToLocalVariable()
+    {
+        // <AliasToLocalVariable>
+        int a = 1;
+        ref int alias = ref a;
+        Console.WriteLine($"(a, alias) is ({a}, {alias})");  // output: (a, alias) is (1, 1)
+
+        a = 2;
+        Console.WriteLine($"(a, alias) is ({a}, {alias})");  // output: (a, alias) is (2, 2)
+
+        alias = 3;
+        Console.WriteLine($"(a, alias) is ({a}, {alias})");  // output: (a, alias) is (3, 3)
+        // </AliasToLocalVariable>
+    }
+
+    private static void RefReassign()
+    {
+        // <RefReassign>
+        void Display(int[] s) => Console.WriteLine(string.Join(" ", s));
+
+        int[] xs = { 0, 0, 0 };
+        Display(xs);
+
+        ref int element = ref xs[0];
+        element = 1;
+        Display(xs);
+
+        element = ref xs[^1];
+        element = 3;
+        Display(xs);
+        // Output:
+        // 0 0 0
+        // 1 0 0
+        // 1 0 3
+        // </RefReassign>
+    }
+
+    private static void RefReadonly()
+    {
+        // <RefReadonly>
+        int[] xs = { 1, 2, 3 };
+
+        ref readonly int element = ref xs[0];
+        // element = 100;  error CS0131: The left-hand side of an assignment must be a variable, property or indexer
+        Console.WriteLine(element);  // output: 1
+
+        element = ref xs[^1];
+        Console.WriteLine(element);  // output: 3
+        // </RefReadonly>
+    }
+}

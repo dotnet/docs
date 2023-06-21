3bc83994c4f2c7e7029344b6d9a7b3bfef846430
Bill Wagner <wiwagn@microsoft.com>
2023-06-21 10:51:13 -0400

    Explore primary constructors (#35742)
    * create outline
    
    Capture notes from the [primary constructors preview discussion](https://github.com/dotnet/csharplang/discussions/7109)
    
    * interim checkin
    
    * add primary constructor tutorial
    
    Explore primary constructors.
    
    * one small edit
    
    * Apply suggestions from code review
    
    Co-authored-by: Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
    
    * respond to feedback
    
    * add link from whats new page.
    
    * typo
    
    * Update docs/csharp/whats-new/tutorials/primary-constructors.md
    
    Co-authored-by: Petr Kulikov <petr.kulikov@gmail.com>
    
    * Update docs/csharp/whats-new/tutorials/primary-constructors.md
    
    Co-authored-by: Petr Kulikov <petr.kulikov@gmail.com>
    
    * respond to feedback
    
    Great suggestion to add examples that validate the primary constructor parameters.
    
    ---------
    
    Co-authored-by: Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
    Co-authored-by: Petr Kulikov <petr.kulikov@gmail.com>

---
+++ docs/csharp/whats-new/tutorials/snippets/primary-constructors/Distance.cs
@@ -0,0 +1,75 @@
+﻿namespace StructOne
+{
+    // <ReadonlyStruct>
+    public readonly struct Distance(double dx, double dy)
+    {
+        public readonly double Magnitude = Math.Sqrt(dx * dx + dy * dy);
+        public readonly double Direction = Math.Atan2(dy, dx);
+    }
+    // </ReadonlyStruct>
+}
+
+namespace Lowered
+{
+    // <StructOneLowered>
+    public readonly struct Distance
+    {
+        public readonly double Magnitude { get; }
+
+        public readonly double Direction { get; }
+
+        public Distance(double dx, double dy)
+        {
+            Magnitude = Math.Sqrt(dx * dx + dy * dy);
+            Direction = Math.Atan2(dy, dx);
+        }
+    }
+    // </StructOneLowered>
+}
+
+namespace StructTwo
+{
+    // <MutableStruct>
+    public struct Distance(double dx, double dy)
+    {
+        public readonly double Magnitude => Math.Sqrt(dx * dx + dy * dy);
+        public readonly double Direction => Math.Atan2(dy, dx);
+
+        public void Translate(double deltaX, double deltaY)
+        {
+            dx += deltaX;
+            dy += deltaY;
+        }
+
+        public Distance() : this(0,0) { }
+    }
+    // </MutableStruct>
+}
+
+namespace LoweredMutable
+{
+    // <StructTwoLowered>
+    public struct Distance
+    {
+        private double __unspeakable_dx;
+        private double __unspeakable_dy;
+
+        public readonly double Magnitude => Math.Sqrt(__unspeakable_dx * __unspeakable_dx + __unspeakable_dy * __unspeakable_dy);
+        public readonly double Direction => Math.Atan2(__unspeakable_dy, __unspeakable_dx);
+
+        public void Translate(double deltaX, double deltaY)
+        {
+            __unspeakable_dx += deltaX;
+            __unspeakable_dy += deltaY;
+        }
+
+        public Distance(double dx, double dy)
+        {
+            __unspeakable_dx = dx;
+            __unspeakable_dy = dy;
+        }
+        public Distance() : this(0, 0) { }
+    }
+    // </StructTwoLowered>
+
+}

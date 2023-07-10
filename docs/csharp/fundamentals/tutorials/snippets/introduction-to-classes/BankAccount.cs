be6bac9b31e3f3204835e335850ece21f38024cb
imehdi7 <24858712+imehdi7@users.noreply.github.com>
2023-07-10 16:41:28 +0200

    Update BankAccount.cs (#36138)
    addes s_ prefix to accountNumberSeed 
    
    added _ prefix to allTransactions

--- docs/csharp/fundamentals/tutorials/snippets/introduction-to-classes/BankAccount.cs
+++ docs/csharp/fundamentals/tutorials/snippets/introduction-to-classes/BankAccount.cs
@@ -13,7 +13,7 @@ public class BankAccount
         get
         {
             decimal balance = 0;
-            foreach (var item in allTransactions)
+            foreach (var item in _allTransactions)
             {
                 balance += item.Amount;
             }
@@ -23,12 +23,12 @@ public class BankAccount
     }
 #endregion
 
-    private static int accountNumberSeed = 1234567890;
+    private static int s_accountNumberSeed = 1234567890;
 #region Constructor
     public BankAccount(string name, decimal initialBalance)
     {
-        Number = accountNumberSeed.ToString();
-        accountNumberSeed++;
+        Number = s_accountNumberSeed.ToString();
+        s_accountNumberSeed++;
 
         Owner = name;
         MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
@@ -36,7 +36,7 @@ public class BankAccount
 #endregion
 
 #region TransactionDeclaration
-    private List<Transaction> allTransactions = new List<Transaction>();
+    private List<Transaction> _allTransactions = new List<Transaction>();
 #endregion
 
 #region DepositAndWithdrawal
@@ -47,7 +47,7 @@ public class BankAccount
             throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
         }
         var deposit = new Transaction(amount, date, note);
-        allTransactions.Add(deposit);
+        _allTransactions.Add(deposit);
     }
 
     public void MakeWithdrawal(decimal amount, DateTime date, string note)
@@ -61,7 +61,7 @@ public class BankAccount
             throw new InvalidOperationException("Not sufficient funds for this withdrawal");
         }
         var withdrawal = new Transaction(-amount, date, note);
-        allTransactions.Add(withdrawal);
+        _allTransactions.Add(withdrawal);
     }
 #endregion
 
@@ -72,7 +72,7 @@ public class BankAccount
 
         decimal balance = 0;
         report.AppendLine("Date\t\tAmount\tBalance\tNote");
-        foreach (var item in allTransactions)
+        foreach (var item in _allTransactions)
         {
             balance += item.Amount;
             report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{balance}\t{item.Notes}");

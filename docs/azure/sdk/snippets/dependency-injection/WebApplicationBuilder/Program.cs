2a7f088694acc982fb2499d8247ec17926fa7301
Scott Addie <scott.addie@microsoft.com>
2023-07-18 14:59:51 -0500

    Update Azure SDK DI doc for additional app builder

---
+++ docs/azure/sdk/snippets/dependency-injection/WebApplicationBuilder/Program.cs
@@ -0,0 +1,55 @@
+﻿#region snippet_WebApplicationBuilder
+using Azure.Identity;
+using Microsoft.Extensions.Azure;
+
+var keyVaultUrl = new Uri("");
+var storageUrl = new Uri("");
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAzureClients(clientBuilder =>
+{
+    clientBuilder.AddSecretClient(keyVaultUrl);
+    clientBuilder.AddBlobServiceClient(storageUrl);
+    clientBuilder.UseCredential(new DefaultAzureCredential());
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+WebApplication app = builder.Build();
+#endregion snippet_WebApplicationBuilder
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+string[] summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    WeatherForecast[] forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+await app.RunAsync();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}

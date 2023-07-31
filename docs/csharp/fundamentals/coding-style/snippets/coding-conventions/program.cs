49d553fc8d467f89df96df23744b7d153d51a9be
Bill Wagner <wiwagn@microsoft.com>
2023-07-31 08:47:27 -0400

    Update coding guidelines (#36428)
    * rearrange content
    
    Make the new content organization follow better ordering.
    
    * rewrite and update
    
    Rewrite to match the docs team's accepted style.
    
    * Edits to fix `var` issues
    
    Fixes #26787: clarify "obvious"
    Fixes #32633: add explanation, update variable names.
    Fixes #34940: Explain that `var` is preferred in LINQ queries, despite other rules.
    
    * Fix naming conventions
    
    Fixes #30626: Clarify (again) that these are our guidelines, not yours. Point out that it's not the VS default, but a configuration option.
    Fixes #30642: Again, our style.
    Fixes #30799: Change constant style from ALL_CAPS to PascalCase to match runtime repo.
    Fixes #33959: Update variable names so delegate types are PascalCased and instances of a delegate are camelCase. Add clarifying text for the same.
    
    * Fix exception example
    
    Fixes #31951 : Rewrite the exception example so that it's still obvious what can fail, but couldn't be easily anticipated before making the computation.
    
    * Fixes #30897
    
    Move the Generic type parameter naming conventions to the general naming conventions.
    
    * Fix build warnings
    
    * Reference runtime convention
    
    The use of `_` and `s_` follow the runtime conventions. I'd missed that in the previous commit.
    
    * Apply suggestions from code review
    
    Co-authored-by: David Pine <david.pine@microsoft.com>
    
    ---------
    
    Co-authored-by: David Pine <david.pine@microsoft.com>

--- docs/csharp/fundamentals/coding-style/snippets/coding-conventions/program.cs
+++ docs/csharp/fundamentals/coding-style/snippets/coding-conventions/program.cs
@@ -31,11 +31,11 @@ namespace Coding_Conventions_Examples
                 PerformanceCounterCategory();
             //</snippet1>
 
-            int val1 = 1;
-            int val2 = 2;
-            int val3 = 3;
+            int startX = 1;
+            int endX = 2;
+            int previousX = 3;
             //<snippet2>
-            if ((val1 > val2) && (val1 > val3))
+            if ((startX > endX) && (startX > previousX))
             {
                 // Take appropriate action.
             }
@@ -71,13 +71,13 @@ namespace Coding_Conventions_Examples
             //</snippet7>
 
             //<snippet8>
-            var var1 = "This is clearly a string.";
-            var var2 = 27;
+            var message = "This is clearly a string.";
+            var currentTemperature = 27;
             //</snippet8>
 
             //<snippet9>
-            int var3 = Convert.ToInt32(Console.ReadLine());
-            int var4 = ExampleClass.ResultSoFar();
+            int numberOfIterations = Convert.ToInt32(Console.ReadLine());
+            int currentMaximum = ExampleClass.ResultSoFar();
             //</snippet9>
 
             //<snippet10>
@@ -133,32 +133,32 @@ namespace Coding_Conventions_Examples
 
 
             // #16 is below Main.
-            Console.WriteLine(GetValueFromArray(vowels1, 1));
+            Console.WriteLine(ComputeDistance(1,2,3,4));
 
             // 17 requires System.Drawing
             //<snippet17a>
-            Font font1 = new Font("Arial", 10.0f);
+            Font bodyStyle = new Font("Arial", 10.0f);
             try
             {
-                byte charset = font1.GdiCharSet;
+                byte charset = bodyStyle.GdiCharSet;
             }
             finally
             {
-                if (font1 != null)
+                if (bodyStyle != null)
                 {
-                    ((IDisposable)font1).Dispose();
+                    ((IDisposable)bodyStyle).Dispose();
                 }
             }
             //</snippet17a>
             //<snippet17b>
-            using (Font font2 = new Font("Arial", 10.0f))
+            using (Font arial = new Font("Arial", 10.0f))
             {
-                byte charset2 = font2.GdiCharSet;
+                byte charset2 = arial.GdiCharSet;
             }
             //</snippet17b>
             //<snippet17c>
-            using Font font3 = new Font("Arial", 10.0f);
-            byte charset3 = font3.GdiCharSet;
+            using Font normalStyle = new Font("Arial", 10.0f);
+            byte charset3 = normalStyle.GdiCharSet;
             //</snippet17c>
 
             //<snippet18>
@@ -180,24 +180,24 @@ namespace Coding_Conventions_Examples
 
 
             //<snippet19>
-            var instance1 = new ExampleClass();
+            var firstExample = new ExampleClass();
             //</snippet19>
             // Can't show `ExampleClass instance1 = new()` because this project targets net48.
 
             //<snippet20>
-            ExampleClass instance2 = new ExampleClass();
+            ExampleClass secondExample = new ExampleClass();
             //</snippet20>
 
             //<snippet21a>
-            var instance3 = new ExampleClass { Name = "Desktop", ID = 37414,
+            var thirdExample = new ExampleClass { Name = "Desktop", ID = 37414,
                 Location = "Redmond", Age = 2.3 };
             //</snippet21a>
             //<snippet21b>
-            var instance4 = new ExampleClass();
-            instance4.Name = "Desktop";
-            instance4.ID = 37414;
-            instance4.Location = "Redmond";
-            instance4.Age = 2.3;
+            var fourthExample = new ExampleClass();
+            fourthExample.Name = "Desktop";
+            fourthExample.ID = 37414;
+            fourthExample.Location = "Redmond";
+            fourthExample.Age = 2.3;
             //</snippet21b>
 
             // #22 and #23 are in Coding_Conventions_WF, below.
@@ -276,15 +276,15 @@ namespace Coding_Conventions_Examples
         }
 
         //<snippet16>
-        static string GetValueFromArray(string[] array, int index)
+        static double ComputeDistance(double x1, double y1, double x2, double y2)
         {
             try
             {
-                return array[index];
+                return Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
             }
-            catch (System.IndexOutOfRangeException ex)
+            catch (System.ArithmeticException ex)
             {
-                Console.WriteLine("Index is out of range: {0}", index);
+                Console.WriteLine($"Arithmetic overflow or underflow: {ex}");
                 throw;
             }
         }
@@ -438,4 +438,32 @@ namespace Coding_Conventions_WF2
         {
         }
     }
+}
+
+namespace GenericTypeParameters
+{
+    public class WrapParameters
+    {
+        //<TypeParametersOne>
+        public interface ISessionChannel<TSession> { /*...*/ }
+        public delegate TOutput Converter<TInput, TOutput>(TInput from);
+        public class List<T> { /*...*/ }
+        //</TypeParametersOne>
+
+        //<TypeParametersTwo>
+        public int IComparer<T>() { return 0; }
+        public delegate bool Predicate<T>(T item);
+        public struct Nullable<T> where T : struct { /*...*/ }
+        //</TypeParametersTwo>
+
+        class wrap
+        {
+            //<TypeParametersThree>
+            public interface ISessionChannel<TSession>
+            {
+                TSession Session { get; }
+            }
+            //</TypeParametersThree>
+        }
+    }//WrapParameters
 }

6db9f08fd9ffecb0b362cdd9cd4829ba0753fb9d
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2023-08-05 19:12:57 +0200

    Correct formatting of all language-ref-1 snippets using fantomas

--- samples/snippets/fsharp/lang-ref-1/snippet101.fs
+++ samples/snippets/fsharp/lang-ref-1/snippet101.fs
@@ -1,7 +1,8 @@
-let list1 = [ 1; 2; 3]
+let list1 = [ 1; 2; 3 ]
 // Error: duplicate definition.
 let list1 = []
+
 let function1 () =
-   let list1 = [1; 2; 3]
-   let list1 = []
-   list1
+    let list1 = [ 1; 2; 3 ]
+    let list1 = []
+    list1
--- samples/snippets/fsharp/lang-ref-1/snippet1602.fs
+++ samples/snippets/fsharp/lang-ref-1/snippet1602.fs
@@ -1,1 +1,1 @@
-let array3 = [| for i in 1 .. 10 -> i * i |]
+let array3 = [| for i in 1..10 -> i * i |]

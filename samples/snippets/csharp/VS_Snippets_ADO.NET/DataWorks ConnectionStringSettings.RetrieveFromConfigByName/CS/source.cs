881cff0aca6bed7deb3b613c9c3ff64a3c9dc7d5
Dick Baker <dickhub@dogbol.com>
2023-08-13 10:03:44 +0100

    Update GetConnectionStringByName
    embrace NRT

--- samples/snippets/csharp/VS_Snippets_ADO.NET/DataWorks ConnectionStringSettings.RetrieveFromConfigByName/CS/source.cs
+++ samples/snippets/csharp/VS_Snippets_ADO.NET/DataWorks ConnectionStringSettings.RetrieveFromConfigByName/CS/source.cs
@@ -1,4 +1,3 @@
-﻿
 using System;
 using System.Configuration;
 
@@ -6,27 +5,21 @@ class Program
 {
     static void Main()
     {
-        string s = GetConnectionStringByName("NorthwindSQL");
+        string? s = GetConnectionStringByName("NorthwindSQL");
         Console.WriteLine(s);
         Console.ReadLine();
     }
     // <Snippet1>
     // Retrieves a connection string by name.
     // Returns null if the name is not found.
-    static string GetConnectionStringByName(string name)
+    static string? GetConnectionStringByName(string name)
     {
-        // Assume failure.
-        string returnValue = null;
-
         // Look for the name in the connectionStrings section.
-        ConnectionStringSettings settings =
+        ConnectionStringSettings? settings =
             ConfigurationManager.ConnectionStrings[name];
 
-        // If found, return the connection string.
-        if (settings != null)
-            returnValue = settings.ConnectionString;
-
-        return returnValue;
+        // If found, return the connection string (otherwise return null)
+        return settings?.ConnectionString;
     }
     // </Snippet1>
 }

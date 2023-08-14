df57a53a0969a418df63e53dd17bbe9a879cb6ea
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2023-08-14 11:04:43 -0700

    Fix backtracking example (#36660)

--- docs/standard/base-types/snippets/regex-backtracking/vb/backtracking2.vb
+++ docs/standard/base-types/snippets/regex-backtracking/vb/backtracking2.vb
@@ -4,15 +4,15 @@ Option Strict On
 ' <Snippet2>
 Imports System.Text.RegularExpressions
 
-Module Example
-    Public Sub Main()
+Module Example2
+    Public Sub Run()
         Dim input As String = "Essential services are provided by regular expressions."
         Dim pattern As String = ".*(es)"
         Dim m As Match = Regex.Match(input, pattern, RegexOptions.IgnoreCase)
         If m.Success Then
-            Console.WriteLine("'{0}' found at position {1}", _
+            Console.WriteLine("'{0}' found at position {1}",
                               m.Value, m.Index)
-            Console.WriteLine("'es' found at position {0}", _
+            Console.WriteLine("'es' found at position {0}",
                               m.Groups(1).Index)
         End If
     End Sub

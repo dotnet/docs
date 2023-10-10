b42263651ac65ce08da3ac80fae97aff22b2cdfa
Bartosz Klonowski <Bartosz-Klonowski@tlen.pl>
2023-10-10 17:30:13 +0200

    Implement all code snippets from async-scenarios as a class

---
+++ docs/csharp/asynchronous-programming/snippets/async-scenarios/Program.cs
@@ -0,0 +1,118 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using Microsoft.AspNetCore.Mvc;
+
+class Button
+{
+    public Func<object, object, Task>? Clicked
+    {
+        get;
+        internal set;
+    }
+}
+
+class DamageResult
+{
+    public int Damage
+    {
+        get { return 0; }
+    }
+}
+
+class User
+{
+    public bool isEnabled
+    {
+        get;
+        set;
+    }
+
+    public int id
+    {
+        get;
+        set;
+    }
+}
+
+public class Program
+{
+    private static readonly Button s_downloadButton = new();
+    private static readonly Button s_calculateButton = new();
+
+    private static readonly HttpClient s_httpClient = new();
+
+    private static void Calculate()
+    {
+        static DamageResult CalculateDamageDone()
+        {
+            return new DamageResult()
+            {
+                // Code omitted:
+                //
+                // Does an expensive calculation and returns
+                // the result of that calculation.
+            };
+        }
+
+        s_calculateButton.Clicked += async (o, e) =>
+        {
+            // This line will yield control to the UI while CalculateDamageDone()
+            // performs its work. The UI thread is free to perform other work.
+            var damageResult = await Task.Run(() => CalculateDamageDone());
+            DisplayDamage(damageResult);
+        };
+    }
+
+    private static void DisplayDamage(DamageResult damage)
+    {
+        Console.WriteLine(damage.Damage);
+    }
+
+    private static void Download(string URL)
+    {
+        s_downloadButton.Clicked += async (o, e) =>
+        {
+            // This line will yield control to the UI as the request
+            // from the web service is happening.
+            //
+            // The UI thread is now free to perform other work.
+            var stringData = await s_httpClient.GetStringAsync(URL);
+            DoSomethingWithData(stringData);
+        };
+    }
+
+    private static void DoSomethingWithData(object stringData)
+    {
+        Console.WriteLine("Displaying data: ", stringData);
+    }
+
+    private static async Task<User> GetUserAsync(int userId)
+    {
+        // Code omitted:
+        //
+        // Given a user Id {userId}, retrieves a User object corresponding
+        // to the entry in the database with {userId} as its Id.
+
+        return await Task.FromResult(new User() { id = userId });
+    }
+
+    private static async Task<IEnumerable<User>> GetUsersAsync(IEnumerable<int> userIds)
+    {
+        var getUserTasks = new List<Task<User>>();
+        foreach (int userId in userIds)
+        {
+            getUserTasks.Add(GetUserAsync(userId));
+        }
+
+        return await Task.WhenAll(getUserTasks);
+    }
+
+    [HttpGet, Route("DotNetCount")]
+    static public async Task<int> GetDotNetCount(string URL)
+    {
+        // Suspends GetDotNetCount() to allow the caller (the web server)
+        // to accept another request, rather than blocking on this one.
+        var html = await s_httpClient.GetStringAsync(URL);
+        return Regex.Matches(html, @"\.NET").Count;
+    }
+}

6befcd01d7de7ff23adb67523f27165eaf7b5a97
David Pine <david.pine@microsoft.com>
2023-10-19 15:36:08 -0500

    Update source with markers

--- docs/core/resilience/snippets/resilience/Program.cs
+++ docs/core/resilience/snippets/resilience/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿// <setup>
+using Microsoft.Extensions.DependencyInjection;
 using Polly;
 using Polly.CircuitBreaker;
 using Polly.Registry;
@@ -19,21 +20,28 @@ services.AddResiliencePipeline(key, static builder =>
     // See: https://www.pollydocs.org/strategies/timeout.html
     builder.AddTimeout(TimeSpan.FromSeconds(1.5));
 });
+// </setup>
 
+// <enricher>
 services.AddResilienceEnricher();
+// </enricher>
 
+// <pipeline>
 using ServiceProvider provider = services.BuildServiceProvider();
 
 ResiliencePipelineProvider<string> pipelineProvider =
     provider.GetRequiredService<ResiliencePipelineProvider<string>>();
 
 ResiliencePipeline pipeline = pipelineProvider.GetPipeline(key);
+// </pipeline>
 
+// <execute>
 await pipeline.ExecuteAsync(static async cancellationToken =>
 {
     // Code that could potentially fail.
 
     await ValueTask.CompletedTask;
 });
+// </execute>
 
 Console.WriteLine("Done...");

e36c495e3bb86ff133fcc1dcde079610a065ad39
David Pine <david.pine@microsoft.com>
2023-10-20 08:21:47 -0500

    Resiliency in .NET content (#37270)
    * Initial bits
    
    * Trying to explore the resilience libs
    
    * Updates to resiliency
    
    * More updates
    
    * Updates to docs
    
    * More fixes and updates
    
    * Fix heading
    
    * More HTTP bits
    
    * Apply suggestions from code review
    
    Co-authored-by: Martin Taillefer <geeknoid@users.noreply.github.com>
    
    * More tweaks
    
    * Added image
    
    * Additions
    
    * More updates
    
    * Apply suggestions from code review
    
    Co-authored-by: Martin Costello <martin@martincostello.com>
    Co-authored-by: martintmk <103487740+martintmk@users.noreply.github.com>
    
    * Clean up and updates
    
    * Add context
    
    * Retitle a bit
    
    * A quick fix
    
    * Resilience in TOC
    
    * Updates to headings
    
    * Fix TOC entry
    
    * Added a bit more detail
    
    * Update diagram
    
    * Updates to HTTP resiliency content
    
    * Add more configuration bits
    
    * From peer feedback
    
    * Clean up
    
    * Apply suggestions from code review
    
    Co-authored-by: Martin Costello <martin@martincostello.com>
    
    * Update image
    
    * Apply suggestions from code review
    
    Co-authored-by: martintmk <103487740+martintmk@users.noreply.github.com>
    
    * Updates from peer review
    
    * Major updates to the resilience bits
    
    * A bit closer
    
    * The id's should work
    
    * There we go
    
    * code bits for resilience
    
    * Update source with markers
    
    * Apply suggestions from code review
    
    Co-authored-by: martintmk <103487740+martintmk@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Genevieve Warren <24882762+gewarren@users.noreply.github.com>
    Co-authored-by: martintmk <103487740+martintmk@users.noreply.github.com>
    Co-authored-by: Martin Costello <martin@martincostello.com>
    
    * Address all feedback
    
    * More clean up, final touches
    
    ---------
    
    Co-authored-by: Martin Taillefer <geeknoid@users.noreply.github.com>
    Co-authored-by: Martin Costello <martin@martincostello.com>
    Co-authored-by: martintmk <103487740+martintmk@users.noreply.github.com>
    Co-authored-by: Genevieve Warren <24882762+gewarren@users.noreply.github.com>

---
+++ docs/core/resilience/snippets/http-resilience/Program.RetryOptions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Http.Resilience;
+
+internal partial class Program
+{
+    private static void ConfigureRetryOptions(HostApplicationBuilder builder)
+    {
+        // <options>
+        var section = builder.Configuration.GetSection("RetryOptions");
+
+        builder.Services.Configure<HttpRetryStrategyOptions>(section);
+        // </options>
+    }
+}

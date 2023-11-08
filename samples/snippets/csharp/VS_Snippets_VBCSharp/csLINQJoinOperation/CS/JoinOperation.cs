c7bbd478fbbe9a4f0ca8953d21a84bc445e3f229
Bill Wagner <wiwagn@microsoft.com>
2023-11-08 12:32:19 -0500

    more LINQ samples

--- samples/snippets/csharp/VS_Snippets_VBCSharp/csLINQJoinOperation/CS/JoinOperation.cs
+++ samples/snippets/csharp/VS_Snippets_VBCSharp/csLINQJoinOperation/CS/JoinOperation.cs
@@ -1,129 +1,124 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿namespace Sandbox;
 
-namespace Sandbox
+class JoinOperation
 {
-    class JoinOperation
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Join.Example();
+    }
+
+    static class Join
+    {
+        // <SnippetJoin>
+        class Product
         {
-            Join.Example();
+            public string? Name { get; set; }
+            public int CategoryId { get; set; }
         }
 
-        static class Join
+        class Category
         {
-            // <SnippetJoin>
-            class Product
+            public int Id { get; set; }
+            public string? CategoryName { get; set; }
+        }
+
+        public static void Example()
+        {
+            List<Product> products =
+            [
+                new Product { Name = "Cola", CategoryId = 0 },
+                new Product { Name = "Tea", CategoryId = 0 },
+                new Product { Name = "Apple", CategoryId = 1 },
+                new Product { Name = "Kiwi", CategoryId = 1 },
+                new Product { Name = "Carrot", CategoryId = 2 },
+            ];
+
+            List<Category> categories =
+            [
+                new Category { Id = 0, CategoryName = "Beverage" },
+                new Category { Id = 1, CategoryName = "Fruit" },
+                new Category { Id = 2, CategoryName = "Vegetable" }
+            ];
+
+            // Join products and categories based on CategoryId
+            var query = from product in products
+                        join category in categories on product.CategoryId equals category.Id
+                        select new { product.Name, category.CategoryName };
+
+            foreach (var item in query)
             {
-                public string? Name { get; set; }
-                public int CategoryId { get; set; }
+                Console.WriteLine($"{item.Name} - {item.CategoryName}");
             }
 
-            class Category
+            // This code produces the following output:
+            //
+            // Cola - Beverage
+            // Tea - Beverage
+            // Apple - Fruit
+            // Kiwi - Fruit
+            // Carrot - Vegetable
+        }
+        // </SnippetJoin>
+    }
+
+    static class GroupJoin
+    {
+        // <SnippetGroupJoin>
+        class Product
+        {
+            public string? Name { get; set; }
+            public int CategoryId { get; set; }
+        }
+
+        class Category
+        {
+            public int Id { get; set; }
+            public string? CategoryName { get; set; }
+        }
+
+        public static void Example()
+        {
+            List<Product> products =
+            [
+                new Product { Name = "Cola", CategoryId = 0 },
+                new Product { Name = "Tea", CategoryId = 0 },
+                new Product { Name = "Apple", CategoryId = 1 },
+                new Product { Name = "Kiwi", CategoryId = 1 },
+                new Product { Name = "Carrot", CategoryId = 2 },
+            ];
+
+            List<Category> categories =
+            [
+                new Category { Id = 0, CategoryName = "Beverage" },
+                new Category { Id = 1, CategoryName = "Fruit" },
+                new Category { Id = 2, CategoryName = "Vegetable" }
+            ];
+
+            // Join categories and product based on CategoryId and grouping result
+            var productGroups = from category in categories
+                                join product in products on category.Id equals product.CategoryId into productGroup
+                                select productGroup;
+
+            foreach (IEnumerable<Product> productGroup in productGroups)
             {
-                public int Id { get; set; }
-                public string? CategoryName { get; set; }
-            }
-
-            public static void Example()
-            {
-                List<Product> products = new List<Product>
+                Console.WriteLine("Group");
+                foreach (Product product in productGroup)
                 {
-                    new Product { Name = "Cola", CategoryId = 0 },
-                    new Product { Name = "Tea", CategoryId = 0 },
-                    new Product { Name = "Apple", CategoryId = 1 },
-                    new Product { Name = "Kiwi", CategoryId = 1 },
-                    new Product { Name = "Carrot", CategoryId = 2 },
-                };
-
-                List<Category> categories = new List<Category>
-                {
-                    new Category { Id = 0, CategoryName = "Beverage" },
-                    new Category { Id = 1, CategoryName = "Fruit" },
-                    new Category { Id = 2, CategoryName = "Vegetable" }
-                };
-
-                // Join products and categories based on CategoryId
-                var query = from product in products
-                            join category in categories on product.CategoryId equals category.Id
-                            select new { product.Name, category.CategoryName };
-
-                foreach (var item in query)
-                {
-                    Console.WriteLine($"{item.Name} - {item.CategoryName}");
+                    Console.WriteLine($"{product.Name,8}");
                 }
-
-                // This code produces the following output:
-                //
-                // Cola - Beverage
-                // Tea - Beverage
-                // Apple - Fruit
-                // Kiwi - Fruit
-                // Carrot - Vegetable
             }
-            // </SnippetJoin>
+
+            // This code produces the following output:
+            //
+            // Group
+            //     Cola
+            //      Tea
+            // Group
+            //    Apple
+            //     Kiwi
+            // Group
+            //   Carrot
         }
-
-        static class GroupJoin
-        {
-            // <SnippetGroupJoin>
-            class Product
-            {
-                public string? Name { get; set; }
-                public int CategoryId { get; set; }
-            }
-
-            class Category
-            {
-                public int Id { get; set; }
-                public string? CategoryName { get; set; }
-            }
-
-            public static void Example()
-            {
-                List<Product> products = new List<Product>
-                {
-                    new Product { Name = "Cola", CategoryId = 0 },
-                    new Product { Name = "Tea", CategoryId = 0 },
-                    new Product { Name = "Apple", CategoryId = 1 },
-                    new Product { Name = "Kiwi", CategoryId = 1 },
-                    new Product { Name = "Carrot", CategoryId = 2 },
-                };
-
-                List<Category> categories = new List<Category>
-                {
-                    new Category { Id = 0, CategoryName = "Beverage" },
-                    new Category { Id = 1, CategoryName = "Fruit" },
-                    new Category { Id = 2, CategoryName = "Vegetable" }
-                };
-
-                // Join categories and product based on CategoryId and grouping result
-                var productGroups = from category in categories
-                                    join product in products on category.Id equals product.CategoryId into productGroup
-                                    select productGroup;
-
-                foreach (IEnumerable<Product> productGroup in productGroups)
-                {
-                    Console.WriteLine("Group");
-                    foreach (Product product in productGroup)
-                    {
-                        Console.WriteLine($"{product.Name,8}");
-                    }
-                }
-
-                // This code produces the following output:
-                //
-                // Group
-                //     Cola
-                //      Tea
-                // Group
-                //    Apple
-                //     Kiwi
-                // Group
-                //   Carrot
-            }
-            // </SnippetGroupJoin>
-        }
+        // </SnippetGroupJoin>
     }
 }

31b1f1dcc8a613fb543d4b9c39e1911a3d32a352
Dapeng Zhang <ufo54153@gmail.com>
2023-11-22 10:10:29 +0800

    Update docs/azure/sdk/snippets/unit-testing/ResourceManager/ArmCollection.cs
    Co-authored-by: David Pine <david.pine@microsoft.com>

--- docs/azure/sdk/snippets/unit-testing/ResourceManager/ArmCollection.cs
+++ docs/azure/sdk/snippets/unit-testing/ResourceManager/ArmCollection.cs
@@ -21,7 +21,7 @@ public abstract partial class ArmCollection
 
     protected internal virtual ArmClient Client { get; }
 
-    private readonly ConcurrentDictionary<Type, object> _clientCache = new ConcurrentDictionary<Type, object>();
+    private readonly ConcurrentDictionary<Type, object> _clientCache = new();
 
     [EditorBrowsable(EditorBrowsableState.Never)]
     public virtual T GetCachedClient<T>(Func<ArmClient, T> clientFactory)

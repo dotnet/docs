3bf7c58d45d5b2b0f11a6b7b1136ee01e541da2d
Bill Wagner <wiwagn@microsoft.com>
2023-11-22 10:04:22 -0500

    Revert "yup, thought that  would red-flag us, and CI barfed at"
    This reverts commit 288a27a7176906d6e4422611a8c8e6a123e5664a.

---
+++ samples/snippets/csharp/VS_Snippets_ADO.NET/DataWorks DataSet.Merge/CS/source.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+static class Program
+{
+    static void Main()
+    {
+        var connectionString = GetConnectionString();
+        ConnectToData(connectionString);
+        Console.ReadLine();
+    }
+    static void ConnectToData(string connectionString)
+    {
+        // <Snippet1>
+        using (SqlConnection connection =
+                   new(connectionString))
+        {
+            SqlDataAdapter adapter =
+                new(
+                "SELECT CustomerID, CompanyName FROM dbo.Customers",
+                connection);
+
+            connection.Open();
+
+            DataSet customers = new();
+            adapter.FillSchema(customers, SchemaType.Source, "Customers");
+            adapter.Fill(customers, "Customers");
+
+            DataSet orders = new();
+            orders.ReadXml("Orders.xml", XmlReadMode.ReadSchema);
+            orders.AcceptChanges();
+
+            customers.Merge(orders, true, MissingSchemaAction.AddWithKey);
+            // </Snippet1>
+        }
+    }
+
+    static string GetConnectionString() =>
+        // To avoid storing the connection string in your code,
+        // you can retrieve it from a configuration file.
+        "Data Source=(local);Initial Catalog=Northwind;"
+            + "Integrated Security=SSPI";
+}

6e33f4d80652b94059553a8a1d4bf528356ae456
Bill Wagner <wiwagn@microsoft.com>
2023-12-15 10:26:18 -0500

    typo

--- docs/csharp/language-reference/operators/snippets/shared/LambdaOperator.cs
+++ docs/csharp/language-reference/operators/snippets/shared/LambdaOperator.cs
@@ -27,7 +27,7 @@ public static class LambdaOperator
     private static void WithExplicitTypes()
     {
         // <SnippetExplicitTypes>
-        int[] numbers = { 4, 7, 10 ;
+        int[] numbers = { 4, 7, 10 };
         int product = numbers.Aggregate(1, (int interim, int next) => interim * next);
         Console.WriteLine(product);   // output: 280
         // </SnippetExplicitTypes>

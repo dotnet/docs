f4e0530fe3dd87a673c818bdc5e4bb144977a6bd
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2023-12-28 13:05:01 -0800

    fix errors

--- docs/fundamentals/runtime-libraries/snippets/System.Globalization/CultureInfo/csharp/change1.cs
+++ docs/fundamentals/runtime-libraries/snippets/System.Globalization/CultureInfo/csharp/change1.cs
@@ -2,22 +2,22 @@
 using System;
 using System.Globalization;
 
-public class Example
+public class ChangeEx1
 {
-   public static void Main()
-   {
-      CultureInfo current = CultureInfo.CurrentCulture;
-      Console.WriteLine("The current culture is {0}", current.Name);
-      CultureInfo newCulture;
-      if (current.Name.Equals("fr-FR"))
-         newCulture = new CultureInfo("fr-LU");
-      else
-         newCulture = new CultureInfo("fr-FR");
+    public static void Main()
+    {
+        CultureInfo current = CultureInfo.CurrentCulture;
+        Console.WriteLine("The current culture is {0}", current.Name);
+        CultureInfo newCulture;
+        if (current.Name.Equals("fr-FR"))
+            newCulture = new CultureInfo("fr-LU");
+        else
+            newCulture = new CultureInfo("fr-FR");
 
-      CultureInfo.CurrentCulture = newCulture;
-      Console.WriteLine("The current culture is now {0}",
-                        CultureInfo.CurrentCulture.Name);
-   }
+        CultureInfo.CurrentCulture = newCulture;
+        Console.WriteLine("The current culture is now {0}",
+                          CultureInfo.CurrentCulture.Name);
+    }
 }
 // The example displays output like the following:
 //     The current culture is en-US

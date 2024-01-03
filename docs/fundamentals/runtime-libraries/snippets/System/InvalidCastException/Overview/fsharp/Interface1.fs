932bcca807e1dcca58c159256fbbacfdfab1b405
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2024-01-03 11:30:39 -0800

    Remarks for exception types and Registry class (#38930)

---
+++ docs/fundamentals/runtime-libraries/snippets/System/InvalidCastException/Overview/fsharp/Interface1.fs
@@ -0,0 +1,17 @@
+﻿module Interface1
+
+// <Snippet3>
+open System
+open System.Globalization
+
+let culture = CultureInfo.InvariantCulture
+let provider: IFormatProvider = culture
+
+let dt = provider :?> DateTimeFormatInfo
+
+// The example displays the following output:
+//    Unhandled Exception: System.InvalidCastException:
+//       Unable to cast object of type //System.Globalization.CultureInfo// to
+//           type //System.Globalization.DateTimeFormatInfo//.
+//       at Example.main()
+// </Snippet3>
---
+++ docs/fundamentals/runtime-libraries/snippets/System/InvalidOperationException/Overview/WPF1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFCrossThreadCS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static void Main() { }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // <Snippet1>
+        private async void threadExampleBtn_Click(object sender, RoutedEventArgs e)
+        {
+            textBox1.Text = String.Empty;
+
+            textBox1.Text = "Simulating work on UI thread.\n";
+            DoSomeWork(20);
+            textBox1.Text += "Work completed...\n";
+
+            textBox1.Text += "Simulating work on non-UI thread.\n";
+            await Task.Run(() => DoSomeWork(1000));
+            textBox1.Text += "Work completed...\n";
+        }
+
+        private async void DoSomeWork(int milliseconds)
+        {
+            // Simulate work.
+            await Task.Delay(milliseconds);
+
+            // Report completion.
+            var msg = String.Format("Some work completed in {0} ms.\n", milliseconds);
+            textBox1.Text += msg;
+        }
+        // </Snippet1>
+    }
+}

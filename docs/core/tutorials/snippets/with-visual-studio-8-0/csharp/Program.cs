8922f631e5709b7ae4da80579f49481e22e381bd
Tom Dykstra <tdykstra@microsoft.com>
2024-01-27 18:43:24 -0800

    draft

---
+++ docs/core/tutorials/snippets/with-visual-studio-8-0/csharp/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // <MainMethod>
+        Console.WriteLine("What is your name?");
+        var name = Console.ReadLine();
+        var currentDate = DateTime.Now;
+        Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+        Console.Write($"{Environment.NewLine}Press Enter to exit...");
+        Console.ReadLine();
+        // </MainMethod>
+    }
+}

93fc45cad5d9a7ab7db5bdfb94167f128873ffce
prime167 <3722845+prime167@users.noreply.github.com>
2024-03-05 10:04:01 +0800

    Update program.cs
    Fix #39784 : description and code example not match issue ,f

--- docs/fundamentals/runtime-libraries/snippets/System/Span/Overview/csharp/program.cs
+++ docs/fundamentals/runtime-libraries/snippets/System/Span/Overview/csharp/program.cs
@@ -1,134 +1,28 @@
-﻿using System;
-using System.Runtime.InteropServices;
-using static ProgramB;
-
-class Program
+class SpanSliceExample
 {
-    static void Main()
-    {
-        CreateSpanFromArray();
-        Console.WriteLine("-----");
-        CreateSpanFromNativeMemory();
-        Console.WriteLine("-----");
-        CreateSpanFromStack();
-        WorkWithSpans();
-        Console.WriteLine("-----");
-        ProgramB.WorkWithSpans();
-    }
+	static void Main(string[] args)
+	{
+		// Create a 10-element integer array
+		int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-    private static void CreateSpanFromArray()
-    {
-        // <Snippet1>
-        // Create a span over an array.
-        var array = new byte[100];
-        var arraySpan = new Span<byte>(array);
+		// Create a slice of the middle five elements
+		Span<int> middleSlice = new Span<int>(numbers, 2, 5);
 
-        byte data = 0;
-        for (int ctr = 0; ctr < arraySpan.Length; ctr++)
-            arraySpan[ctr] = data++;
+		// Double the values of each integer in the slice
+		for (int i = 0; i < middleSlice.Length; i++)
+		{
+			middleSlice[i] *= 2;
+		}
 
-        int arraySum = 0;
-        foreach (var value in array)
-            arraySum += value;
+		// Print the original array
+		Console.WriteLine("new array:");
+		foreach (int number in numbers)
+		{
+			Console.Write(number + " ");
+		}
 
-        Console.WriteLine($"The sum is {arraySum}");
-        // Output:  The sum is 4950
-        // </Snippet1>
-    }
-
-    private static void CreateSpanFromNativeMemory()
-    {
-        // <Snippet2>
-        // Create a span from native memory.
-        var native = Marshal.AllocHGlobal(100);
-        Span<byte> nativeSpan;
-        unsafe
-        {
-            nativeSpan = new Span<byte>(native.ToPointer(), 100);
-        }
-        byte data = 0;
-        for (int ctr = 0; ctr < nativeSpan.Length; ctr++)
-            nativeSpan[ctr] = data++;
-
-        int nativeSum = 0;
-        foreach (var value in nativeSpan)
-            nativeSum += value;
-
-        Console.WriteLine($"The sum is {nativeSum}");
-        Marshal.FreeHGlobal(native);
-        // Output:  The sum is 4950
-        // </Snippet2>
-    }
-
-    private static void CreateSpanFromStack()
-    {
-        // <Snippet3>
-        // Create a span on the stack.
-        byte data = 0;
-        Span<byte> stackSpan = stackalloc byte[100];
-        for (int ctr = 0; ctr < stackSpan.Length; ctr++)
-            stackSpan[ctr] = data++;
-
-        int stackSum = 0;
-        foreach (var value in stackSpan)
-            stackSum += value;
-
-        Console.WriteLine($"The sum is {stackSum}");
-        // Output:  The sum is 4950
-        // </Snippet3>
-    }
-}
-
-public class ProgramB
-{
-    // <Snippet4>
-    public static void WorkWithSpans()
-    {
-        // Create a span over an array.
-        var array = new byte[100];
-        var arraySpan = new Span<byte>(array);
-
-        InitializeSpan(arraySpan);
-        Console.WriteLine($"The sum is {ComputeSum(arraySpan):N0}");
-
-        // Create an array from native memory.
-        var native = Marshal.AllocHGlobal(100);
-        Span<byte> nativeSpan;
-        unsafe
-        {
-            nativeSpan = new Span<byte>(native.ToPointer(), 100);
-        }
-
-        InitializeSpan(nativeSpan);
-        Console.WriteLine($"The sum is {ComputeSum(nativeSpan):N0}");
-
-        Marshal.FreeHGlobal(native);
-
-        // Create a span on the stack.
-        Span<byte> stackSpan = stackalloc byte[100];
-
-        InitializeSpan(stackSpan);
-        Console.WriteLine($"The sum is {ComputeSum(stackSpan):N0}");
-    }
-
-    public static void InitializeSpan(Span<byte> span)
-    {
-        byte value = 0;
-        for (int ctr = 0; ctr < span.Length; ctr++)
-            span[ctr] = value++;
-    }
-
-    public static int ComputeSum(Span<byte> span)
-    {
-        int sum = 0;
-        foreach (var value in span)
-            sum += value;
-
-        return sum;
-    }
-    // The example displays the following output:
-    //    The sum is 4,950
-    //    The sum is 4,950
-    //    The sum is 4,950
-    // </Snippet4>
+		// Output: 
+		//new array:
+		// 1 2 6 8 10 12 14 8 9 10
+	}
 }

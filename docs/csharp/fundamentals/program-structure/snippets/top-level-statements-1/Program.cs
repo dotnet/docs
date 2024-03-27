326069edf56fbd520e86daed5845da0622a779bd
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2024-03-27 20:28:47 +0100

    addition to top-level statements

--- docs/csharp/fundamentals/program-structure/snippets/top-level-statements-1/Program.cs
+++ docs/csharp/fundamentals/program-structure/snippets/top-level-statements-1/Program.cs
@@ -1,7 +1,16 @@
 ﻿using System.Text;
 
 StringBuilder builder = new();
-builder.AppendLine("Hello");
-builder.AppendLine("World!");
+builder.AppendLine("Hello,");
+builder.AppendLine("The following arguments are passed:");
 
 Console.WriteLine(builder.ToString());
+
+// Display the command line arguments using the args variable.
+foreach (var arg in args)
+{
+    Console.WriteLine($"Argument={arg}");
+}
+
+// Return a success code.
+return 0;

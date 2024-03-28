e449f0e7de66fd53828d9897d74da6007c53a29e
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2024-03-28 13:48:55 +0100

    address review comment

--- docs/csharp/fundamentals/program-structure/snippets/top-level-statements-1/Program.cs
+++ docs/csharp/fundamentals/program-structure/snippets/top-level-statements-1/Program.cs
@@ -1,16 +1,15 @@
 ﻿using System.Text;
 
 StringBuilder builder = new();
-builder.AppendLine("Hello,");
 builder.AppendLine("The following arguments are passed:");
-
-Console.WriteLine(builder.ToString());
 
 // Display the command line arguments using the args variable.
 foreach (var arg in args)
 {
-    Console.WriteLine($"Argument={arg}");
+    builder.AppendLine($"Argument={arg}");
 }
+
+Console.WriteLine(builder.ToString());
 
 // Return a success code.
 return 0;

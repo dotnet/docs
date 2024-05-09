81318f6de2a2904f891ef9b921e3a843c37cbd1d
Aaron R Robinson <arobins@microsoft.com>
2024-05-09 15:21:21 -0700

    Feedback

--- samples/snippets/standard/interop/pinvoke/enumwindows.cs
+++ samples/snippets/standard/interop/pinvoke/enumwindows.cs
@@ -3,27 +3,25 @@ using System.Runtime.InteropServices;
 
 namespace ConsoleApplication1
 {
-    public static partial class Program
+    public static unsafe partial class Program
     {
-        // Define a delegate that corresponds to the unmanaged function.
-        private delegate bool EnumWC(IntPtr hwnd, IntPtr lParam);
-
         // Import user32.dll (containing the function we need) and define
         // the method corresponding to the native function.
         [LibraryImport("user32.dll")]
-        private static partial int EnumWindows(EnumWC lpEnumFunc, IntPtr lParam);
+        private static partial int EnumWindows(delegate* unmanaged<IntPtr, IntPtr, int> lpEnumFunc, IntPtr lParam);
 
-        // Define the implementation of the delegate; here, we simply output the window handle.
-        private static bool OutputWindow(IntPtr hwnd, IntPtr lParam)
+        // Define the implementation of the callback; here, we simply output the window handle.
+        [UnmanagedCallersOnly]
+        private static int OutputWindow(IntPtr hwnd, IntPtr lParam)
         {
             Console.WriteLine(hwnd.ToInt64());
-            return true;
+            return 1 /* true */;
         }
 
-        public static void Main(string[] args)
+        public static void Main2(string[] args)
         {
             // Invoke the method; note the delegate as a first parameter.
-            EnumWindows(OutputWindow, IntPtr.Zero);
+            EnumWindows(&OutputWindow, IntPtr.Zero);
         }
     }
 }

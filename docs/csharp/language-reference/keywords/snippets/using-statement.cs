a9b8b493c85f40daef18d58befb87d4fe7f7d9f7
Olabamiji Oyetubo <60369677+bigboybamo@users.noreply.github.com>
2024-05-20 22:59:04 +0100

    Update and rename using-statement.cs to UsingKeywordExample.cs

--- docs/csharp/language-reference/keywords/snippets/using-statement.cs
+++
@@ -1,8 +0,0 @@
-string filePath = "example.txt";
-string textToWrite = "Hello, this is a test message!";
-
-// Use the using statement to ensure the StreamWriter is properly disposed of
-using (StreamWriter writer = new StreamWriter(filePath))
-{
-    writer.WriteLine(textToWrite);
-}

54058ae1163506f42821b253d0606bd1e68ab43a
Andy (Steve) De George <67293991+adegeo@users.noreply.github.com>
2024-06-03 11:52:05 -0700

    Update VB move example to use my docs instead of c: (#41203)
    * Update VB move example to use my docs instead of c:
    
    * Fix vb mytype

---
+++ docs/visual-basic/developing-apps/programming/drives-directories-files/snippets/how-to-rename-a-file/Form1.vb
@@ -0,0 +1,26 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Sub RenameFileNameMy()
+        '<MyRename>
+        Dim myDocsFolder As String = My.Computer.FileSystem.SpecialDirectories.MyDocuments
+        Dim filePath = System.IO.Path.Combine(myDocsFolder, "TextFile.txt")
+
+        My.Computer.FileSystem.RenameFile(filePath, "NewName.txt")
+        '</MyRename>
+    End Sub
+
+    Sub RenameFileNameBCL()
+
+        '<BCLRename>
+        Dim myDocsFolder As String = My.Computer.FileSystem.SpecialDirectories.MyDocuments
+        Dim filePathSource = System.IO.Path.Combine(myDocsFolder, "TextFile.txt")
+        Dim filePathTarget = System.IO.Path.Combine(myDocsFolder, "NewName.txt")
+
+        System.IO.File.Move(filePathSource, filePathTarget)
+        '</BCLRename>
+
+    End Sub
+
+End Class

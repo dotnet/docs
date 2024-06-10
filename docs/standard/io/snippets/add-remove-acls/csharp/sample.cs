5ab9ed789bee13779e979edc82447fb87fcb6bdf
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2024-06-10 04:50:30 -0700

    update example to be .net friendly (#41343)

--- docs/standard/io/snippets/add-remove-acls/csharp/sample.cs
+++ docs/standard/io/snippets/add-remove-acls/csharp/sample.cs
@@ -1,10 +1,8 @@
-﻿//<SNIPPET1>
+﻿//<Snippet1>
 using System;
 using System.IO;
 using System.Security.AccessControl;
 
-namespace FileSystemExample
-{
     class FileExample
     {
         public static void Main()
@@ -13,15 +11,13 @@ namespace FileSystemExample
             {
                 string fileName = "test.xml";
 
-                Console.WriteLine("Adding access control entry for "
-                    + fileName);
+                Console.WriteLine($"Adding access control entry for {fileName}");
 
                 // Add the access control entry to the file.
                 AddFileSecurity(fileName, @"DomainName\AccountName",
                     FileSystemRights.ReadData, AccessControlType.Allow);
 
-                Console.WriteLine("Removing access control entry from "
-                    + fileName);
+                Console.WriteLine($"Removing access control entry from {fileName}");
 
                 // Remove the access control entry from the file.
                 RemoveFileSecurity(fileName, @"DomainName\AccountName",
@@ -39,35 +35,30 @@ namespace FileSystemExample
         public static void AddFileSecurity(string fileName, string account,
             FileSystemRights rights, AccessControlType controlType)
         {
-
-            // Get a FileSecurity object that represents the
-            // current security settings.
-            FileSecurity fSecurity = File.GetAccessControl(fileName);
+            FileInfo fileInfo = new(fileName);
+            FileSecurity fSecurity = fileInfo.GetAccessControl();
 
             // Add the FileSystemAccessRule to the security settings.
             fSecurity.AddAccessRule(new FileSystemAccessRule(account,
                 rights, controlType));
 
             // Set the new access settings.
-            File.SetAccessControl(fileName, fSecurity);
+            fileInfo.SetAccessControl(fSecurity);
         }
 
         // Removes an ACL entry on the specified file for the specified account.
         public static void RemoveFileSecurity(string fileName, string account,
             FileSystemRights rights, AccessControlType controlType)
         {
-
-            // Get a FileSecurity object that represents the
-            // current security settings.
-            FileSecurity fSecurity = File.GetAccessControl(fileName);
+            FileInfo fileInfo = new(fileName);
+            FileSecurity fSecurity = fileInfo.GetAccessControl();
 
             // Remove the FileSystemAccessRule from the security settings.
             fSecurity.RemoveAccessRule(new FileSystemAccessRule(account,
                 rights, controlType));
 
             // Set the new access settings.
-            File.SetAccessControl(fileName, fSecurity);
+            fileInfo.SetAccessControl(fSecurity);
         }
-    }
 }
-//</SNIPPET1>
+//</Snippet1>

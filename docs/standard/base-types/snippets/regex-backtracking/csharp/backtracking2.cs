fcd17cc0dca63f34b69a6039d9dfc5c1573279b0
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2024-06-11 12:53:35 -0700

    Update C# output

--- docs/standard/base-types/snippets/regex-backtracking/csharp/backtracking2.cs
+++ docs/standard/base-types/snippets/regex-backtracking/csharp/backtracking2.cs
@@ -11,13 +11,11 @@ public class Example2
         Match m = Regex.Match(input, pattern, RegexOptions.IgnoreCase);
         if (m.Success)
         {
-            Console.WriteLine("'{0}' found at position {1}",
-                              m.Value, m.Index);
-            Console.WriteLine("'es' found at position {0}",
-                              m.Groups[1].Index);
+            Console.WriteLine($"'{m.Value}' found at position {m.Index}");
+            Console.WriteLine($"'es' found at position {m.Groups[1].Index}");
         }
     }
 }
-//    'Essential services are provided by regular expressions found at position 0
+//    'Essential services are provided by regular expres' found at position 0
 //    'es' found at position 47
 // </Snippet2>

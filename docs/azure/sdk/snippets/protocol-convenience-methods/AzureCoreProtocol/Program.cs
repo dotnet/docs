e0a5957bec588aace81c8547c3e2d445aa29a0db
alexwolfmsft <93200798+alexwolfmsft@users.noreply.github.com>
2024-07-23 13:53:56 -0400

    Protocol methods doc (#41744)
    New Protocol and Convenience methods doc
    
    ---------
    
    Co-authored-by: Scott Addie <10702007+scottaddie@users.noreply.github.com>
    Co-authored-by: Anne Thompson <annelo@microsoft.com>

---
+++ docs/azure/sdk/snippets/protocol-convenience-methods/AzureCoreProtocol/Program.cs
@@ -0,0 +1,33 @@
+﻿using Azure;
+using Azure.AI.ContentSafety;
+using Azure.Core;
+using Azure.Identity;
+
+// Create the client
+ContentSafetyClient safetyClient = new(
+    new Uri("https://contentsafetyai.cognitiveservices.azure.com/"),
+    new DefaultAzureCredential());
+
+// Create the prompt
+RequestContent prompt = RequestContent.Create(new
+{
+    text = "What is Microsoft Azure?",
+});
+
+// Call the protocol method
+Response response = safetyClient.AnalyzeText(
+    prompt, 
+    new RequestContext()
+    {
+        ErrorOptions = ErrorOptions.NoThrow
+    });
+
+// Any non-200 response code from Azure AI Content Safety AnalyzeText REST API isn't considered a success response.
+// See REST API details at https://azure-ai-content-safety-api-docs.developer.azure-api.net/api-details#api=content-safety-service-2023-10-01&operation=TextOperations_AnalyzeText
+if (response.Status != 200) 
+{
+    throw new RequestFailedException(response); 
+}
+
+// Display the results
+Console.WriteLine(response.Content);
---
+++ docs/azure/sdk/snippets/protocol-convenience-methods/SCMProtocol/Program.cs
@@ -0,0 +1,51 @@
+﻿using OpenAI;
+using OpenAI.Chat;
+using System.ClientModel;
+using System.ClientModel.Primitives;
+using System.Text.Json;
+
+// Create the client
+OpenAIClient client = new("<your-openai-api-key>");
+ChatClient chatClient = client.GetChatClient("gpt-4");
+
+// Create the request content
+BinaryData input = BinaryData.FromBytes("""
+    {  
+        "model": "gpt-4o",
+        "messages": [
+           {
+               "role": "user",
+               "content": "What is Microsoft Azure?."
+           }
+        ]
+    }
+    """u8.ToArray());
+using BinaryContent content = BinaryContent.Create(input);
+
+// Call the protocol method
+ClientResult result = chatClient.CompleteChat(
+        content,
+        new RequestOptions()
+        {
+            ErrorOptions = ClientErrorBehaviors.NoThrow
+        });
+
+PipelineResponse response = result.GetRawResponse();
+
+// Any non-200 response code from CompleteChat endpoint isn't considered a success response.
+if (response.Status != 200)
+{
+    throw new ClientResultException(response);
+}
+
+// Display the results
+BinaryData output = result.GetRawResponse().Content;
+
+using JsonDocument outputAsJson = JsonDocument.Parse(output);
+string message = outputAsJson.RootElement
+    .GetProperty("choices"u8)[0]
+    .GetProperty("message"u8)
+    .GetProperty("content"u8)
+    .GetString();
+
+Console.WriteLine(message);

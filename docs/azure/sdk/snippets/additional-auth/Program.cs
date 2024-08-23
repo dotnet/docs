ae7d83955f744011df410c90adf7e46a35e746a3
Alex Wolf <alexwolf@microsoft.com>
2024-08-23 14:25:35 -0400

    fix build

--- docs/azure/sdk/snippets/additional-auth/Program.cs
+++ docs/azure/sdk/snippets/additional-auth/Program.cs
@@ -1,5 +1,4 @@
-using Azure.Identity;
-using Azure.Identity.Broker;
+using InteractiveBrokeredAuthSample;
 
 namespace WinFormsApp1
 {
@@ -14,7 +13,7 @@ namespace WinFormsApp1
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new InteractiveBrowserAuth());
 
             
         }

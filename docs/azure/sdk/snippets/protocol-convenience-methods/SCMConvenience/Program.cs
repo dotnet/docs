81ef6ac8facd9c4529e072c3ecd4354a749da41b
Scott Addie <10702007+scottaddie@users.noreply.github.com>
2024-08-28 10:18:22 -0500

    Add dynamic data sample with protocol methods (#42351)
    * Add dynamic data sample with protocol methods
    
    * Update SCM sample apps
    
    * Code formatting
    
    * Rename client variable
    
    * Fix xref link
    
    * Fix line highlighting
    
    * Add clarity

--- docs/azure/sdk/snippets/protocol-convenience-methods/SCMConvenience/Program.cs
+++ docs/azure/sdk/snippets/protocol-convenience-methods/SCMConvenience/Program.cs
@@ -1,14 +1,12 @@
-﻿using OpenAI;
-using OpenAI.Chat;
-using System.ClientModel;
+﻿using OpenAI.Chat;
 
 // Create the client
-OpenAIClient client = new("<your-openai-api-key>");
-ChatClient chatClient = client.GetChatClient("gpt-4");
+ChatClient client = new(
+    model: "gpt-4o-mini",
+    credential: Environment.GetEnvironmentVariable("OPENAI_API_KEY")!);
 
 // Call the convenience method
-ChatCompletion completion
-    = chatClient.CompleteChat("What is Microsoft Azure?");
+ChatCompletion completion = client.CompleteChat("What is Microsoft Azure?");
 
 // Display the results
-Console.WriteLine($"{completion.Role}: {completion.Content}");
+Console.WriteLine($"[{completion.Role}]: {completion}");

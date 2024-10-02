f0905f5895c3b96d3db7e25937fec9906a062edf
Bill Wagner <wiwagn@microsoft.com>
2024-10-02 12:48:48 -0400

    Clarify extension method declaration
    Extension methods can only be declared in top-level type. They can't be declared in nested types.

--- samples/snippets/csharp/VS_Snippets_VBCSharp/csProgGuideExtensionMethods/cs/extensionmethods.cs
+++ samples/snippets/csharp/VS_Snippets_VBCSharp/csProgGuideExtensionMethods/cs/extensionmethods.cs
@@ -1,38 +1,4 @@
-﻿//<Snippet1>
-namespace CustomExtensions
-{
-    // Extension methods must be defined in a static class.
-    public static class StringExtension
-    {
-        // This is the extension method.
-        // The first parameter takes the "this" modifier
-        // and specifies the type for which the method is defined.
-        public static int WordCount(this string str)
-        {
-            return str.Split(new char[] {' ', '.','?'}, StringSplitOptions.RemoveEmptyEntries).Length;
-        }
-    }
-}
-namespace Extension_Methods_Simple
-{
-    // Import the extension method namespace.
-    using CustomExtensions;
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string s = "The quick brown fox jumped over the lazy dog.";
-            // Call the method as if it were an
-            // instance method on the type. Note that the first
-            // parameter is not specified by the calling code.
-            int i = s.WordCount();
-            System.Console.WriteLine("Word count of s is {0}", i);
-        }
-    }
-}
-//</Snippet1>
-
-namespace Extension2
+﻿namespace Extension2
 {
 
     //<snippet2>

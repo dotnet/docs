3873697ecbc6937d0571f235d1c3a5a095283f1a
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2024-12-18 13:03:56 -0800

    respond to feedback

--- docs/machine-learning/snippets/mldotnet-api/csharp/Program.cs
+++ docs/machine-learning/snippets/mldotnet-api/csharp/Program.cs
@@ -1,15 +1,15 @@
-using Microsoft.ML;
+﻿using Microsoft.ML;
 using Microsoft.ML.Data;
 
 class Program
 {
-    public class HouseData
+    public record HouseData
     {
         public float Size { get; set; }
         public float Price { get; set; }
     }
 
-    public class Prediction
+    public record Prediction
     {
         [ColumnName("Score")]
         public float Price { get; set; }
@@ -21,23 +21,23 @@ class Program
 
         // 1. Import or create training data.
         HouseData[] houseData = [
-                new HouseData() { Size = 1.1F, Price = 1.2F },
-                new HouseData() { Size = 1.9F, Price = 2.3F },
-                new HouseData() { Size = 2.8F, Price = 3.0F },
-                new HouseData() { Size = 3.4F, Price = 3.7F }
+                new() { Size = 1.1F, Price = 1.2F },
+                new() { Size = 1.9F, Price = 2.3F },
+                new() { Size = 2.8F, Price = 3.0F },
+                new() { Size = 3.4F, Price = 3.7F }
                 ];
         IDataView trainingData = mlContext.Data.LoadFromEnumerable(houseData);
 
         // 2. Specify data preparation and model training pipeline.
-        var pipeline = mlContext.Transforms.Concatenate("Features", ["Size"])
+        EstimatorChain<RegressionPredictionTransformer<Microsoft.ML.Trainers.LinearRegressionModelParameters>> pipeline = mlContext.Transforms.Concatenate("Features", ["Size"])
             .Append(mlContext.Regression.Trainers.Sdca(labelColumnName: "Price", maximumNumberOfIterations: 100));
 
         // 3. Train model.
-        var model = pipeline.Fit(trainingData);
+        TransformerChain<RegressionPredictionTransformer<Microsoft.ML.Trainers.LinearRegressionModelParameters>> model = pipeline.Fit(trainingData);
 
         // 4. Make a prediction.
-        var size = new HouseData() { Size = 2.5F };
-        var price = mlContext.Model.CreatePredictionEngine<HouseData, Prediction>(model).Predict(size);
+        HouseData size = new() { Size = 2.5F };
+        Prediction price = mlContext.Model.CreatePredictionEngine<HouseData, Prediction>(model).Predict(size);
 
         Console.WriteLine($"Predicted price for size: {size.Size * 1000} sq ft = {price.Price * 100:C}k");
 

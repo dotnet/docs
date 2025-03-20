3ade4987f88fb6411f5c5f6ff9a6b613c872051a
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2025-03-20 11:49:47 -0700

    add parentheses around conditional expression

--- docs/fundamentals/runtime-libraries/snippets/System/Boolean/Overview/csharp/tostring2.cs
+++ docs/fundamentals/runtime-libraries/snippets/System/Boolean/Overview/csharp/tostring2.cs
@@ -8,8 +8,8 @@ public class Example11
         bool raining = false;
         bool busLate = true;
 
-        Console.WriteLine($"It is raining: {raining ? "Yes" : "No"}");
-        Console.WriteLine($"The bus is late: {busLate ? "Yes" : "No"}");
+        Console.WriteLine($"It is raining: {(raining ? "Yes" : "No")}");
+        Console.WriteLine($"The bus is late: {(busLate ? "Yes" : "No")}");
     }
 }
 // The example displays the following output:

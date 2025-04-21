fdf67dc2f01875ed9eaa304509b78ca6a6212f2a
Kohei Hakoishi <kohei.hakoishi@hey.com>
2025-04-22 07:21:15 +0900

    fix code snippet in 'Collection lookups with spans (What's new in .NET libraries for .NET 9)' documentation (#43702)

--- docs/core/whats-new/snippets/dotnet-9/csharp/Collections.cs
+++ docs/core/whats-new/snippets/dotnet-9/csharp/Collections.cs
@@ -58,14 +58,18 @@ internal partial class ReadOnlyCollections
     // </ReadOnlySet>
 
     // <AlternateLookup>
-    private static Dictionary<string, int> CountWords(ReadOnlySpan<char> input)
+    static Dictionary<string, int> CountWords(ReadOnlySpan<char> input)
     {
         Dictionary<string, int> wordCounts = new(StringComparer.OrdinalIgnoreCase);
         Dictionary<string, int>.AlternateLookup<ReadOnlySpan<char>> spanLookup =
             wordCounts.GetAlternateLookup<ReadOnlySpan<char>>();
 
-        foreach (Range wordRange in Regex.EnumerateSplits(input, @"\b\w+\b"))
+        foreach (Range wordRange in Regex.EnumerateSplits(input, @"\b\W+"))
         {
+            if (wordRange.Start.Value == wordRange.End.Value)
+            {
+                continue; // Skip empty ranges.
+            }
             ReadOnlySpan<char> word = input[wordRange];
             spanLookup[word] = spanLookup.TryGetValue(word, out int count) ? count + 1 : 1;
         }

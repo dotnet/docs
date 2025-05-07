36bc15b05cef4e0b8b50a23419fa94ad00bba76f
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2025-05-06 21:29:01 -0700

    uncomment lines

--- docs/standard/serialization/system-text-json/snippets/how-to-contd/csharp/Program.cs
+++ docs/standard/serialization/system-text-json/snippets/how-to-contd/csharp/Program.cs
@@ -4,11 +4,11 @@
     {
         static async Task Main(string[] args)
         {
-            //Console.WriteLine("======== Custom converter Unix Epoch DateTimeOffset =========");
-            //DateTimeOffset date = DateTimeOffset.Now;
-            //CustomConverterUnixEpochDate.Program.Main(date);
-            //CustomConverterUnixEpochDate.Program.Main2(date);
-            //Console.WriteLine();
+            Console.WriteLine("======== Custom converter Unix Epoch DateTimeOffset =========");
+            DateTimeOffset date = DateTimeOffset.Now;
+            CustomConverterUnixEpochDate.Program.Main(date);
+            CustomConverterUnixEpochDate.Program.Main2(date);
+            Console.WriteLine();
 
             Console.WriteLine("======== Custom converter Unix Epoch DateTime =========");
             DateTime date2 = DateTime.Now;
@@ -16,81 +16,81 @@
             CustomConverterUnixEpochDateNoZone.Program.Main2(date2);
             Console.WriteLine();
 
-            //Console.WriteLine("======== Preserve references =========");
-            //PreserveReferences.Program.Main();
-            //Console.WriteLine();
+            Console.WriteLine("======== Preserve references =========");
+            PreserveReferences.Program.Main();
+            Console.WriteLine();
 
-            //Console.WriteLine("======== Custom converter Preserve references =========");
-            //CustomConverterPreserveReferences.Program.Main();
-            //Console.WriteLine();
+            Console.WriteLine("======== Custom converter Preserve references =========");
+            CustomConverterPreserveReferences.Program.Main();
+            Console.WriteLine();
 
-            //Console.WriteLine("======== Preserve references Multiple calls =========");
-            //PreserveReferencesMultipleCalls.Program.Main();
-            //Console.WriteLine();
+            Console.WriteLine("======== Preserve references Multiple calls =========");
+            PreserveReferencesMultipleCalls.Program.Main();
+            Console.WriteLine();
 
-            //Console.WriteLine("======== Immutable types =========");
-            //ImmutableTypes.Program.Main();
-            //Console.WriteLine();
+            Console.WriteLine("======== Immutable types =========");
+            ImmutableTypes.Program.Main();
+            Console.WriteLine();
 
-            //Console.WriteLine("======== Immutable types =========");
-            //ImmutableTypesCtorParms.Program.Main();
-            //Console.WriteLine();
+            Console.WriteLine("======== Immutable types =========");
+            ImmutableTypesCtorParms.Program.Main();
+            Console.WriteLine();
 
-            //Console.WriteLine("======== Record support =========");
-            //Records.Program.Main();
-            //Console.WriteLine();
+            Console.WriteLine("======== Record support =========");
+            Records.Program.Main();
+            Console.WriteLine();
 
-            //Console.WriteLine("======== Field support =========");
-            //Fields.Program.Main();
-            //Console.WriteLine();
+            Console.WriteLine("======== Field support =========");
+            Fields.Program.Main();
+            Console.WriteLine();
 
-            //Console.WriteLine("======== Non-string key dictionary =========");
-            //NonStringKeyDictionary.Program.Main();
-            //Console.WriteLine();
+            Console.WriteLine("======== Non-string key dictionary =========");
+            NonStringKeyDictionary.Program.Main();
+            Console.WriteLine();
 
-            //Console.WriteLine("======== HttpClient extension methods =========");
-            //await HttpClientExtensionMethods.Program.Main();
-            //Console.WriteLine();
+            Console.WriteLine("======== HttpClient extension methods =========");
+            await HttpClientExtensionMethods.Program.Main();
+            Console.WriteLine();
 
-            //Console.WriteLine("======== Custom converter handle null =========");
-            //CustomConverterHandleNull.Program.Main();
-            //Console.WriteLine();
+            Console.WriteLine("======== Custom converter handle null =========");
+            CustomConverterHandleNull.Program.Main();
+            Console.WriteLine();
 
-            //Console.WriteLine("======== Custom converter inferred types to object =========");
-            //CustomConverterInferredTypesToObject.Program.Main();
-            //Console.WriteLine();
+            Console.WriteLine("======== Custom converter inferred types to object =========");
+            CustomConverterInferredTypesToObject.Program.Main();
+            Console.WriteLine();
 
-            //Console.WriteLine("======== Ignore value type default on serialize =========");
-            //IgnoreValueDefaultOnSerialize.Program.Main();
-            //Console.WriteLine();
+            Console.WriteLine("======== Ignore value type default on serialize =========");
+            IgnoreValueDefaultOnSerialize.Program.Main();
+            Console.WriteLine();
 
-            //Console.WriteLine("======== Ignore null on serialize =========");
-            //IgnoreNullOnSerialize.Program.Main();
-            //Console.WriteLine();
+            Console.WriteLine("======== Ignore null on serialize =========");
+            IgnoreNullOnSerialize.Program.Main();
+            Console.WriteLine();
 
-            //Console.WriteLine("======== Conditionally ignore selected properties on serialize =========");
-            //JsonIgnoreAttributeExample.Program.Main();
-            //Console.WriteLine();
+            Console.WriteLine("======== Conditionally ignore selected properties on serialize =========");
+            JsonIgnoreAttributeExample.Program.Main();
+            Console.WriteLine();
 
-            //Console.WriteLine("======== Non-public accessors =========");
-            //NonPublicAccessors.Program.Main();
-            //Console.WriteLine();
+            Console.WriteLine("======== Non-public accessors =========");
+            NonPublicAccessors.Program.Main();
+            Console.WriteLine();
 
-            //Console.WriteLine("======== Copy options instance =========");
-            //CopyOptions.Program.Main();
-            //Console.WriteLine();
+            Console.WriteLine("======== Copy options instance =========");
+            CopyOptions.Program.Main();
+            Console.WriteLine();
 
-            //Console.WriteLine("======== Create options instance with specified defaults =========");
-            //OptionsDefaults.Program.Main();
-            //Console.WriteLine();
+            Console.WriteLine("======== Create options instance with specified defaults =========");
+            OptionsDefaults.Program.Main();
+            Console.WriteLine();
 
-            //Console.WriteLine("======== Quoted numbers =========");
-            //QuotedNumbers.Program.Main();
-            //Console.WriteLine();
+            Console.WriteLine("======== Quoted numbers =========");
+            QuotedNumbers.Program.Main();
+            Console.WriteLine();
 
-            //Console.WriteLine("======== GuidReferenceResolver =========");
-            //GuidReferenceResolverExample.Program.Main();
-            //Console.WriteLine();
+            Console.WriteLine("======== GuidReferenceResolver =========");
+            GuidReferenceResolverExample.Program.Main();
+            Console.WriteLine();
         }
     }
 }

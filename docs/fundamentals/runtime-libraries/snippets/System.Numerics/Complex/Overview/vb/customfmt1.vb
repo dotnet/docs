c07a47f57bd3282b9c88b73715a3d39441e838c8
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2025-05-16 09:48:58 -0700

    break long lines

--- docs/fundamentals/runtime-libraries/snippets/System.Numerics/Complex/Overview/vb/customfmt1.vb
+++ docs/fundamentals/runtime-libraries/snippets/System.Numerics/Complex/Overview/vb/customfmt1.vb
@@ -58,14 +58,16 @@ Module Example2
         Dim c1 As New Complex(12.1, 15.4)
         Console.WriteLine($"Formatting with ToString():       {c1}")
         Console.WriteLine($"Formatting with ToString(format): {c1:N2}")
-        Console.WriteLine($"Custom formatting with I0:        {String.Format(New ComplexFormatter(), "{0:I0}", c1)}")
-        Console.WriteLine($"Custom formatting with J3:        {String.Format(New ComplexFormatter(), "{0:J3}", c1)}")
+        Console.WriteLine($"Custom formatting with I0:        " +
+                          $"{String.Format(New ComplexFormatter(), "{0:I0}", c1)}")
+        Console.WriteLine($"Custom formatting with J3:        " +
+                          $"{String.Format(New ComplexFormatter(), "{0:J3}", c1)}")
     End Sub
 End Module
 
 ' The example displays the following output:
-'    Formatting with ToString():       (12.1, 15.4)
-'    Formatting with ToString(format): (12.10, 15.40)
+'    Formatting with ToString():       <12.1; 15.4>
+'    Formatting with ToString(format): <12.10; 15.40>
 '    Custom formatting with I0:        12 + 15i
 '    Custom formatting with J3:        12.100 + 15.400j
 ' </Snippet4>

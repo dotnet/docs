b7d09e795fe924e51099ff25d6beac694e5bc80d
alexwolfmsft <93200798+alexwolfmsft@users.noreply.github.com>
2025-05-29 12:20:39 -0400

    New mcp client quickstart (#46450)
    * New mcp client quickstart
    
    ---------
    
    Co-authored-by: Genevieve Warren <24882762+gewarren@users.noreply.github.com>

---
+++ docs/ai/quickstarts/snippets/mcp-client/Program.cs
@@ -0,0 +1,52 @@
+﻿using Azure.AI.OpenAI;
+using Azure.Identity;
+using Microsoft.Extensions.AI;
+using ModelContextProtocol.Client;
+using ModelContextProtocol.Protocol.Transport;
+
+// Create an IChatClient using Azure OpenAI.
+IChatClient client =
+    new ChatClientBuilder(
+        new AzureOpenAIClient(new Uri("<your-azure-openai-endpoint>"),
+        new DefaultAzureCredential())
+        .GetChatClient("gpt-4o").AsIChatClient())
+    .UseFunctionInvocation()
+    .Build();
+
+// Create the MCP client
+// Configure it to start and connect to your MCP server.
+var mcpClient = await McpClientFactory.CreateAsync(
+    new StdioClientTransport(new()
+    {
+        Command = "dotnet run",
+        Arguments = ["--project", "<path-to-your-mcp-server-project>"],
+        Name = "Minimal MCP Server",
+    }));
+
+// List all available tools from the MCP server.
+Console.WriteLine("Available tools:");
+var tools = await mcpClient.ListToolsAsync();
+foreach (var tool in tools)
+{
+    Console.WriteLine($"{tool}");
+}
+Console.WriteLine();
+
+// Conversational loop that can utilize the tools via prompts.
+List<ChatMessage> messages = [];
+while (true)
+{
+    Console.Write("Prompt: ");
+    messages.Add(new(ChatRole.User, Console.ReadLine()));
+
+    List<ChatResponseUpdate> updates = [];
+    await foreach (var update in client
+        .GetStreamingResponseAsync(messages, new() { Tools = [.. tools] }))
+    {
+        Console.Write(update);
+        updates.Add(update);
+    }
+    Console.WriteLine();
+
+    messages.AddMessages(updates);
+}

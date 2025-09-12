0ba12689b5b5e3ca09dfa615d1231007ac3f6096
Bill Wagner <wiwagn@microsoft.com>
2025-09-12 17:16:47 -0400

    Outline and sample

---
+++ docs/csharp/whats-new/tutorials/snippets/CompoundAssignment/GateAttendance.cs
@@ -0,0 +1,24 @@
+﻿public record class GateAttendance(string GateId)
+{
+    private int count = 0;
+
+    public int Count => count;
+
+    public static GateAttendance operator ++(GateAttendance gate)
+    {
+        GateAttendance updateGate = gate with { count = gate.count + 1 };
+        return updateGate;
+    }
+
+    public static GateAttendance operator +(GateAttendance gate, int partySize)
+    {
+        GateAttendance updateGate = gate with { count = gate.count + partySize };
+        return updateGate;
+    }
+
+    // New style:
+    public void operator ++() => this.count++;
+
+    public void operator +=(int partySize) => this.count += partySize;
+}
+

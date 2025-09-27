2e717d62ff2e35cef28732e251c6e5ffd182d2f7
kurnakovv <maksk210@gmail.com>
2025-09-27 15:41:17 +0900

    Add code example for CA1002 rule (#48762)

---
+++ docs/fundamentals/code-analysis/quality-rules/snippets/csharp/all-rules/ca1002.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+
+namespace ca1001
+{
+    //<snippet1>
+    // This class violates the rule.
+    public class MutableItems
+    {
+        // CA1002: Change 'List<string>' in 'ViolationItems.Members' to use 'Collection<T>', 'ReadOnlyCollection<T>' or 'KeyedCollection<K,V>'
+        public List<string> Items { get; } = new List<string>();
+
+        public void Add(string item)
+        {
+            Items.Add(item);
+        }
+    }
+
+    // This class satisfies the rule.
+    public class ReadOnlyItems
+    {
+        private readonly List<string> _items = new List<string>();
+
+        public IReadOnlyCollection<string> Items => _items.AsReadOnly();
+
+        public void Add(string item)
+        {
+            _items.Add(item);
+        }
+    }
+    //</snippet1>
+}

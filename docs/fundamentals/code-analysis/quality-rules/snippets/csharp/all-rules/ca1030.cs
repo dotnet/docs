c8a11569d1a29ca923d6b465bce2e23991285d1e
kurnakovv <maksk210@gmail.com>
2025-10-06 20:11:01 +0900

    Add code example for CA1030 rule (#48914)

---
+++ docs/fundamentals/code-analysis/quality-rules/snippets/csharp/all-rules/ca1030.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+
+namespace ca1030
+{
+    //<snippet1>
+    // This class violates the rule.
+    public class BadButton
+    {
+        private readonly List<Action> _clickHandlers = new List<Action>();
+
+        public void AddOnClick(Action handler)
+        {
+            // Some internal logic...
+
+            _clickHandlers.Add(handler);
+        }
+
+        public void RemoveOnClick(Action handler)
+        {
+            // Some internal logic...
+
+            _clickHandlers.Remove(handler);
+        }
+
+        public void FireClick()
+        {
+            foreach (Action handler in _clickHandlers)
+            {
+                handler();
+            }
+        }
+    }
+
+    // This class satisfies the rule.
+    public class GoodButton
+    {
+        private EventHandler? _clickHandler;
+
+        public event EventHandler? ClickHandler
+        {
+            add
+            {
+                // Some internal logic...
+
+                _clickHandler += value;
+            }
+            remove
+            {
+                // Some internal logic...
+
+                _clickHandler -= value;
+            }
+        }
+
+        protected virtual void OnClick(EventArgs e)
+        {
+            _clickHandler?.Invoke(this, e);
+        }
+
+        public void Click()
+        {
+            OnClick(EventArgs.Empty);
+        }
+    }
+    //</snippet1>
+}

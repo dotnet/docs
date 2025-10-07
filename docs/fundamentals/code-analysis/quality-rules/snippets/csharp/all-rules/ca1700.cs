f692c2bb3b267e790793b889e5ec61b890334a5e
kurnakovv <maksk210@gmail.com>
2025-10-07 14:41:03 +0900

    Add code example for CA1700 rule (#48932)

---
+++ docs/fundamentals/code-analysis/quality-rules/snippets/csharp/all-rules/ca1700.cs
@@ -0,0 +1,22 @@
+namespace ca1700
+{
+    //<snippet1>
+    // This class violates the rule.
+    public enum BadPaymentStatus
+    {
+        Pending = 0,
+        Completed = 1,
+        ReservedError = 2,
+        Reserved = 3,
+    }
+
+    // This class satisfies the rule.
+    public enum GoodPaymentStatus
+    {
+        Pending = 0,
+        Completed = 1,
+        Error = 2,
+        Unknown = 3,
+    }
+    //</snippet1>
+}

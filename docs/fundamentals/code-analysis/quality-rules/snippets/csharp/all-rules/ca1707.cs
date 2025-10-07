64d0f31f393ac5b8f230e536559bc2ae7f7e1f90
kurnakovv <maksk210@gmail.com>
2025-10-07 16:15:26 +0900

    Add code example for CA1707 rule (#48934)

---
+++ docs/fundamentals/code-analysis/quality-rules/snippets/csharp/all-rules/ca1707.cs
@@ -0,0 +1,35 @@
+using System;
+
+// <Snippet1>
+namespace ca_1707
+{
+    public interface IUser_Service
+    {
+        void Add_User(User_Model user_Model);
+    }
+
+    public class User_Service : IUser_Service
+    {
+        public const string Admin_Name = "admin";
+        public event EventHandler? User_Added;
+
+        public void Add_User(User_Model user_Model)
+        {
+            // ...
+        }
+
+    }
+
+    public struct User_Model
+    {
+        public int User_Id { get; set; }
+    }
+
+    public enum User_Type
+    {
+        Client_User = 0,
+        Manager_Admin = 1,
+        Syper_Admin = 3,
+    }
+}
+// </Snippet1>

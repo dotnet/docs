3cef53d971d4a0759fc6001b008943b725a01784
kurnakovv <maksk210@gmail.com>
2025-10-08 14:30:03 +0900

    Add code example for CA1720 rule (#48958)

---
+++ docs/fundamentals/code-analysis/quality-rules/snippets/csharp/all-rules/ca1720.cs
@@ -0,0 +1,13 @@
+namespace ca1715
+{
+    //<snippet1>
+    // This code violates the rule.
+    public class Short
+    {
+        public int Int32 { get; set; }
+        public Guid Guid { get; set; }
+
+        public void Float(int int32) { }
+    }
+    //</snippet1>
+}

1373c56329da9ef12f056b5d36d277d8b1e5a08a
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2025-11-06 09:22:23 -0800

    Apply suggestions from code review

--- docs/fundamentals/code-analysis/quality-rules/snippets/csharp/all-rules/ca2026.cs
+++ docs/fundamentals/code-analysis/quality-rules/snippets/csharp/all-rules/ca2026.cs
@@ -8,22 +8,15 @@ namespace ca2026
         public static void ProcessJsonViolation(string json)
         {
             JsonElement element = JsonDocument.Parse(json).RootElement;
-            // Process element
+            //...
         }
         //</Violation>
 
         //<Fixed>
         public static void ProcessJsonFixed(string json)
         {
-            // JsonElement.Parse is available in .NET 10+
-#if NET10_0_OR_GREATER
             JsonElement element = JsonElement.Parse(json);
-#else
-            // This code demonstrates the preferred API for .NET 10+
-            // For compilation on earlier versions, we use the old pattern
-            JsonElement element = JsonDocument.Parse(json).RootElement;
-#endif
-            // Process element
+            //...
         }
         //</Fixed>
     }
--- docs/fundamentals/code-analysis/quality-rules/snippets/vb/all-rules/ca2026.vb
+++ docs/fundamentals/code-analysis/quality-rules/snippets/vb/all-rules/ca2026.vb
@@ -7,21 +7,14 @@ Namespace ca2026
         '<Violation>
         Public Sub ProcessJsonViolation(json As String)
             Dim element As JsonElement = JsonDocument.Parse(json).RootElement
-            ' Process element
+            '...
         End Sub
         '</Violation>
 
         '<Fixed>
         Public Sub ProcessJsonFixed(json As String)
-            ' JsonElement.Parse is available in .NET 10+
-#If NET10_0_OR_GREATER Then
             Dim element As JsonElement = JsonElement.Parse(json)
-#Else
-            ' This code demonstrates the preferred API for .NET 10+
-            ' For compilation on earlier versions, we use the old pattern
-            Dim element As JsonElement = JsonDocument.Parse(json).RootElement
-#End If
-            ' Process element
+            '...
         End Sub
         '</Fixed>
 

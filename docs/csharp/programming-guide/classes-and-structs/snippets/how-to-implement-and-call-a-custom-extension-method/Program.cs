b8fcd03695d2c87493a353fd04c74271d0a57b60
Bill Wagner <wiwagn@microsoft.com>
2025-11-25 17:04:08 -0500

    Finish programming guide.

---
+++ docs/csharp/programming-guide/classes-and-structs/snippets/how-to-implement-and-call-a-custom-extension-method/Program.cs
@@ -0,0 +1,27 @@
+﻿using CustomExtensions;
+
+string s = "The quick brown fox jumped over the lazy dog.";
+// Call the method as if it were an
+// instance method on the type. Note that the first
+// parameter is not specified by the calling code.
+int i = s.WordCount();
+System.Console.WriteLine($"Word count of s is {i}");
+
+
+namespace CustomExtensions
+{
+    // Extension methods must be defined in a static class.
+    public static class StringExtension
+    {
+        extension(string str)
+        {
+            // This is the extension member.
+            // The `str` parameter is declared on the extension declaration.
+            public int WordCount()
+            {
+                return str.Split(new char[] { ' ', '.', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+            }
+        }
+    }
+}
+

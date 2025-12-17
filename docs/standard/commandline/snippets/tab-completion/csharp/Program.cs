cf92b66732e59040fe9c1ce2f810583afb9fbbdd
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2025-12-17 14:51:10 -0800

    undo two files that need to be verified

--- docs/standard/commandline/snippets/tab-completion/csharp/Program.cs
+++ docs/standard/commandline/snippets/tab-completion/csharp/Program.cs
@@ -1,39 +1,33 @@
 ﻿// <all>
 using System.CommandLine;
 using System.CommandLine.Completions;
+using System.CommandLine.Parsing;
 
-var rootCommand = new RootCommand("Sample app with tab completion")
-{
-    new DateCommand()
-};
-
-await rootCommand.Parse(args).InvokeAsync();
-
-//new DateCommand().Parse(args).Invoke();
+new DateCommand().Parse(args).Invoke();
 
 class DateCommand : Command
 {
-    private readonly Argument<string> _subjectArgument = new("subject")
+    private Argument<string> subjectArgument = new("subject")
     {
         Description = "The subject of the appointment."
     };
-    private readonly Option<DateTime> _dateOption = new("--date")
+    private Option<DateTime> dateOption = new("--date")
     {
         Description = "The day of week to schedule. Should be within one week."
     };
 
     public DateCommand() : base("schedule", "Makes an appointment for sometime in the next week.")
     {
-        this.Arguments.Add(_subjectArgument);
-        this.Options.Add(_dateOption);
+        this.Arguments.Add(subjectArgument);
+        this.Options.Add(dateOption);
 
         // <dateoption>
-        _dateOption.CompletionSources.Add(ctx => {
-            DateTime today = DateTime.Today;
-            List<CompletionItem> dates = [];
-            foreach (int i in Enumerable.Range(1, 7))
+        dateOption.CompletionSources.Add(ctx => {
+            var today = System.DateTime.Today;
+            List<CompletionItem> dates = new();
+            foreach (var i in Enumerable.Range(1, 7))
             {
-                DateTime date = today.AddDays(i);
+                var date = today.AddDays(i);
                 // <completionitem>
                 dates.Add(new CompletionItem(
                     label: date.ToShortDateString(),
@@ -46,7 +40,7 @@ class DateCommand : Command
 
         this.SetAction(parseResult =>
         {
-            Console.WriteLine($"Scheduled \"{parseResult.GetValue(_subjectArgument)}\" for {parseResult.GetValue(_dateOption)}");
+            Console.WriteLine($"Scheduled \"{parseResult.GetValue(subjectArgument)}\" for {parseResult.GetValue(dateOption)}");
         });
     }
 }

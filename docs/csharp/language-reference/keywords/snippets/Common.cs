2d143531234c4d09a83957d5eeb41b5fa32dc74a
Bill Wagner <wiwagn@microsoft.com>
2026-01-23 13:25:03 -0500

    Restore needed file

---
+++ docs/csharp/language-reference/keywords/snippets/Common.cs
@@ -0,0 +1,30 @@
+﻿namespace CommonTypes;
+
+
+public class Common
+{
+    // The element type of the data source
+    public class Student
+    {
+        public required string First { get; init; }
+        public required string Last { get; init; }
+        public required int ID { get; init; }
+        public required List<int> Scores;
+    }
+
+    public static List<Student> GetStudents()
+    {
+        // Use a collection initializer to create the data source. Note that each element
+        //  in the list contains an inner sequence of scores.
+        List<Student> students =
+        [
+           new Student {First="Svetlana", Last="Omelchenko", ID=111, Scores= [97, 72, 81, 60]},
+           new Student {First="Claire", Last="O'Donnell", ID=112, Scores= [75, 84, 91, 39]},
+           new Student {First="Sven", Last="Mortensen", ID=113, Scores= [88, 94, 65, 85]},
+           new Student {First="Cesar", Last="Garcia", ID=114, Scores= [97, 89, 85, 82]},
+           new Student {First="Debra", Last="Garcia", ID=115, Scores= [35, 72, 91, 70]}
+        ];
+
+        return students;
+    }
+}

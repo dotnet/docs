0a9deaf2e4be9b75f3e2043e55ef1722fac1471a
Ivan Pehsterski <peshterskiivan@gmail.com>
2026-01-30 20:38:36 +0200

    Add interface-based greeting example using switch expression

---
+++ docs/csharp/tutorials/snippets/Greeting/Greeting/Models/Greeter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Greeting.Contracts;
+
+namespace Greeting.Models
+{
+	public class Greeter : IGreet
+	{
+        public string GetGreeting(int hour)
+        {
+            return hour switch
+            {
+                >= 5 and <= 11 => "Good morning!",
+                >= 12 and <= 17 => "Good afternoon!",
+                >= 18 and <= 22 => "Good evening!",
+                _ => "Good night!"
+
+            };
+        }
+    }
+}
+

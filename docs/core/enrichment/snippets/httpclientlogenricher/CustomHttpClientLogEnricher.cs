69d93d5b4a2b09c2de2b9a83b8d376ec3e0b826a
mariamgerges <mariamaziz@microsoft.com>
2026-02-13 14:00:31 +0100

    Adding documents for HTTP client logging and HTTP client log enricher (#51715)
    * Create documentation for HTTP client logging and HTTP client log enrichment
    
    * Create documentation for HTTP client logging and HTTP client log enrichment
    
    * updated tocs
    
    * fixing reference
    
    * fix
    
    * suggestions
    
    * comments

---
+++ docs/core/enrichment/snippets/httpclientlogenricher/CustomHttpClientLogEnricher.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Diagnostics.Enrichment;
+using Microsoft.Extensions.Http.Logging;
+
+public class CustomHttpClientLogEnricher : IHttpClientLogEnricher
+{
+    public void Enrich(IEnrichmentTagCollector collector, HttpRequestMessage request,
+        HttpResponseMessage? response, Exception? exception)
+    {
+        // Add custom tags based on the request
+        collector.Add("request_method", request.Method.ToString());
+
+        // Add tags based on the response (if available)
+        if (response is not null)
+        {
+            collector.Add("response_status_code", (int)response.StatusCode);
+        }
+
+        // Add tags based on the exception (if available)
+        if (exception is not null)
+        {
+            collector.Add("error_type", exception.GetType().Name);
+        }
+    }
+}

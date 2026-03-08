c25c30a18212d66d729fcaa716d901b29d0738f4
Ivan Peshterski <peshterskiivan@gmail.com>
2026-03-08 23:06:23 +0200

    Improve sample: safer argument parsing and clearer delay

--- docs/core/docker/snippets/App/Program.cs
+++ docs/core/docker/snippets/App/Program.cs
@@ -1,9 +1,16 @@
 ﻿var counter = 0;
-var max = args.Length is not 0 ? Convert.ToInt32(args[0]) : -1;
 
-while (max is -1 || counter < max)
+// If a number is passed as a command-line argument,
+// it will be used as the maximum counter value.
+var max = args.Length > 0 && int.TryParse(args[0], out var parsedMax)
+    ? parsedMax
+    : -1;
+
+// Run indefinitely if no max value is provided
+while (max == -1 || counter < max)
 {
     Console.WriteLine($"Counter: {++counter}");
 
-    await Task.Delay(TimeSpan.FromMilliseconds(1_000));
+    // Wait for one second between iterations
+    await Task.Delay(TimeSpan.FromSeconds(1));
 }

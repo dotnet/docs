aec74be00dfcfa3f7aed67f3f9fad0b7c927d4a6
Bill Wagner <wiwagn@microsoft.com>
2026-03-11 11:19:47 -0400

    Major review of the index
    Content review of the index.md file.

--- docs/csharp/fundamentals/program-structure/snippets/structure/Program.cs
+++ docs/csharp/fundamentals/program-structure/snippets/structure/Program.cs
@@ -1,24 +1,6 @@
 ﻿// A skeleton of a C# program using an explicit Main method
 namespace YourNamespace;
 
-class YourClass
-{
-}
-
-struct YourStruct
-{
-}
-
-interface IYourInterface
-{
-}
-
-delegate int YourDelegate();
-
-enum YourEnum
-{
-}
-
 class Program
 {
     static void Main(string[] args)

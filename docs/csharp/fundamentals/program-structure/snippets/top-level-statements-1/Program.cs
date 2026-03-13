70b50a8506350de3c422fc868c6e274ea30f8197
Bill Wagner <wiwagn@microsoft.com>
2026-03-13 17:07:17 -0400

    Fix up samples

---
+++ docs/csharp/fundamentals/program-structure/snippets/top-level-statements-1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+StringBuilder builder = new();
+builder.AppendLine("The following arguments are passed:");
+
+foreach (var arg in args)
+{
+    builder.AppendLine($"Argument={arg}");
+}
+
+Console.WriteLine(builder.ToString());
+
+return 0;

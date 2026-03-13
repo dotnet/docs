acc5dbad62206fdac6f4fff181d7140c5404bad1
Bill Wagner <wiwagn@microsoft.com>
2026-03-13 17:42:02 -0400

    Revert "remove snippets no longer needed"
    This reverts commit 6d02366f5e83f2c2698c86c7f7b447680204461d.

---
+++ docs/csharp/fundamentals/program-structure/snippets/main-command-line/Factorial.cs
@@ -0,0 +1,50 @@
+﻿public class Functions
+{
+    public static long Factorial(int n)
+    {
+        // Test for invalid input.
+        if ((n < 0) || (n > 20))
+        {
+            return -1;
+        }
+
+        // Calculate the factorial iteratively rather than recursively.
+        long tempResult = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            tempResult *= i;
+        }
+        return tempResult;
+    }
+}
+
+class MainClass
+{
+    static int Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Please enter a numeric argument.");
+            Console.WriteLine("Usage: Factorial <num>");
+            return 1;
+        }
+
+        int num;
+        bool test = int.TryParse(args[0], out num);
+        if (!test)
+        {
+            Console.WriteLine("Please enter a numeric argument.");
+            Console.WriteLine("Usage: Factorial <num>");
+            return 1;
+        }
+
+        long result = Functions.Factorial(num);
+
+        if (result == -1)
+            Console.WriteLine("Input must be >= 0 and <= 20.");
+        else
+            Console.WriteLine($"The Factorial of {num} is {result}.");
+
+        return 0;
+    }
+}
---
+++ docs/csharp/fundamentals/program-structure/snippets/main-command-line/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace main_command_line;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        //<Snippet4>
+        if (args.Length == 0)
+        {
+            System.Console.WriteLine("Please enter a numeric argument.");
+            return 1;
+        }
+        //</Snippet4>
+
+        return 0;
+    }
+}
+

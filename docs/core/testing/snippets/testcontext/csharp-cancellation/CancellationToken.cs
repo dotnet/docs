6a7090c2c26e7196b7fc92c893e950cc3b4df5fe
Amaury Levé <amauryleve@microsoft.com>
2026-03-19 09:10:26 +0100

    Improve doc for MSTEST0049 and TestContext
    Give example on how to use the cancellation token

---
+++ docs/core/testing/snippets/testcontext/csharp-cancellation/CancellationToken.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class TestClassCancellationToken
+{
+    public TestContext TestContext { get; set; }
+
+    [TestMethod]
+    [Timeout(5000, CooperativeCancellation = true)]
+    public async Task MyAsyncTest()
+    {
+        using var client = new HttpClient();
+        var response = await client.GetAsync(
+            "https://example.com", TestContext.CancellationToken);
+
+        Assert.IsTrue(response.IsSuccessStatusCode);
+    }
+}

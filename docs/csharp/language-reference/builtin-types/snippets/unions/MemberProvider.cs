1230ccd4d4f0705f5866ca95bac6f9c60428b82b
Barion <barion+git@ik.me>
2026-08-04 07:48:20 +0200

    Update MemberProvider.cs

--- docs/csharp/language-reference/builtin-types/snippets/unions/MemberProvider.cs
+++ docs/csharp/language-reference/builtin-types/snippets/unions/MemberProvider.cs
@@ -8,12 +8,38 @@ public record class Outcome<T> : Outcome<T>.IUnionMembers
 
     public interface IUnionMembers
     {
-        static Outcome<T> Create(T? value) => new(value);
-        static Outcome<T> Create(Exception? value) => new(value);
+        public static Outcome<T> Create(T? value) => new(value: value);
+        public static Outcome<T> Create(Exception? value) => new(value: value);
         object? Value { get; }
+
+        // only when needed
+        bool TryGetValue(out T value);
+        bool TryGetValue(out Exception value);
     }
 
     object? IUnionMembers.Value => _value;
+
+    public bool TryGetValue(out T value)
+    {
+        if (_value is T t)
+        {
+            value = t;
+            return true;
+        }
+        value = default!;
+        return false;
+    }
+
+    public bool TryGetValue(out Exception value)
+    {
+        if (_value is Exception e)
+        {
+            value = e;
+            return true;
+        }
+        value = default!;
+        return false;
+    }
 }
 // </MemberProvider>
 

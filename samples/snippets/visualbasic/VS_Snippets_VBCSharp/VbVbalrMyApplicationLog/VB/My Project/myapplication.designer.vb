44f135f83b02eae002f7dcb3f5cf56dc75e8ebd6
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2026-09-22 16:15:32 -0700

    copilot fixes the compile errors

--- samples/snippets/visualbasic/VS_Snippets_VBCSharp/VbVbalrMyApplicationLog/VB/My Project/myapplication.designer.vb
+++ samples/snippets/visualbasic/VS_Snippets_VBCSharp/VbVbalrMyApplicationLog/VB/My Project/myapplication.designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.VbVbalrMyApplicationLog.VbVbalrMyApplicationLog.Form1
+            Me.MainForm = Global.VbVbalrMyApplicationLog.Form1
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
